84474c3db2693eff4a1e617f6d13994ae9e38abb
Milan Mladoniczky <mladoniczky@netgrif.com>
2017-03-06 18:47:09 +0100

    WMS-90 - Data fields - frontend - fixed loading field input template - fixed bug with loading ngData module

--- scripts/controllers/DataController.js
+++ scripts/controllers/DataController.js
@@ -1,7 +1,7 @@
 /**
  * Created by martin on 6.3.2017.
  */
-define(['angular','../modules/Main'],function (angular) {
+define(['angular','../modules/Main','../modules/Data'],function (angular) {
     angular.module('ngData').controller('DataController',['$log','$scope',
         function ($log, $scope) {
             var self = this;
