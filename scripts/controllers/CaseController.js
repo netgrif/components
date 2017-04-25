492a92ece4fc6f1102362dd3e5866256b8d4e27c
Milan Mladoniczky <mladoniczky@netgrif.com>
2017-04-26 00:28:32 +0200

    WMS-153 - Case view - load all tasks in case - assign task

--- scripts/controllers/CaseController.js
+++ scripts/controllers/CaseController.js
@@ -1,27 +1,70 @@
 define(['angular', '../modules/Main', '../modules/Workflow'], function (angular) {
     class Tab {
-        constructor(index, useCase) {
+        constructor(index, useCase, $http, $snackbar) {
             this.index = index;
             this.useCase = useCase;
+            this.$http = $http;
+            this.$snackbar = $snackbar;
 
             this.tasks = [];
             this.sort = {
-                field:'',
+                field: '',
                 reverse: false
             }
         }
 
-        setSortField(field){
+        setSortField(field) {
             this.sort.reverse = this.sort.field === field ? !this.sort.reverse : false;
             this.sort.field = field;
         }
 
-        loadTaskData(visualId){
+        getStatus(task) {
+            if (task.user && task.finishDate) task.status = "Done";
+            else if (task.user && !task.finishDate && task.startDate) task.status = "Assigned";
+            else task.status = "New";
+        };
+
+        loadTasks() {
+            const self = this;
+            this.$http.post("/res/task/case", [this.useCase.stringId]).then(function (response) {
+                response.$request().$get("tasks").then(function (resources) {
+                    self.tasks = resources;
+                    self.tasks.forEach(task => self.getStatus(task));
+                }, function () {
+                    self.$snackbar("Task resources was not found!");
+                    self.tasks.splice(0, self.tasks.length);
+                })
+            }, function () {
+                self.$snackbar("Failed to tasks for case " + self.useCase.title);
+            })
+        }
+
+        assignTask(task) {
+            const self = this;
+            this.$http.get(task.$href("assign")).then(function (response) {
+                if (response.success) self.loadTasks();
+                if (response.error) self.$snackbar.error(response.error);
+            }, function () {
+                self.$snackbar.error("Assigning task " + task.title + " failed");
+            });
+        };
+
+        cancelTask(task) {
+            const self = this;
+            this.$http.get(task.$href("cancel")).then(function (response) {
+                if (response.success) self.loadTasks();
+                if (response.error) self.$snackbar.error(response.error);
+            }, function () {
+                self.$snackbar.error("Canceling assignment of task " + task.title + " failed");
+            });
+        };
+
+        loadTaskData(visualId) {
 
         }
     }
 
-    angular.module('ngWorkflow').controller('CaseController', ['$log', '$scope','$http','$snackbar',
+    angular.module('ngWorkflow').controller('CaseController', ['$log', '$scope', '$http', '$snackbar',
         function ($log, $scope, $http, $snackbar) {
             var self = this;
 
@@ -34,7 +77,7 @@ define(['angular', '../modules/Main', '../modules/Workflow'], function (angular)
                 selected: undefined,
                 chips: [],
                 filter: [],
-                nets:[]
+                nets: []
             };
             self.sort = {
                 field: '',
@@ -42,20 +85,21 @@ define(['angular', '../modules/Main', '../modules/Workflow'], function (angular)
             };
 
             self.searchCases = function () {
-                $http.post("/res/workflow/case/search",self.filter.filter).then(function (response) {
+                $http.post("/res/workflow/case/search", self.filter.filter).then(function (response) {
                     response.$request().$get("cases").then(function (resources) {
                         self.cases = resources;
                     }, function () {
                         $snackbar.error("No resource for cases was found!");
-                        self.cases.splice(0,self.cases.length);
+                        self.cases.splice(0, self.cases.length);
                     });
-                },function () {
+                }, function () {
                     $snackbar.error("Getting cases failed!");
                 })
             };
 
             self.tabChange = function () {
                 self.activeTab = self.tabs[self.activeTabIndex - 1];
+                self.activeTab.loadTasks();
             };
 
             self.createCase = function () {
@@ -63,12 +107,13 @@ define(['angular', '../modules/Main', '../modules/Workflow'], function (angular)
             };
 
             self.openCase = function (useCase) {
-                self.tabs.push(new Tab(self.tabs.length, useCase));
+                if (!self.tabs.some(tab => tab.useCase.stringId === useCase.stringId))
+                    self.tabs.push(new Tab(self.tabs.length, useCase, $http, $snackbar));
             };
 
             self.tabClose = function (tabIndex) {
                 self.tabs.splice(tabIndex, 1);
-                self.activeTabIndex = tabIndex < self.activeTabIndex ? self.activeTabIndex-1 : self.activeTabIndex;
+                self.activeTabIndex = tabIndex < self.activeTabIndex ? self.activeTabIndex - 1 : self.activeTabIndex;
                 for (let i = tabIndex; i < self.tabs.length; i++) {
                     self.tabs[i].index = i;
                 }
