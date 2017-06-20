218cb5f62b3a07730b1381a4023431c940e49852
Tomáš Gažo <gazo@netgrif.com>
2017-06-20 14:54:06 +0200

    Add toolbar and started main nav

--- scripts/services/Style.js
+++ scripts/services/Style.js
@@ -5,22 +5,11 @@ define(['angular','../modules/Main'],function (angular) {
         return {
             mainView: function () {
 
-                el('.btn-menu > .material-icons').css({
-					marginRight: '14px',
-					marginTop: '1.5px',
-					marginBottom: '1.5px',
-					fontSize: '24px'
-				});
+				// el('.btn-menu > span').css({
+				// 	marginLeft: '-1px',
+				// 	fontSize: '15px'
+				// });
 
-				el('.btn-menu > span').css({
-					marginLeft: '-1px',
-					fontSize: '15px'
-				});
-
-                el('.btn-menu > span').css({
-                    marginLeft: '-1px',
-                    fontSize: '15px'
-                });
             }
         };
     });
