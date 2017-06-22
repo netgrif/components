9ad32f2b286013cae95287ec921c8be0f3c6c6c0
Tomáš Gažo <gazo@netgrif.com>
2017-06-22 20:50:27 +0200

    Style contacts page

--- scripts/controllers/ContactsController.js
+++ scripts/controllers/ContactsController.js
@@ -1,10 +1,11 @@
 define(['angular', '../modules/Contacts', '../modules/Main'],
     function (angular) {
         angular.module('ngContacts').controller('ContactsController',
-            ['$log', '$scope', '$timeout',
-                function ($log, $scope, $timeout) {
+            ['$log', '$scope',
+                function ($log, $scope) {
                     const self = this;
 
+                    self.alphabet = Array.apply(null, {length: 26}).map((x,i) => String.fromCharCode(65 + i));
                 }]);
     });
 
