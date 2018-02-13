c3af7fabf154131ee428181a7dab234417ef22a9
Milan Mladoniczky <mladoniczky@netgrif.com>
2018-02-13 21:48:32 +0100

    NAE-74 - Case date time - fix wrong padding generation for date if hours or minutes are 0

--- scripts/classes/Case.js
+++ scripts/classes/Case.js
@@ -1,4 +1,4 @@
-define(['./DataField','./HalResource','./Task'], function (DataField, HalResource, Task) {
+define(['./DataField', './HalResource', './Task'], function (DataField, HalResource, Task) {
     /**
      * Constructor for Case class
      * Angular dependency: $http, $snackbar, $dialog, $fileUpload, $user, $i18n
@@ -16,9 +16,9 @@ define(['./DataField','./HalResource','./Task'], function (DataField, HalResourc
         this.panel = panel;
         Object.assign(this, resource, angular, config);
 
-        if(this.immediateData){
+        if (this.immediateData) {
             this.immediateData.forEach(data => {
-                if(data.type === 'date') data.value = Task.formatDate(data.value);
+                if (data.type === 'date') data.value = Task.formatDate(data.value);
             });
         }
         this.formatedDate = Case.formatDate(this.creationDate);
@@ -31,13 +31,16 @@ define(['./DataField','./HalResource','./Task'], function (DataField, HalResourc
     Case.prototype.constructor = Case;
 
     Case.formatDate = function (date) {
-        if (!date) return undefined;
-        if (date instanceof Date) return `${DataField.padding(date.getDate(), 0)}.${DataField.padding(date.getMonth() + 1, 0)}. ${date.getFullYear()}`;
+        if (!date)
+            return undefined;
+        if (date instanceof Date)
+            return `${DataField.padding(date.getDate(), 0)}.${DataField.padding(date.getMonth() + 1, 0)}. ${date.getFullYear()}`;
+
         return `${DataField.padding(date.dayOfMonth, 0)}.${DataField.padding(date.monthValue, 0)}.${date.year}
-            ${DataField.padding(date.hour, 0)}${date.hour ? ':' : ''}${DataField.padding(date.minute, 0)}`;
+            ${DataField.padding(date.hour, 0, 0)}:${DataField.padding(date.minute, 0, 0)}`;
     };
 
-    Case.prototype.load = function (callback = ()=>{}) {
+    Case.prototype.load = function (callback = () => {}) {
         //TODO 9.7.2017 load case data
     };
 
@@ -47,12 +50,12 @@ define(['./DataField','./HalResource','./Task'], function (DataField, HalResourc
 
     Case.prototype.delete = function () {
         const self = this;
-        this.$http.delete("/res/workflow/case/"+this.stringId).then(function (response){
-            if(response.success){
+        this.$http.delete("/res/workflow/case/" + this.stringId).then(function (response) {
+            if (response.success) {
                 self.tab.closeCase(self);
                 self.$snackbar.success(`${self.$i18n.block.snackbar.case} ${self.title} ${self.$i18n.block.snackbar.wasDeleted}`);
                 self.tab.delete(self);
-            } else if(response.error){
+            } else if (response.error) {
                 self.$snackbar.error(response.error); //TODO 9.8.2017 Server i18n?
             }
         }, function () {
