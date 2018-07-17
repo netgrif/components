12447e38ed053bff5837d6f1a084ad829e25bc56
Tomáš Gažo <gazo@netgrif.com>
2018-07-18 00:56:15 +0200

    NAE-274 - Button data-field UI - create test data group in dashboard for developing UI - create button field base structure samples

--- scripts/controllers/DashboardController.js
+++ scripts/controllers/DashboardController.js
@@ -11,80 +11,9 @@ define(['angular', '../classes/Case', '../classes/ActionCase', '../classes/Filte
                         direction: "desc"
                     };
 
-                    // self.tabs = {
-                    //     quotes: {
-                    //         cases: [],
-                    //         loading: false,
-                    //         title: $i18n.page.dashboard.quotes
-                    //     },
-                    //     policies: {
-                    //         cases: [],
-                    //         loading: false,
-                    //         title: $i18n.page.dashboard.policies
-                    //     },
-                    //     contacts: {
-                    //         cases: [],
-                    //         loading: false,
-                    //         title: $i18n.page.dashboard.contacts
-                    //     }
-                    // };
-                    //
-                    // self.load = function (filter, tab, type) {
-                    //     const request = {
-                    //         method: "POST",
-                    //         url: `/api/workflow/case/search?sort=${self.sort.attribute},${self.sort.direction}&size=${self.limit}`,
-                    //         data: filter.query
-                    //     };
-                    //     tab.loading = true;
-                    //     $http(request).then(response => {
-                    //         if (!response.$response().data._embedded) {
-                    //             tab.loading = false;
-                    //             return;
-                    //         }
-                    //         const rawData = response.$response().data._embedded.cases;
-                    //         response.$request().$get("cases").then(resources => {
-                    //             resources.forEach((r, i) => tab.cases.push(new Case(self, null, r, rawData[i]._links, {
-                    //                 $http,
-                    //                 $dialog,
-                    //                 $snackbar,
-                    //                 $user,
-                    //                 $fileUpload,
-                    //                 $i18n
-                    //             }, {
-                    //                 represents: type
-                    //             })));
-                    //             tab.loading = false;
-                    //
-                    //         }, () => {
-                    //             $snackbar.error($i18n.block.snackbar.noResourceForCasesFound);
-                    //             tab.splice(0, tab.cases.length);
-                    //             tab.loading = false;
-                    //         });
-                    //     }, error => {
-                    //         $snackbar.error($i18n.block.snackbar.gettingCasesFailed);
-                    //         $log.error(error);
-                    //         tab.loading = false;
-                    //     });
-                    // };
-                    //
-                    // self.openCase = function (useCase) {
-                    //     const o = {
-                    //         cases: useCase
-                    //     };
-                    //     $cache.put("dashboard", o);
-                    //     $location.path("/"+useCase.represents);
-                    // };
-                    //
-                    // self.load(new Filter("Quotes", Filter.CASE_TYPE,
-                    //     `{"author":"${$user.login}","transition":["${$process.get("Insurance").transition("Iba nehnuteľnosť").id}", "${$process.get("Insurance").transition("Všeobecné informácie").id}"]}`,
-                    //     "{}", null, null), self.tabs.quotes, "quotes");
-                    //
-                    // self.load(new Filter("Policies", Filter.CASE_TYPE,
-                    //     `{"author":"${$user.login}","transition":"${$process.get("Insurance").transition("Sekcia - Všeobecné informácie").id}"}`,
-                    //     "{}", null, null), self.tabs.policies, "policies");
-                    //
-                    // self.load(new Filter("Contacts", Filter.CASE_TYPE,
-                    //     `{"author":"${$user.login}","petriNet":"${$process.get("Contact").id}"}`,
-                    //     "{}", null, null), self.tabs.contacts, "contacts");
+                    self.testArray = [];
+                    for (let i = 1; i <= 5; i++) {
+                        self.testArray.push(i);
+                    }
                 }]);
     });
