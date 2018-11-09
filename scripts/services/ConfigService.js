4aa8c33ac6b55f26b99c07704f70fe8f89dee6a0
Tomáš Krasoň <krason@netgrif.com>
2018-11-09 09:44:18 +0100

    NAE-300 Version plugin integration
    - version plugin integrated

--- scripts/services/ConfigService.js
+++ scripts/services/ConfigService.js
@@ -1,9 +1,9 @@
 
-define(['angular','config', '../modules/Main'],function (angular, config) {
+define(['angular','config', 'scripts/version', '../modules/Main'],function (angular, config, version) {
     angular.module('ngMain').factory('$config',function ($log) {
         const service = {
             //Object for some methods to the future
         };
-        return Object.assign(service, config);
+        return Object.assign(service, config, version);
     });
 });
