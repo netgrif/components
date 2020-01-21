059639d787e8b23abfbea0332a43469b2996e454
Daniel Vaník <vanik@netgrif.com>
2020-01-21 16:09:46 +0100

    NAE-330 - Random file upload - fix random file upload in task view to unique id for each input file element

---
+++ projects/netgrif-application-engine/src/lib/data-fields/abstract-data-field/unvalidated-data-fields/unvalidated-data-field.ts
@@ -0,0 +1,4 @@
+import {DataField} from "../abstract-data-field";
+
+export abstract class UnvalidatedDataField extends DataField{
+}
---
+++ projects/netgrif-application-engine/src/lib/data-fields/abstract-data-field/validated-data-fields/validated-data-field.ts
@@ -0,0 +1,4 @@
+import {DataField} from "../abstract-data-field";
+
+export abstract class ValidatedDataField extends DataField{
+}
