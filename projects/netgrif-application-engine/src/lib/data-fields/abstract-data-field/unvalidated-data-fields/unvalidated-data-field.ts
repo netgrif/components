0e624948b42a33cf451bca0f0c812beb440c0d56
Daniel Vaník <vanik@netgrif.com>
2020-01-22 16:11:29 +0100

    NAE-540 - Abstract Data Field - refactor abstract data field structure

--- projects/netgrif-application-engine/src/lib/data-fields/abstract-data-field/unvalidated-data-fields/unvalidated-data-field.ts
+++
@@ -1,4 +0,0 @@
-import {DataField} from "../abstract-data-field";
-
-export abstract class UnvalidatedDataField extends DataField{
-}
--- projects/netgrif-application-engine/src/lib/data-fields/abstract-data-field/validated-data-fields/validated-data-field.ts
+++
@@ -1,5 +0,0 @@
-import {DataField} from "../abstract-data-field";
-
-export abstract class ValidatedDataField extends DataField{
-}
-
