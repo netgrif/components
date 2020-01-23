88120b5d6da8a1f7755fef0644b8f3f911fd1696
Jakub Kovář <kovar@netgrif.com>
2020-01-23 13:45:01 +0100

    NAE-543- Number Field - This mostly reverts commit e4710fa9

--- projects/nae-example-app/src/app/app.module.ts
+++ projects/nae-example-app/src/app/app.module.ts
@@ -3,7 +3,7 @@ import {NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {LoggerModule, DataFieldsModule} from '@netgrif/application-engine';
+import {LoggerModule} from '@netgrif/application-engine';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
@@ -14,9 +14,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
         BrowserModule,
         AppRoutingModule,
         BrowserAnimationsModule,
-        LoggerModule,
-        DataFieldsModule,
-        DataFieldsModule
+        LoggerModule
     ],
     providers: [],
     bootstrap: [AppComponent]
