a92d68a8d752fc752d03f8cca7ddbf9a9929933b
Jakub Kovář <kovar@netgrif.com>
2020-01-30 09:44:02 +0100

    NAE-543- Number Field - saving modified files before revert

--- projects/netgrif-application-engine/src/lib/data-fields/data-fields.module.ts
+++ projects/netgrif-application-engine/src/lib/data-fields/data-fields.module.ts
@@ -1,27 +1,32 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { TextFieldComponent } from "./text-field/text-field.component";
-import { NumberFieldComponent } from './number-field/number-field.component';
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatInputModule } from "@angular/material/input";
-import { FormsModule } from "@angular/forms";
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { DataFieldTemplateComponent } from './data-field-template/data-field-template.component';
-import { ParentWidthDependantStyleDirective } from './data-field-template/directives/parent-width-dependant-style.directive';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {TextFieldComponent} from "./text-field/text-field.component";
+import {NumberFieldComponent} from './number-field/number-field.component';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {FormsModule} from "@angular/forms";
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {DataFieldTemplateComponent} from './data-field-template/data-field-template.component';
+import {ParentWidthDependantStyleDirective} from './data-field-template/directives/parent-width-dependant-style.directive';
 
 @NgModule({
-  declarations: [
-      DataFieldTemplateComponent,
-      ParentWidthDependantStyleDirective,
-      TextFieldComponent,
-      NumberFieldComponent
-  ],
-  imports: [
-    CommonModule,
-      MatFormFieldModule,
-      MatInputModule,
-      FormsModule,
-    FlexLayoutModule
-  ]
+    declarations: [
+        DataFieldTemplateComponent,
+        ParentWidthDependantStyleDirective,
+        TextFieldComponent,
+        NumberFieldComponent
+    ],
+    exports: [
+        TextFieldComponent,
+        NumberFieldComponent
+    ],
+    imports: [
+        CommonModule,
+        MatFormFieldModule,
+        MatInputModule,
+        FormsModule,
+        FlexLayoutModule
+    ]
 })
-export class DataFieldsModule { }
+export class DataFieldsModule {
+}
