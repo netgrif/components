03ad9193c4a55673935466df6510f3046d41eb24
Martin Miklovič <miklovic@netgrif.com>
2020-02-18 15:40:13 +0100

    NAE-511 - Case Panel - Removed example documentation

--- projects/nae-example-app/src/app/app.component.ts
+++ projects/nae-example-app/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import {Component} from '@angular/core';
 import {LoggerService} from '@netgrif/application-engine';
-import {Router} from '@angular/router';
 
 @Component({
     selector: 'app-root',
@@ -10,7 +9,7 @@ import {Router} from '@angular/router';
 export class AppComponent {
     title = 'nae-example-app';
 
-    constructor(log: LoggerService, public routes: Router) {
+    constructor(log: LoggerService) {
         log.info('App component has started');
     }
 }
--- projects/nae-example-app/src/app/app.module.ts
+++ projects/nae-example-app/src/app/app.module.ts
@@ -1,37 +1,20 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import {LoggerModule} from '@netgrif/application-engine';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {RouterModule, Routes} from "@angular/router";
-import {FlexLayoutModule} from "@angular/flex-layout";
-import {
-    LoggerModule,
-    PanelModule,
-    MaterialModule,
-    CovalentModule
-} from '@netgrif/application-engine';
-import {ExamplePanelComponent} from './example-panel/example-panel.component';
-
-export const routes: Routes = [
-    {path: 'panel component example', component: ExamplePanelComponent},
-];
 
 @NgModule({
     declarations: [
-        AppComponent,
-        ExamplePanelComponent
+        AppComponent
     ],
     imports: [
         BrowserModule,
         AppRoutingModule,
         BrowserAnimationsModule,
-        LoggerModule,
-        RouterModule.forRoot(routes),
-        FlexLayoutModule,
-        PanelModule,
-        MaterialModule,
-        CovalentModule
+        LoggerModule
     ],
     providers: [],
     bootstrap: [AppComponent]
