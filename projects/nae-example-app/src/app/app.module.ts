f79d08f1f31e09fc4b055fd9281ad42e472d55a5
Milan Mladoniczky <mladoniczky@netgrif.com>
2020-02-20 12:44:29 +0100

    Revert "NAE-515 Config Service Add schematic to create scss files"
    This reverts commit 6142ef8e

--- projects/nae-example-app/src/app/app.module.ts
+++ projects/nae-example-app/src/app/app.module.ts
@@ -3,7 +3,7 @@ import {NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {LoggerModule, MaterialModule} from '@netgrif/application-engine';
+import {LoggerModule} from '@netgrif/application-engine';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
@@ -14,8 +14,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
         BrowserModule,
         AppRoutingModule,
         BrowserAnimationsModule,
-        LoggerModule,
-        MaterialModule
+        LoggerModule
     ],
     providers: [],
     bootstrap: [AppComponent]
