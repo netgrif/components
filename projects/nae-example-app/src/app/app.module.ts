f2cbdca3e76d2db3410b9b39461b90a9a4ac043b
Martin Miklovič <miklovic@netgrif.com>
2020-02-21 16:33:50 +0100

    NAE-510 - Task Panel - Example app revert

--- projects/nae-example-app/src/app/app.module.ts
+++ projects/nae-example-app/src/app/app.module.ts
@@ -3,15 +3,7 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCardModule, MatListModule, MatSidenavModule} from "@angular/material";
-import {RouterModule, Routes} from "@angular/router";
-import {FlexLayoutModule} from "@angular/flex-layout";
-import {
-    LoggerModule,
-} from '@netgrif/application-engine';
 
-export const routes: Routes = [
-];
 
 @NgModule({
     declarations: [
@@ -21,13 +13,6 @@ export const routes: Routes = [
         BrowserModule,
         AppRoutingModule,
         BrowserAnimationsModule,
-        LoggerModule,
-        MatButtonModule,
-        RouterModule.forRoot(routes),
-        MatSidenavModule,
-        MatListModule,
-        FlexLayoutModule,
-        MatCardModule
     ],
     providers: [],
     bootstrap: [AppComponent]
