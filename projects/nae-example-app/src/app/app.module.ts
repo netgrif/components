85da29ecea7a6e2d1973317d1c80807a4d6a5eb0
Daniel Vaník <vanik@netgrif.com>
2020-02-22 15:57:50 +0100

    release/4.0.0 - remove LoggerModule from demo app.module

--- projects/nae-example-app/src/app/app.module.ts
+++ projects/nae-example-app/src/app/app.module.ts
@@ -3,7 +3,6 @@ import {NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {LoggerModule} from '@netgrif/application-engine';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
@@ -13,8 +12,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
     imports: [
         BrowserModule,
         AppRoutingModule,
-        BrowserAnimationsModule,
-        LoggerModule
+        BrowserAnimationsModule
     ],
     providers: [],
     bootstrap: [AppComponent]
