1c32ba73a16e927230ff2f205f3d1486ebba1456
Lukáš Rezák <rezak@netgrif.com>
2020-02-25 17:33:17 +0100

    NAE-757 Corect paths and user input

--- projects/netgrif-application-engine/schematics/custom-templates/index.ts
+++ projects/netgrif-application-engine/schematics/custom-templates/index.ts
@@ -1,4 +1,6 @@
-import {Rule, SchematicContext, Tree, url, applyTemplates, apply, move, chain, mergeWith} from '@angular-devkit/schematics';
+import {Rule, SchematicContext,SchematicsException, Tree, url, applyTemplates, apply, move, chain, mergeWith} from '@angular-devkit/schematics';
+import {Schema} from "./schema";
+import { getProjectInfo } from "../utilityFunctions";
 
 function handleJsonString(json: string, mainJsonobject: boolean, darkTheme: boolean) {
     if (mainJsonobject) {
@@ -18,10 +20,24 @@ function handleJsonString(json: string, mainJsonobject: boolean, darkTheme: bool
     return json
 }
 
-export function customTemplates(_options: any): Rule {
+export function customTemplates(_options: Schema): Rule {
     return (tree: Tree, _context: SchematicContext) => {
 
+
         let data = JSON.parse(tree.read("./nae.json")!.toString());
+        let correctPath = false;
+
+        const { path } = _options;
+        if (tree.exists(path) && path.includes("styles.scss")) {
+            tree.overwrite(path,"@import \"./styles/templates/customThemes.scss\"; "+ '\n' + "@include mat-core();" + '\n'  +tree.read(path));
+            correctPath = true
+        }
+        else if(tree.exists("./projects/"+getProjectInfo(tree).projectName+"/src/styles.scss")){
+            tree.overwrite("./projects/"+getProjectInfo(tree).projectName+"/src/styles.scss","@import \"./styles/templates/customThemes.scss;"+ '\n' + "@include mat-core();" + '\n'  +tree.read("./projects/"+getProjectInfo(tree).projectName+"/src/styles.scss"));
+        }
+        else{
+            throw new SchematicsException('Your typed path : '+path+' incorrect and no file styles.scss in src folder.');
+        }
 
         let primaryLigt = handleJsonString(JSON.stringify(data.theme.pallets.light.primary), true, false) + '\n';
         let primaryContrastLight = handleJsonString(JSON.stringify(data.theme.pallets.light.primary.contrast.light), false, false) + " " + handleJsonString(JSON.stringify(data.theme.pallets.light.primary.contrast.dark), false, true);
@@ -35,7 +51,6 @@ export function customTemplates(_options: any): Rule {
         let secondaryContrastDark = handleJsonString(JSON.stringify(data.theme.pallets.dark.secondary.contrast.light), false, false) + " " + handleJsonString(JSON.stringify(data.theme.pallets.dark.secondary.contrast.dark), false, true);
         let warnDark = handleJsonString(JSON.stringify(data.theme.pallets.dark.warn), true, false);
         let warnContrastDark = handleJsonString(JSON.stringify(data.theme.pallets.dark.warn.contrast.light), false, false) + " " + handleJsonString(JSON.stringify(data.theme.pallets.dark.warn.contrast.dark), false, true);
-
 
         const customTemplates = apply(
             url('./files'),
@@ -54,7 +69,7 @@ export function customTemplates(_options: any): Rule {
                     warnDark : warnDark,
                     warnContrastDark : warnContrastDark
                 }),
-                move("./styles/templates")
+                correctPath ? move(path.replace('styles.scss','')+"/styles/templates") : move("./projects/"+getProjectInfo(tree).projectName+"/src/styles/templates")
             ]
         );
 
--- projects/netgrif-application-engine/schematics/project-configuration-service/index.ts
+++ projects/netgrif-application-engine/schematics/project-configuration-service/index.ts
@@ -4,12 +4,8 @@ import {
     chain, mergeWith
 } from '@angular-devkit/schematics';
 
-import { strings, normalize, experimental } from '@angular-devkit/core';
-
-interface ProjectInfo {
-    path: string,
-    projectName: string
-}
+import { getProjectInfo } from "../utilityFunctions";
+import { strings, normalize } from '@angular-devkit/core';
 
 export function projectConfigurationService(): Rule {
     return (tree: Tree) => {
@@ -30,32 +26,6 @@ export function projectConfigurationService(): Rule {
             mergeWith(templateSource)
         ]);
     };
-}
-
-function getProjectInfo(tree: Tree): ProjectInfo {
-    const workspaceConfig = tree.read('/angular.json');
-    if (!workspaceConfig) {
-        throw new SchematicsException('Could not find Angular workspace configuration');
-    }
-
-    const workspaceContent = workspaceConfig.toString();
-
-    const workspace: experimental.workspace.WorkspaceSchema = JSON.parse(workspaceContent);
-
-    const result = {
-        path: '',
-        projectName: ''
-    };
-
-    result.projectName = workspace.defaultProject as string;
-
-    const project = workspace.projects[result.projectName];
-
-    const projectType = project.projectType === 'application' ? 'app' : 'lib';
-
-    result.path = `${project.sourceRoot}/${projectType}`;
-
-    return result;
 }
 
 function getNaeConfiguration(tree: Tree): string {
