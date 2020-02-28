27d83512bc31040c8e60b028220e9bd2ebd1a70c
Martin Miklovič <miklovic@netgrif.com>
2020-02-28 16:00:51 +0100

    NAE-753 Example App - Merge edit

--- projects/nae-example-app/src/app/app.module.ts
+++ projects/nae-example-app/src/app/app.module.ts
@@ -3,7 +3,7 @@ import {NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {LoggerModule, MaterialModule, CovalentModule} from '@netgrif/application-engine';
+import {MaterialModule, CovalentModule} from '@netgrif/application-engine';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {FlexModule} from "@angular/flex-layout";
 import { DocumentationComponent } from './documentation/documentation.component';
@@ -20,11 +20,9 @@ import { ConfigurationService } from '@netgrif/application-engine';
         BrowserModule,
         AppRoutingModule,
         BrowserAnimationsModule,
-        LoggerModule,
         FlexModule,
         MaterialModule,
         CovalentModule
-        BrowserAnimationsModule
     ],
     providers: [{provide: ConfigurationService, useClass: NaeExampleAppConfigurationService}],
     bootstrap: [AppComponent]
