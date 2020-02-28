92b252da6ef573e52a926a0efa7c1c9eebe58473
Jakub Kovář <kovar@netgrif.com>
2020-02-28 14:20:58 +0100

    NAE-504 - Abstract view - getParentPath view utility function

--- projects/netgrif-application-engine/schematics/view/viewUtilityFunctions.ts
+++ projects/netgrif-application-engine/schematics/view/viewUtilityFunctions.ts
@@ -9,3 +9,11 @@ export function getRoutesJsonContent(tree: Tree, projectInfo: ProjectInfo): Rout
     }
     return JSON.parse(routes.toString());
 }
+
+export function getParentPath(path: string): string {
+    const index = path.lastIndexOf('/');
+    if (index === -1) {
+        return '';
+    }
+    return path.substring(0, index-1)
+}
