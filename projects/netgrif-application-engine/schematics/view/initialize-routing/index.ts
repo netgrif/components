ee4063823ce0ff05a4f41417a6e55cf24c7706d7
Lukáš Rezák <rezak@netgrif.com>
2020-03-03 13:11:38 +0100

    NAE-768 - Schematic initialize rout object Schematic that initialize routs

--- projects/netgrif-application-engine/schematics/view/initialize-routing/index.ts
+++ projects/netgrif-application-engine/schematics/view/initialize-routing/index.ts
@@ -1,21 +1,39 @@
 import {
-    chain,
     Rule,
-    Tree,
-    schematic
+    Tree
 } from '@angular-devkit/schematics';
-import {Route} from '@angular/router';
-import {Input} from "@angular/core";
+import {getRoutesJsonContent} from "../viewUtilityFunctions";
+import {getProjectInfo} from "../../utilityFunctions";
+import {Route} from "@angular/router";
 
 
-
-export function initializeRoutes(routeObject: Route ,path: string): Rule {
-
+export function initializeRoutes(routeObject: Route, path: string): Rule {
     return (tree: Tree) => {
-        path = path.split("/");
-
-        return chain([
-            schematic('create-configuration-service', {}),
-        ]);
+        const paths: String[] = path.split("/");
+        const data: Route[] = getRoutesJsonContent(tree, getProjectInfo(tree));
+        (paths.length == 1) ? data.push(routeObject) : updateRoutes(data, routeObject, 0, paths);
+        tree.overwrite(getProjectInfo(tree).path + "/routes.json", JSON.stringify(data))
     };
+}
+
+function updateRoutes(data: Route[], routeToAdd: Route, index: number, paths: String[]): Route[] {
+    for (let route of data) {
+        if (route['path'] == paths[index] && paths.length - 1 == index) {
+            addRoute(route, routeToAdd);
+            break
+        }
+        if (route['path'] == paths[index]) {
+            if (route.children != undefined)
+                updateRoutes(route.children, routeToAdd, index + 1, paths);
+        }
+    }
+    return data
+}
+
+function addRoute(route: Route, routeToAdd: Route) {
+    if (route.children != undefined) {
+        route.children.push(routeToAdd)
+    } else {
+        route.children = [routeToAdd];
+    }
 }
