262320e90b45dcd2fc29224987294af4ea40b0ff
Jakub Kovář <kovar@netgrif.com>
2020-03-06 14:00:46 +0100

    NAE-789 - Add schematic - bare minimum add schematic

---
+++ projects/netgrif-application-engine/schematics/add/index.ts
@@ -0,0 +1,13 @@
+import {
+    Rule,
+    Tree,
+    SchematicContext
+} from '@angular-devkit/schematics';
+import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
+
+export function ngAdd(): Rule {
+    return (tree: Tree, _context: SchematicContext) => {
+        _context.addTask(new NodePackageInstallTask());
+        return tree;
+    };
+}
