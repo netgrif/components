7e97c098f4d9775f48b63156d963d1d94c35a263
Tomáš Kováčik <kovacik@netgrif.com>
2020-03-09 12:52:01 +0100

    NAE-540 - Abstract data field
    - merging

--- projects/netgrif-application-engine/src/lib/data-fields/data-fields.module.ts
+++ projects/netgrif-application-engine/src/lib/data-fields/data-fields.module.ts
@@ -3,31 +3,24 @@ import {CommonModule} from '@angular/common';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {DataFieldTemplateComponent} from './data-field-template/data-field-template.component';
 import {TextFieldComponent} from './text-field/text-field.component';
-import {
-    MatAutocompleteModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatListModule,
-    MatOptionModule,
-    MatRadioModule,
-    MatSelectModule
-} from "@angular/material";
 import { TextareaFieldComponent } from './text-field/textarea-field/textarea-field.component';
 import { SimpleTextFieldComponent } from './text-field/simple-text-field/simple-text-field.component';
 import {EnumerationFieldComponent} from "./enumeration-field/enumeration-field.component";
 import {EnumerationListFieldComponent} from "./enumeration-field/enumeration-list-field/enumeration-list-field.component";
 import {EnumerationSelectFieldComponent} from "./enumeration-field/enumeration-select-field/enumeration-select-field.component";
 import {EnumerationAutocompleteSelectFieldComponent} from "./enumeration-field/enumeration-autocomplete-select-field/enumeration-autocomplete-select-field.component";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {NumberFieldComponent} from "./number-field/number-field.component";
 import {MultichoiceFieldComponent} from "./multichoice-field/multichoice-field.component";
 import {MultichoiceSelectFieldComponent} from "./multichoice-field/multichoice-select-field/multichoice-select-field.component";
 import {MultichoiceListFieldComponent} from "./multichoice-field/multichoice-list-field/multichoice-list-field.component";
-import {MatSlideToggleModule} from "@angular/material";
 import {BooleanFieldComponent} from "./boolean-field/boolean-field.component";
-import {MatDatepickerModule} from "@angular/material/datepicker";
-import {MatNativeDateModule} from "@angular/material/core";
 import {DateFieldComponent} from './date-field/date-field.component';
+import {FileFieldComponent} from "./file-field/file-field.component";
+import {MaterialModule} from "../material/material.module";
+import {FileUploadService} from "./file-field/file-upload.service";
+import {FileDownloadService} from "./file-field/file-download.service";
+import {SideMenuModule} from "../side-menu/side-menu.module";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
     declarations: [
@@ -59,27 +52,10 @@ import {DateFieldComponent} from './date-field/date-field.component';
     imports: [
         CommonModule,
         FlexLayoutModule,
-        MatFormFieldModule,
-        MatInputModule,
-        MatOptionModule,
-        MatFormFieldModule,
-        MatSelectModule,
-        FormsModule,
-        MatListModule,
-        ReactiveFormsModule,
-        MatRadioModule,
-        MatAutocompleteModule,
-        MatInputModule,
-        MatSlideToggleModule,
-        MatDatepickerModule,
-        MatNativeDateModule,
+        MaterialModule,
         AngularResizedEventModule,
-        MatIconModule,
-        MatProgressBarModule,
-        MatButtonModule,
         HttpClientModule,
         SideMenuModule,
-        MatSnackBarModule
     ],
     providers: [
         FileUploadService,
