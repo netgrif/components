78e9bb0c06bff3d11eba3805894ed9a8ee4f5c65
Tomáš Kováčik <kovacik@netgrif.com>
2020-03-09 13:10:50 +0100

    NAE-540 - Abstract Datafield
    - merging

--- projects/netgrif-application-engine/src/public-api.ts
+++ projects/netgrif-application-engine/src/public-api.ts
@@ -9,11 +9,11 @@ export * from './lib/material/material.module';
 export * from './lib/logger/public-api';
 export * from './lib/side-menu/public-api';
 export * from './lib/data-fields/public-api';
-
-/* SERVICES */
-export * from './lib/snack-bar/snack-bar.service';
 export * from './lib/panel/public-api';
 export * from './lib/configuration/public-api';
 export * from './lib/card/public-api';
 export * from './lib/toolbar/public-api';
+
+/* SERVICES */
+export * from './lib/snack-bar/snack-bar.service';
 
