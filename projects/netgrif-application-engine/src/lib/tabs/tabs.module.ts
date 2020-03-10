76ac786a9879bc9caef1542309c839a22ef4b023
Jakub Kovář <kovar@netgrif.com>
2020-03-10 12:53:30 +0100

    NAE-505 - Abstract tab - tab label layouting

--- projects/netgrif-application-engine/src/lib/tabs/tabs.module.ts
+++ projects/netgrif-application-engine/src/lib/tabs/tabs.module.ts
@@ -1,10 +1,11 @@
 import {InjectionToken, NgModule} from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule} from '@angular/common';
 import {TabGroupComponent} from './tab-group/tab-group.component';
-import {MatTabsModule} from "@angular/material/tabs";
-import {MaterialModule} from "../material/material.module";
-import { TabCreationDetectorComponent } from './tab-creation-detector/tab-creation-detector.component';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MaterialModule} from '../material/material.module';
+import {TabCreationDetectorComponent} from './tab-creation-detector/tab-creation-detector.component';
 import {InjectedTabData} from './interfaces';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import {InjectedTabData} from './interfaces';
     imports: [
         CommonModule,
         MatTabsModule,
-        MaterialModule
+        MaterialModule,
+        FlexLayoutModule
     ]
 })
 export class TabsModule { }
