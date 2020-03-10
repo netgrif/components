db5ae80f650df73af83cf2a4a61189a28d68dbd4
Daniel Vaník <vanik@netgrif.com>
2020-03-10 16:02:09 +0100

    NAE-514 - User Service - add exporting action type to public api for user model

--- projects/netgrif-application-engine/src/lib/user/public-api.ts
+++ projects/netgrif-application-engine/src/lib/user/public-api.ts
@@ -4,6 +4,7 @@ export * from './user.module'
 // MODELS
 export * from './models/user'
 export * from './models/role'
+export * from './models/action-type'
 
 //SERVICES
 export * from './services/user.service'
