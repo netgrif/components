9de16063204d128eb695f39e74d3cafd37dfe218
Daniel Vaník <vanik@netgrif.com>
2020-03-10 13:12:32 +0100

    NAE-514 - User Service - change User model

--- projects/netgrif-application-engine/src/lib/user/models/user.ts
+++ projects/netgrif-application-engine/src/lib/user/models/user.ts
@@ -1,12 +1,19 @@
+import Role from "./role";
+
 export class User {
-    id: string;
-    email: string;
-    firstName: string;
-    lastName: string;
-    permissions: {};
-    roles: {};
-    groups: {};
-    preferences?: any;
+    // id: string;
+    // email: string;
+    // firstName: string;
+    // lastName: string;
+    // authorities: String[];
+    // roles: Role[];
+    // groups: String[];
+    // preferences?: any;
+
+    constructor(public id: string, public email: string, public firstName: string, public lastName: string,
+                public groups: String[], public authorities: String[], public preferences: String[], public roles: Role[]) {
+
+    }
 
     get fullname() {
         return this.firstName + ' ' + this.lastName;
