d3d573206edc4b9996fc9a6c461a48ccbd300e43
Jakub Kovář <kovar@netgrif.com>
2020-03-13 16:28:12 +0100

    release4.0.0@alpha - configuration service test

--- projects/netgrif-application-engine/src/lib/configuration/configuration.service.spec.ts
+++ projects/netgrif-application-engine/src/lib/configuration/configuration.service.spec.ts
@@ -3,14 +3,22 @@ import {TestBed} from '@angular/core/testing';
 import {ConfigurationService} from './configuration.service';
 
 describe('ConfigurationService', () => {
-  let service: ConfigurationService;
+    let service: ConfigurationService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ConfigurationService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [{provide: ConfigurationService, useClass: TestConfigurationService}]
+        });
+        service = TestBed.inject(ConfigurationService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
+
+class TestConfigurationService extends ConfigurationService {
+    constructor() {
+        super(null);
+    }
+}
