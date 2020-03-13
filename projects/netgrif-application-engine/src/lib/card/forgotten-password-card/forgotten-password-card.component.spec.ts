f360db3a57b29fb65ae4b3ef3814ece30ea5cbb0
Jakub Kovář <kovar@netgrif.com>
2020-03-13 16:40:30 +0100

    release4.0.0@alpha - partial forgotten password card test

--- projects/netgrif-application-engine/src/lib/card/forgotten-password-card/forgotten-password-card.component.spec.ts
+++ projects/netgrif-application-engine/src/lib/card/forgotten-password-card/forgotten-password-card.component.spec.ts
@@ -1,24 +1,29 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {ForgottenPasswordCardComponent} from './forgotten-password-panel.component';
+import {FormBuilder, FormsModule} from '@angular/forms';
+import {MatButtonModule, MatCardModule, MatFormFieldModule} from '@angular/material';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 describe('ForgottenPasswordPanelComponent', () => {
-  let component: ForgottenPasswordCardComponent ;
-  let fixture: ComponentFixture<ForgottenPasswordCardComponent>;
+    let component: ForgottenPasswordCardComponent;
+    let fixture: ComponentFixture<ForgottenPasswordCardComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [  ForgottenPasswordCardComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [FlexLayoutModule, MatCardModule, MatButtonModule, MatFormFieldModule, FormsModule],
+            declarations: [ForgottenPasswordCardComponent],
+            providers: [FormBuilder]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ForgottenPasswordCardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ForgottenPasswordCardComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
