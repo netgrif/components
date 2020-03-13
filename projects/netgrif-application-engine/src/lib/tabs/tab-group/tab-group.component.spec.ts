0a9e757f2964becacfe3d88886df14476f07277a
Tomáš Kováčik <kovacik@netgrif.com>
2020-03-13 16:55:22 +0100

    release/4.0.0@alpha - fix user service and toolbar test - remove duplicate enable ivy from tsconfig - start fixing test with tab group

--- projects/netgrif-application-engine/src/lib/tabs/tab-group/tab-group.component.spec.ts
+++ projects/netgrif-application-engine/src/lib/tabs/tab-group/tab-group.component.spec.ts
@@ -1,25 +1,48 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { TabGroupComponent } from './tab-group.component';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {TabGroupComponent} from './tab-group.component';
+import {MaterialModule} from '../../material/material.module';
+import {TabCreationDetectorComponent} from '../tab-creation-detector/tab-creation-detector.component';
+import {Component} from '@angular/core';
+import {TabContent} from '@netgrif/application-engine';
+import {ContentComponent} from '../../../../../nae-example-app/src/app/doc/tab-view-example/content/content.component';
 
 describe('AbstractTabComponent', () => {
-  let component: TabGroupComponent;
-  let fixture: ComponentFixture<TabGroupComponent>;
+    let component: TabGroupComponent;
+    let fixture: ComponentFixture<TestWrapperComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ TabGroupComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [MaterialModule],
+            declarations: [TabGroupComponent,
+                TabCreationDetectorComponent,
+                TestWrapperComponent]
+        })
+        .compileComponents();
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TabGroupComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(TestWrapperComponent);
+        component = fixture.debugElement.children[0].componentInstance;
+        fixture.detectChanges();
+    }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
+
+@Component({
+    selector: 'nae-test-wrapper',
+    template: '<nae-tab-group [initialTabs]="tabs"></nae-tab-group>'
+})
+class TestWrapperComponent {
+    tabs: Array<TabContent> = [
+        {
+            label: {
+                text: 'tab title',
+                icon: 'home'
+            },
+            canBeDeleted: false,
+            tabContentComponent: TestWrapperComponent // https://stackoverflow.com/questions/41689468/how-to-shallow-test-a-component-with-an-entrycomponents
+        }
+    ];
+}
+
