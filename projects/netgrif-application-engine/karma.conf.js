83e343e5851066ff773326f409b42c3d4bc5affa
Tomáš Kováčik <kovacik@netgrif.com>
2020-03-17 08:33:22 +0100

    release/4.0.0@alpha
    - another test with chrome

--- projects/netgrif-application-engine/karma.conf.js
+++ projects/netgrif-application-engine/karma.conf.js
@@ -1,8 +1,5 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-const process = require('process');
-process.env.CHROME_BIN = require('puppeteer').executablePath();
-
 
 module.exports = function(config) {
     config.set({
@@ -47,15 +44,7 @@ module.exports = function(config) {
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
-        browserNoActivityTimeout: 180000,
-        captureTimeout: 180000,
-        browsers: ['HeadlessChrome'],
-        customLaunchers:{
-            HeadlessChrome:{
-                base: 'Chrome',
-                flags: ['--headless','--no-sandbox','--remote-debugging=9222']
-            }
-        },
+        browsers: ['ChromeHeadless'],
         singleRun: true,
         restartOnFileChange: true,
     });
