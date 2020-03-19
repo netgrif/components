7f985591bbd2d55c8d8955a3e20290d8d3bcfe66
Jakub Kovář <kovar@netgrif.com>
2020-03-19 12:50:50 +0100

    NAE-544 - Text Field - UserField roles ype changed to string

--- projects/netgrif-application-engine/src/lib/data-fields/user-field/models/user-field.ts
+++ projects/netgrif-application-engine/src/lib/data-fields/user-field/models/user-field.ts
@@ -3,12 +3,12 @@ import {UserValue} from './user-value';
 import {Behavior} from '../../models/behavior';
 
 export class UserField extends DataField<UserValue> {
-    constructor(stringId: string, title: string, behavior: Behavior, value: UserValue, private _roles: Array<UserValue>,
+    constructor(stringId: string, title: string, behavior: Behavior, value: UserValue, private _roles: Array<string>,
                 placeholder?: string, description?: string) {
         super(stringId, title, behavior, placeholder, description, value);
     }
 
-    get roles(): Array<UserValue> {
+    get roles(): Array<string> {
         return this._roles;
     }
 }
