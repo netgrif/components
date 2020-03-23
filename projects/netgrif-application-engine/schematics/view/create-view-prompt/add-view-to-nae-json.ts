f9e54b5d0c23483830ddedd2c72606e5d27bb8fb
Jakub Kovář <kovar@netgrif.com>
2020-03-23 17:01:05 +0100

    NAE-820 - Update view config with schematic - bugfies in add-view-to-nae-json.ts

--- projects/netgrif-application-engine/schematics/view/create-view-prompt/add-view-to-nae-json.ts
+++ projects/netgrif-application-engine/schematics/view/create-view-prompt/add-view-to-nae-json.ts
@@ -1,33 +1,41 @@
 import {
     Rule,
+    SchematicsException,
     Tree
 } from '@angular-devkit/schematics';
 import {CreateViewArguments} from './schema';
 import {getNaeConfiguration} from '../../utilityFunctions';
-import {createAppRoutesMap} from '../viewUtilityFunctions';
 
 
 export function addViewToNaeJson(createViewArguments: CreateViewArguments): Rule {
     return (tree: Tree) => {
-        const routesMap = createAppRoutesMap(tree);
-        if (routesMap.has(createViewArguments.path as string)) {
-            return tree;
-        } else {
-            const naeJsonContent = getNaeConfiguration(tree);
+        const naeJsonContent = getNaeConfiguration(tree);
 
-            const pathSegments = (createViewArguments.path as string).split('/');
-            let parentRoute;
-            if (naeJsonContent.views.routes !== undefined) {
-                parentRoute = naeJsonContent.views.routes;
-                for (let i = 0; i < pathSegments.length - 1; i++) {
-                    parentRoute = parentRoute[pathSegments[i]];
+        const pathSegments = (createViewArguments.path as string).split('/');
+        let parentRoute;
+        if (naeJsonContent.views.routes !== undefined) {
+            parentRoute = naeJsonContent.views;
+            for (let i = 0; i < pathSegments.length - 1; i++) {
+                if (parentRoute.routes !== undefined) {
+                    parentRoute = parentRoute.routes[pathSegments[i]];
+                } else {
+                    // else cannot happen because that would mean this object doesn't have a parent view
+                    // such cases end with error in create-view-prompt schematic
+                    // the if is necessary because of the strict typescript compiler
+                    throw new SchematicsException('Illegal state. View must have all parent views defined.');
                 }
-            } else {
-                naeJsonContent.views.routes = {};
-                parentRoute = naeJsonContent.views.routes;
             }
+        } else {
+            naeJsonContent.views.routes = {};
+            parentRoute = naeJsonContent.views.routes;
+        }
 
-            parentRoute[pathSegments[pathSegments.length - 1]] = {
+        if (parentRoute.routes === undefined) {
+           parentRoute.routes = {};
+        }
+
+        if (parentRoute.routes[pathSegments[pathSegments.length - 1]] === undefined) {
+            parentRoute.routes[pathSegments[pathSegments.length - 1]] = {
                 layout: {
                     name: createViewArguments.viewType as string,
                     params: {}
@@ -37,7 +45,7 @@ export function addViewToNaeJson(createViewArguments: CreateViewArguments): Rule
             };
 
             tree.overwrite('nae.json', JSON.stringify(naeJsonContent, null, 4));
-            return tree;
         }
+        return tree;
     };
 }
