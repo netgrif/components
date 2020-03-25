e75cbd87125c5a71e9b6f4c939a8010d2af90e2c
Jakub Kovář <kovar@netgrif.com>
2020-03-25 11:54:36 +0100

    NAE-552 - Datetime Field - reintroduced md2 changes from example app as they didn't cause the bug that the commit introduced

--- projects/netgrif-application-engine/src/lib/data-fields/date-time-field/md2/datepicker/datepicker-toggle/datepicker-toggle.ts
+++ projects/netgrif-application-engine/src/lib/data-fields/date-time-field/md2/datepicker/datepicker-toggle/datepicker-toggle.ts
@@ -3,20 +3,20 @@ import {Md2DatepickerComponent} from '../datepicker/datepicker';
 
 
 @Component({
+    // tslint:disable-next-line:component-selector
     selector: 'button[md2DatepickerToggle]',
     template: '',
     styleUrls: ['datepicker-toggle.scss'],
-    host: {
-        type: 'button',
-        class: 'md2-datepicker-toggle',
-        'aria-label': 'Open calendar',
-    },
     encapsulation: ViewEncapsulation.None,
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Md2DatepickerToggle<D> {
-    /** Datepicker instance that the button will toggle. */
+export class Md2DatepickerToggleComponent<D> {
+    /** Datepicker instance that the button will toggle. */ // tslint:disable-next-line:no-input-rename
     @Input('md2DatepickerToggle') datepicker: Md2DatepickerComponent;
+
+    @HostBinding('attr.type') type = 'button';
+    @HostBinding('attr.class') hostClass = 'md2-datepicker-toggle';
+    @HostBinding('attr.aria-label') ariaLabel = 'Open calendar';
 
     @HostListener('click', ['$event']) _open(event: Event): void {
         if (this.datepicker) {
