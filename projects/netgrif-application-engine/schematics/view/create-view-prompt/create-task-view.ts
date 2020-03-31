7614c743c7e2264fe7eb4e024360d886c8599e52
Jakub Kovář <kovar@netgrif.com>
2020-03-31 17:09:41 +0200

    NAE-827 - Connect Tab view with case view and task view - task view schematic changed to allow generation of both simple and tabbed task view variants

--- projects/netgrif-application-engine/schematics/view/create-view-prompt/create-task-view.ts
+++ projects/netgrif-application-engine/schematics/view/create-view-prompt/create-task-view.ts
@@ -5,19 +5,29 @@ import {ClassName} from './classes/ClassName';
 import {strings} from '@angular-devkit/core';
 import {addRouteToRoutesJson, addRoutingModuleImport, updateAppModule} from '../view-utility-functions';
 import {ImportToAdd} from './classes/ImportToAdd';
+import {TabbedView} from './tabbed-view';
 
 
-export function createTaskView(tree: Tree, args: CreateViewArguments, addRoute: boolean): Rule {
+export function createTaskView(tree: Tree, args: CreateViewArguments & TabbedView, addRoute: boolean): Rule {
     const projectInfo = getProjectInfo(tree);
     const rules = [];
     const className = new ClassName(args.path as string, 'TaskView');
 
-    rules.push(createFilesFromTemplates('./files/task-view', `${projectInfo.path}/views/${args.path}`, {
+    const templateParams = {
         prefix: projectInfo.projectPrefixDasherized,
         path: className.prefix,
         dasherize: strings.dasherize,
         classify: strings.classify
-    }));
+    };
+
+    const commonPathPrefix = './files/task-view/';
+    const destinationPath = `${projectInfo.path}/views/${args.path}`;
+    rules.push(createFilesFromTemplates(`${commonPathPrefix}common`, destinationPath, templateParams));
+    if (!!args.isTabbed) {
+        rules.push(createFilesFromTemplates(`${commonPathPrefix}tabbed`, destinationPath, templateParams));
+    } else {
+        rules.push(createFilesFromTemplates(`${commonPathPrefix}simple`, destinationPath, templateParams));
+    }
 
     updateAppModule(tree, className.name, className.fileImportPath, [
         new ImportToAdd('FlexModule', '@angular/flex-layout'),
