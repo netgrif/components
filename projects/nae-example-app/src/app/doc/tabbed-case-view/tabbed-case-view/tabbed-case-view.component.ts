bc32ecefc348f52cac2368ffc98f2ad0f58862f1
Jakub Kovář <kovar@netgrif.com>
2020-04-08 19:22:12 +0200

    NAE-525 - Case View Header - added all nets to allowed nets in tabbed case view in demo app

--- projects/nae-example-app/src/app/doc/tabbed-case-view/tabbed-case-view/tabbed-case-view.component.ts
+++ projects/nae-example-app/src/app/doc/tabbed-case-view/tabbed-case-view/tabbed-case-view.component.ts
@@ -5,8 +5,9 @@ import {
     NAE_TAB_DATA,
     TabbedCaseView,
     LoggerService,
-    CaseViewService,
+    CaseViewService, PetriNetReference, PetriNetResourceService,
 } from '@netgrif/application-engine';
+import {ReplaySubject} from 'rxjs';
 
 @Component({
     selector: 'nae-app-tabbed-case-view',
@@ -17,11 +18,17 @@ import {
 export class TabbedCaseViewComponent extends TabbedCaseView implements AfterViewInit {
 
     @ViewChild('header') public caseHeaderComponent: HeaderComponent;
+    public allowedNets$: ReplaySubject<Array<PetriNetReference>>;
 
     constructor(caseViewService: CaseViewService,
                 loggerService: LoggerService,
-                @Inject(NAE_TAB_DATA) injectedTabData: InjectedTabbedCaseViewData) {
+                @Inject(NAE_TAB_DATA) injectedTabData: InjectedTabbedCaseViewData,
+                petriNetResourceService: PetriNetResourceService) {
         super(caseViewService, loggerService, injectedTabData, '{}');
+        this.allowedNets$ = new ReplaySubject<Array<PetriNetReference>>(1);
+        petriNetResourceService.getAll().subscribe(result => {
+            this.allowedNets$.next(result.petriNetReferences);
+        });
     }
 
     ngAfterViewInit(): void {
