f46ee1a7f66a70f28191b86220b9bdf02af3a07c
Tomáš Kováčik <kovacik@netgrif.com>
2020-04-09 11:30:42 +0200

    NAE-838 - Rich text area view
    - fix tests

--- projects/netgrif-application-engine/src/lib/data-fields/text-field/rich-textarea-field/rich-textarea-field.component.spec.ts
+++ projects/netgrif-application-engine/src/lib/data-fields/text-field/rich-textarea-field/rich-textarea-field.component.spec.ts
@@ -1,25 +1,51 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { RichTextareaFieldComponent } from './rich-textarea-field.component';
+import {RichTextareaFieldComponent} from './rich-textarea-field.component';
+import {Component, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {WrappedBoolean} from '../../data-field-template/models/wrapped-boolean';
+import {TextField} from '../models/text-field';
+import {FormControl} from '@angular/forms';
+import {MaterialModule} from '../../../material/material.module';
+import {AngularResizedEventModule} from 'angular-resize-event';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {CovalentModule} from '../../../covalent/covalent.module';
 
 describe('RichTextareaFieldComponent', () => {
-  let component: RichTextareaFieldComponent;
-  let fixture: ComponentFixture<RichTextareaFieldComponent>;
+    let component: RichTextareaFieldComponent;
+    let fixture: ComponentFixture<TestWrapperComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ RichTextareaFieldComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [MaterialModule, AngularResizedEventModule, CovalentModule, BrowserAnimationsModule],
+            declarations: [RichTextareaFieldComponent, TestWrapperComponent],
+            schemas: [CUSTOM_ELEMENTS_SCHEMA]
+        })
+            .compileComponents();
+        fixture = TestBed.createComponent(TestWrapperComponent);
+        component = fixture.debugElement.children[0].componentInstance;
+        fixture.detectChanges();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(RichTextareaFieldComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
+
+@Component({
+    selector: 'nae-test-wrapper',
+    template: `
+        <nae-rich-textarea-field [showLargeLayout]="label"
+                                 [formControlRef]="formControl"
+                                 [textAreaField]="dataField"></nae-rich-textarea-field>`
+})
+class TestWrapperComponent {
+    label = new WrappedBoolean();
+    dataField = new TextField('', '', 'text', {
+        editable: true
+    });
+    formControl = new FormControl();
+
+    constructor() {
+        this.formControl.enable();
+    }
+}
