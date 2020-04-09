5a6d84d95eca7399c0853706cb403a05ae3ca6ec
Jozef Machac <machac@netgrif.com>
2020-04-09 13:00:45 +0200

    NAE-836 - User resource service
    In progress
    - define endpoint

---
+++ projects/netgrif-application-engine/src/lib/resources/engine-endpoint/user-resource-service.ts
@@ -0,0 +1,119 @@
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+import {Params, ResourceProvider} from '../resource-provider.service';
+import {changeType, getResourceAddress} from '../resource-utility-functions';
+import {ConfigurationService} from '../../configuration/configuration.service';
+import {MessageResource} from '../interface/message-resource';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class UserResourceService {
+
+    private SERVER_URL: string;
+
+    protected constructor(protected provider: ResourceProvider, protected _configService: ConfigurationService) {
+        this.SERVER_URL = getResourceAddress('user', this._configService.get().providers.resources);
+    }
+
+    /**
+     * Assign authority to the user
+     * POST
+     * {{baseUrl}}/api/user/{id}/authority/assign
+     */
+    public assignAuthority(userId: string, body: object, params?: Params): Observable<MessageResource> {
+        return this.provider.post$('user/' + userId + '/authority/assign', this.SERVER_URL, body, params).pipe(map(r => changeType(r, undefined)));
+    }
+
+    /**
+     * Assign role to the user
+     * POST
+     * {{baseUrl}}/api/user/{id}/role/assign
+     */
+    public assignRoles(userId: string, body: object, params?: Params): Observable<MessageResource> {
+        return this.provider.post$('user/' + userId + '/role/assign', this.SERVER_URL, body, params).pipe(map(r => changeType(r, undefined)));
+    }
+
+    /**
+     * Get all authorities of the system
+     * GET
+     * {{baseUrl}}/api/user/authority
+     */
+    public getAllAuthorities(): Observable<any> { // TODO OBJECT
+        return this.provider.get$('user/authority', this.SERVER_URL).pipe(map(r => changeType(r, undefined)));
+    }
+
+    /**
+     * Get all users
+     * GET
+     * {{baseUrl}}/api/user
+     */
+    public getAll(params?: Params): Observable<any> { // TODO OBJECT
+        return this.provider.get$('user', this.SERVER_URL, params).pipe(map(r => changeType(r, undefined)));
+    }
+
+    /**
+     * Get all users with specified roles
+     * POST
+     * {{baseUrl}}/api/user/role
+     */
+    public getAllWithRole(body: object, params?: Params): Observable<any> { // TODO OBJECT
+        return this.provider.post$('user/role', this.SERVER_URL, body, params).pipe(map(r => changeType(r, undefined)));
+    }
+
+    /**
+     * Get logged user
+     * GET
+     * {{baseUrl}}/api/user/me
+     */
+    public getLoggedUser(params?: Params): Observable<any> { // TODO OBJECT
+        return this.provider.get$('user/me', this.SERVER_URL, params).pipe(map(r => changeType(r, undefined)));
+    }
+
+    /**
+     * Get user by id
+     * GET
+     * {{baseUrl}}/api/user/{id}
+     */
+    public getUser(userId: string, params?: Params): Observable<any> { // TODO OBJECT
+        return this.provider.get$('user/' + userId, this.SERVER_URL, params).pipe(map(r => changeType(r, undefined)));
+    }
+
+    /**
+     * Get user's preferences
+     * GET
+     * {{baseUrl}}/api/user/preferences
+     */
+    public getPreferences(params?: Params): Observable<any> { // TODO OBJECT
+        return this.provider.get$('user/preferences', this.SERVER_URL, params).pipe(map(r => changeType(r, undefined)));
+    }
+
+    /**
+     * Set user's preferences
+     * POST
+     * {{baseUrl}}/api/user/preferences
+     */
+    public setPreferences(body: object, params?: Params): Observable<MessageResource> {
+        return this.provider.post$('user/preferences', this.SERVER_URL, body, params).pipe(map(r => changeType(r, undefined)));
+    }
+
+    /**
+     * Generic user search
+     * POST
+     * {{baseUrl}}/api/user/search
+     */
+    public search(body: object, params?: Params): Observable<any> { // TODO Object
+        return this.provider.post$('user/search', this.SERVER_URL, body, params).pipe(map(r => changeType(r, undefined)));
+    }
+
+    /**
+     * Update user
+     * POST
+     * {{baseUrl}}/api/user/{id}
+     */
+    public updateUser(userId: string, body: object, params?: Params): Observable<any> { // TODO Object
+        return this.provider.post$('/api/user/' + userId, this.SERVER_URL, body, params).pipe(map(r => changeType(r, undefined)));
+    }
+
+}
