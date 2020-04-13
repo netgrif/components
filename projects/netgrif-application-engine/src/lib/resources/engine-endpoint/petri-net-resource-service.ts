0512fedcb8594e26739461d271d96f5ad23c97ab
Jozef Machac <machac@netgrif.com>
2020-04-14 00:30:44 +0200

    NAE-846 - PetriNet Resource Service
    - merged

--- projects/netgrif-application-engine/src/lib/resources/engine-endpoint/petri-net-resource-service.ts
+++ projects/netgrif-application-engine/src/lib/resources/engine-endpoint/petri-net-resource-service.ts
@@ -29,7 +29,8 @@ export class PetriNetResourceService {
      * {{baseUrl}}/api/petrinet
      */
     public getAll(): Observable<Array<Net>> {
-        return this.provider.get$('petrinet', this.SERVER_URL).pipe(map(r => changeType(r, 'petriNetReferences')));
+        return this.provider.get$('petrinet', this.SERVER_URL)
+            .pipe(map(r => changeType(r, 'petriNetReferences')));
     }
 
     /**
@@ -38,7 +39,8 @@ export class PetriNetResourceService {
      * {{baseUrl}}/api/petrinet/data
      */
     public getDataPetriNet(body: object): Observable<any> {  // TODO: response
-        return this.provider.post$('petrinet/data', this.SERVER_URL, body).pipe(map(r => changeType(r, undefined)));
+        return this.provider.post$('petrinet/data', this.SERVER_URL, body)
+            .pipe(map(r => changeType(r, undefined)));
     }
 
     /**
@@ -46,8 +48,8 @@ export class PetriNetResourceService {
      * GET
      * {{baseUrl}}/api/petrinet/transitions
      */
-    public getPetriNetTranstions(NetID: string): Observable<Array<Transition>> {
-        return this.provider.get$('/petrinet/transitions', this.SERVER_URL, new HttpParams().set('ids', NetID))
+    public getPetriNetTranstions(netId: string): Observable<Array<Transition>> {
+        return this.provider.get$('/petrinet/transitions', this.SERVER_URL, new HttpParams().set('ids', netId))
             .pipe(map(r => changeType(r, 'transitionReferences')));
     }
 
@@ -56,8 +58,8 @@ export class PetriNetResourceService {
      * GET
      * {{baseUrl}}/api/petrinet/{id}/transactions
      */
-    public getPetriNetTransactions(NetID: string): Observable<Array<Transaction>> {
-        return this.provider.get$('/petrinet/' + NetID + '/transactions', this.SERVER_URL)
+    public getPetriNetTransactions(netId: string, params?: Params): Observable<Array<Transaction>> {
+        return this.provider.get$('/petrinet/' + netId + '/transactions', this.SERVER_URL, params)
             .pipe(map(r => changeType(r, 'transactions')));
     }
 
@@ -66,17 +68,19 @@ export class PetriNetResourceService {
      * GET
      * {{baseUrl}}/api/petrinet/{id}/roles
      */
-    public getPetriNetRoles(NetID: string): Observable<Array<NetRole>> {
-        return this.provider.get$('/petrinet/' + NetID + '/roles', this.SERVER_URL)
+    public getPetriNetRoles(netId: string, params?: Params): Observable<Array<NetRole>> {
+        return this.provider.get$('/petrinet/' + netId + '/roles', this.SERVER_URL, params)
             .pipe(map(r => changeType(r, 'processRoles')));
     }
+
     /**
      * get Net File
      * GET
      * {{baseUrl}}/api/petrinet/{netId}/file
      */
     public getNetFile(netId: string, params?: Params): Observable<any> {  // TODO: response
-        return this.provider.get$('petrinet/' + netId + 'file', this.SERVER_URL, params).pipe(map(r => changeType(r, undefined)));
+        return this.provider.get$('petrinet/' + netId + 'file', this.SERVER_URL, params)
+            .pipe(map(r => changeType(r, undefined)));
     }
 
     /**
@@ -85,7 +89,8 @@ export class PetriNetResourceService {
      * {{baseUrl}}/api/petrinet/{identifier}/{version}
      */
     public getOne(identifier: string, version: string, params?: Params): Observable<PetriNet> {
-        return this.provider.get$('petrinet/' + identifier + '/' + version, this.SERVER_URL, params).pipe(map(r => changeType(r, undefined)));
+        return this.provider.get$('petrinet/' + identifier + '/' + version, this.SERVER_URL, params)
+            .pipe(map(r => changeType(r, undefined)));
     }
 
     /**
@@ -94,35 +99,8 @@ export class PetriNetResourceService {
      * {{baseUrl}}/api/petrinet/{id}
      */
     public getOneById(netId: string, params?: Params): Observable<PetriNet> {
-        return this.provider.get$('petrinet/' + netId, this.SERVER_URL, params).pipe(map(r => changeType(r, undefined)));
-    }
-
-    /**
-     * get Roles
-     * GET
-     * {{baseUrl}}/api/petrinet/{netId}/roles
-     */
-    public getRoles(netId: string, params?: Params): Observable<any> { // TODO ..
-        return this.provider.get$('petrinet/' + netId + '/roles', this.SERVER_URL, params).pipe(map(r => changeType(r, undefined)));
-    }
-
-    /**
-     * get Transactions
-     * GET
-     * {{baseUrl}}/api/petrinet/{netId}/transactions
-     */
-    public getTransactions(netId: string, params?: Params): Observable<any> { // TODO ..
-        return this.provider.get$('petrinet/' + netId + '/transactions', this.SERVER_URL, params).pipe(map(r => changeType(r, undefined)));
-    }
-
-
-    /**
-     * get Transition References
-     * GET
-     * {{baseUrl}}/api/petrinet/transitions
-     */
-    public getTransitionReferences(netId: string, params?: Params): Observable<any> { // TODO ..
-        return this.provider.get$('petrinet/transactions', this.SERVER_URL, params).pipe(map(r => changeType(r, undefined)));
+        return this.provider.get$('petrinet/' + netId, this.SERVER_URL, params)
+            .pipe(map(r => changeType(r, undefined)));
     }
 
 
@@ -132,7 +110,8 @@ export class PetriNetResourceService {
      * {{baseUrl}}/api/petrinet/import
      */
     public importPetriNet(body: object, params?: Params): Observable<MessageResource> {
-        return this.provider.post$('petrinet/import', this.SERVER_URL, body, params).pipe(map(r => changeType(r, undefined)));
+        return this.provider.post$('petrinet/import', this.SERVER_URL, body, params)
+            .pipe(map(r => changeType(r, undefined)));
     }
 
 
@@ -142,6 +121,7 @@ export class PetriNetResourceService {
      * {{baseUrl}}/api/petrinet/search
      */
     public searchPetriNets(body: object, params?: Params): Observable<PetriNet> {
-        return this.provider.post$('petrinet/search', this.SERVER_URL, body, params).pipe(map(r => changeType(r, undefined)));
+        return this.provider.post$('petrinet/search', this.SERVER_URL, body, params)
+            .pipe(map(r => changeType(r, undefined)));
     }
 }
