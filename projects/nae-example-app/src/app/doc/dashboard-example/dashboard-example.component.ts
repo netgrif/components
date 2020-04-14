39a2de4facfcef4391800bd49c648d1fc3710285
Jakub Kovář <kovar@netgrif.com>
2020-04-14 16:27:38 +0200

    NAE-495 - Dashboard - example app dashboard reads it's content from app configuration

--- projects/nae-example-app/src/app/doc/dashboard-example/dashboard-example.component.ts
+++ projects/nae-example-app/src/app/doc/dashboard-example/dashboard-example.component.ts
@@ -1,53 +1,17 @@
-import {Component, OnInit} from '@angular/core';
-import {DashboardCardTypes, DashboardParams} from 'netgrif-application-engine';
+import {Component} from '@angular/core';
+import {ConfigurationService, DashboardParams} from 'netgrif-application-engine';
 
 @Component({
     selector: 'nae-app-dashboard-example',
     templateUrl: './dashboard-example.component.html',
     styleUrls: ['./dashboard-example.component.scss']
 })
-export class DashboardExampleComponent implements OnInit {
+export class DashboardExampleComponent {
 
-    public params: DashboardParams = {
-        columns: 4,
-        cards: [{
-            type: DashboardCardTypes.COUNT,
-            title: 'All tasks',
-            resourceType: 'task',
-            filter: '{}',
-            layout: {
-                x: 0,
-                y: 0,
-                rows: 1,
-                cols: 1
-            }
-        }, {
-            type: DashboardCardTypes.IFRAME,
-            url: 'https://netgrif.com/',
-            layout: {
-                x: 2,
-                y: 0,
-                rows: 2,
-                cols: 2
-            }
-        }, {
-            type: DashboardCardTypes.COUNT,
-            title: 'All cases',
-            resourceType: 'case',
-            filter: '{}',
-            layout: {
-                x: 1,
-                y: 1,
-                rows: 1,
-                cols: 1
-            }
-        }]
-    };
+    public params: DashboardParams;
 
-    constructor() {
-    }
-
-    ngOnInit(): void {
+    constructor(private _config: ConfigurationService) {
+        this.params = this._config.getViewByPath('dashboard').layout.params as DashboardParams;
     }
 
 }
