32c7aa24a37cd1f57638ab32b3ec21b93340ef41
Jakub Kovář <kovar@netgrif.com>
2020-04-16 13:05:33 +0200

    NAE-613 - Abstract tab - doc - injection token documented

--- projects/netgrif-application-engine/src/lib/tabs/tab-data-injection-token/tab-data-injection-token.module.ts
+++ projects/netgrif-application-engine/src/lib/tabs/tab-data-injection-token/tab-data-injection-token.module.ts
@@ -12,4 +12,9 @@ import {InjectedTabData} from '../interfaces';
 })
 export class TabDataInjectionTokenModule { }
 
+/**
+ * Injection token for injection of {@link InjectedTabData} into tabs in {@link TabView}.
+ *
+ * Also see {@link TabContent} for more information.
+ */
 export const NAE_TAB_DATA = new InjectionToken<InjectedTabData>('NaeTabData');
