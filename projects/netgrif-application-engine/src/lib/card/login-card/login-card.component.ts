73e9511eebbdbb206bb0ea0aae53674386def16c
Martin Miklovič <miklovic@netgrif.com>
2020-04-17 17:50:39 +0200

    NAE-518 - Login Panel - doc - Added documentation to login card and abstract card

--- projects/netgrif-application-engine/src/lib/card/login-card/login-card.component.ts
+++ projects/netgrif-application-engine/src/lib/card/login-card/login-card.component.ts
@@ -3,6 +3,9 @@ import {FormBuilder} from '@angular/forms';
 import {AbstractCard} from '../abstract-card';
 import {Credentials} from '../../authentication/models/credentials';
 
+/**
+ * Provides a login card.
+ */
 @Component({
     selector: 'nae-login-panel',
     templateUrl: './login-card.component.html',
@@ -10,11 +13,29 @@ import {Credentials} from '../../authentication/models/credentials';
 })
 export class LoginCardComponent extends AbstractCard implements OnInit {
 
-    hidePassword = true;
+    /**
+     * Event emitted when user submit login button
+     */
     @Output() public login: EventEmitter<Credentials>;
+
+    /**
+     * Event emitted when user submit reset password button
+     */
     @Output() public resetPassword: EventEmitter<void>;
+
+    /**
+     * Event emitted when user submit sign-up button
+     */
     @Output() public signUp: EventEmitter<void>;
 
+    /**
+     * @ignore
+     */
+    hidePassword = true;
+
+    /**
+     * @ignore
+     */
     constructor(private fb: FormBuilder) {
         super();
         this.form = fb.group({
@@ -26,9 +47,15 @@ export class LoginCardComponent extends AbstractCard implements OnInit {
         this.signUp = new EventEmitter<void>();
     }
 
+    /**
+     * @ignore
+     */
     ngOnInit() {
     }
 
+    /**
+     * Check if form is valid and then emit entered user login and password values
+     */
     onSubmit() {
         if (!this.form.valid) {
             return;
@@ -36,10 +63,16 @@ export class LoginCardComponent extends AbstractCard implements OnInit {
         this.login.emit({username: this.form.controls['login'].value, password: this.form.controls['password'].value});
     }
 
+    /**
+     * @ignore
+     */
     resetEmit() {
         this.resetPassword.emit();
     }
 
+    /**
+     * @ignore
+     */
     signUpEmit() {
         this.signUp.emit();
     }
