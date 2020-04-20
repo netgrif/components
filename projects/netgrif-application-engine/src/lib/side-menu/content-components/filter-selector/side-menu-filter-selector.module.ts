3708837083b6b56ea483c3bbbd6231493d612178
Jakub Kovář <kovar@netgrif.com>
2020-04-20 14:30:43 +0200

    NAE-533 - Filter Side-menu - fixed bad typography import

--- projects/netgrif-application-engine/src/lib/side-menu/content-components/filter-selector/side-menu-filter-selector.module.ts
+++ projects/netgrif-application-engine/src/lib/side-menu/content-components/filter-selector/side-menu-filter-selector.module.ts
@@ -1,15 +1,17 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FilterSelectorComponent} from './filter-selector.component';
-import {FilterSelectorListItemComponent} from './filter-selector-list-item/filter-selector-list-item.component';
+import {FilterSelectorListItemComponent} from './filter-selector-list/filter-selector-list-item/filter-selector-list-item.component';
 import {MaterialModule} from '../../../material/material.module';
 import {TranslateLibModule} from '../../../translate/translate-lib.module';
+import { FilterSelectorListComponent } from './filter-selector-list/filter-selector-list.component';
 
 
 @NgModule({
     declarations: [
         FilterSelectorComponent,
         FilterSelectorListItemComponent,
+        FilterSelectorListComponent,
     ],
     imports: [
         CommonModule,
