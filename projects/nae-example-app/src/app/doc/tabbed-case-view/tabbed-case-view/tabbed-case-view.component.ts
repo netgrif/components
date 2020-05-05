42266c4f6fe3822ef6996f68b948b65621c620d6
Jakub Kovář <kovar@netgrif.com>
2020-05-05 10:42:36 +0200

    NAE-523 - Search Bar - removed unnecessary code from tabbed views

--- projects/nae-example-app/src/app/doc/tabbed-case-view/tabbed-case-view/tabbed-case-view.component.ts
+++ projects/nae-example-app/src/app/doc/tabbed-case-view/tabbed-case-view/tabbed-case-view.component.ts
@@ -1,21 +1,16 @@
 import {AfterViewInit, Component, Inject, ViewChild} from '@angular/core';
 import {
-    CaseParams,
     CaseViewService,
     CaseViewServiceFactory,
-    ConfigurationService,
     FilterType,
     HeaderComponent,
     InjectedTabbedCaseViewData,
     LoggerService,
     NAE_TAB_DATA,
-    Net,
-    ProcessService,
     SearchService,
     SimpleFilter,
     TabbedCaseView,
 } from '@netgrif/application-engine';
-import {ReplaySubject} from 'rxjs';
 
 const localCaseViewServiceFactory = (factory: CaseViewServiceFactory) => {
     return factory.create('case');
@@ -41,32 +36,11 @@ const searchServiceFactory = () => {
 export class TabbedCaseViewComponent extends TabbedCaseView implements AfterViewInit {
 
     @ViewChild('header') public caseHeaderComponent: HeaderComponent;
-    public allowedNets$: ReplaySubject<Array<Net>>;
-    public params: CaseParams;
 
     constructor(caseViewService: CaseViewService,
                 loggerService: LoggerService,
-                @Inject(NAE_TAB_DATA) injectedTabData: InjectedTabbedCaseViewData,
-                processService: ProcessService,
-                configService: ConfigurationService) {
+                @Inject(NAE_TAB_DATA) injectedTabData: InjectedTabbedCaseViewData) {
         super(caseViewService, loggerService, injectedTabData);
-        this.allowedNets$ = new ReplaySubject<Array<Net>>(1);
-        // TODO 16.4. 2020 initialize allowedNets by filter
-        const view = configService.getViewByPath('<%= webPath %>');
-        if (view && view.layout && view.layout.params) {
-            this.params = view.layout.params as CaseParams;
-            if (this.params.allowedNets !== undefined) {
-                const nets = [];
-                this.params.allowedNets.forEach(netId => {
-                    processService.getNet(netId).subscribe(net => {
-                        nets.push(net);
-                        if (nets.length === this.params.allowedNets.length) {
-                            this.allowedNets$.next(nets);
-                        }
-                    });
-                });
-            }
-        }
     }
 
     ngAfterViewInit(): void {
