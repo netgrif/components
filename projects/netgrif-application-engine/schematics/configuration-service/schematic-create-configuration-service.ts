7efd4bb36335362542713060f432573a483f2c93
Jakub Kovář <kovar@netgrif.com>
2020-05-20 11:37:42 +0200

    NAE-861 - Configuration service without schematic - nae.json is imported directly without any need for additional schematics - reduced the amount of schematics necessary for project ConfigurationService creation to 1 - exported types from lib - removed nae.json from .gitignore as it is now a part of the demo app - moved existing configuration from demo app to nae.json

---
+++ projects/netgrif-application-engine/schematics/configuration-service/schematic-create-configuration-service.ts
@@ -0,0 +1,47 @@
+import {
+    Rule,
+    Tree,
+} from '@angular-devkit/schematics';
+import {strings} from '@angular-devkit/core';
+import {insertImport} from '@schematics/angular/utility/ast-utils';
+import {addProviderToModule} from '../modified-library-functions';
+import {
+    commitChangesToFile,
+    createFilesFromTemplates,
+    createRelativePath,
+    getAppModule,
+    getProjectInfo
+} from '../utility-functions';
+
+
+export function schematicEntryPoint(): Rule {
+    return (tree: Tree) => {
+        const projectInfo = getProjectInfo(tree);
+        const appModule = getAppModule(tree, projectInfo.path);
+
+        const configClassName = `${projectInfo.projectNameClassified}ConfigurationService`;
+        const configFileName = `${projectInfo.projectNameDasherized}-configuration.service`;
+        const configPathFromRoot = `${projectInfo.path}/${configFileName}`;
+
+        // if (tree.exists(configPathFromRoot)) {
+        //     tree.delete(configPathFromRoot);
+        // }
+
+        const changes = addProviderToModule(appModule.sourceFile, appModule.fileEntry.path,
+            configClassName,
+            `./${configFileName}`,
+            `{provide: ConfigurationService, useClass: ${configClassName}}`);
+        changes.push(
+            insertImport(appModule.sourceFile, appModule.fileEntry.path, 'ConfigurationService', '@netgrif/application-engine')
+        );
+        commitChangesToFile(tree, appModule.fileEntry, changes);
+
+        return createFilesFromTemplates('./files', projectInfo.path as string, {
+            classify: strings.classify,
+            dasherize: strings.dasherize,
+            project: projectInfo.projectName,
+            configPath: createRelativePath(configPathFromRoot, 'nae.json')
+        });
+
+    };
+}
