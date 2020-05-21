26c11a00857081b181acb9e15a795d9081be6984
Jakub Kovář <kovar@netgrif.com>
2020-05-21 11:51:19 +0200

    NAE-854 - Refactor of the nae configuration file (nae.json) - fixed schematic build errors for schema.ts file

--- projects/netgrif-application-engine/schematics/view/create-view-prompt/schema.ts
+++ projects/netgrif-application-engine/schematics/view/create-view-prompt/schema.ts
@@ -1,8 +1,8 @@
-import {View as NaeRoute} from '../../../src/lib/configuration/interfaces/schema';
+
 
 export interface CreateViewArguments {
     path: string | undefined;
-    viewType: NaeRoute['layout']['name'] | undefined;
-    layoutParams?: NaeRoute['layout']['params'];
+    viewType: string;
+    layoutParams?: { [k: string]: any; };
     access: { [k: string]: any; } | ('public' | 'private');
 }
