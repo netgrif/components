fc991875f0cd8588ec2d19b343f00e92192b6ced
Jakub Kovář <kovar@netgrif.com>
2020-06-01 16:41:56 +0200

    NAE-800 - User Preference Service - updated preferences model to reflect backend reality

---
+++ projects/netgrif-application-engine/src/lib/user/models/preferences.ts
@@ -0,0 +1,17 @@
+export interface Preferences {
+    locale: string;
+
+    taskFilters: {
+        [viewId: string]: Array<string>
+    };
+
+    caseFilters: {
+        [viewId: string]: Array<string>
+    };
+    /**
+     * Header preferences are strings in format: <petrinet identifier>-<datafield id> or meta-<meta identifier>
+     */
+    caseViewHeaders: {
+        [viewId: string]: Array<string>
+    };
+}
--- projects/netgrif-application-engine/src/lib/user/models/user.ts
+++ projects/netgrif-application-engine/src/lib/user/models/user.ts
@@ -1,5 +1,5 @@
 import Role from './role';
-import {Preferences} from './preferences/preferences';
+import {Preferences} from './preferences';
 
 export class User {
     constructor(
