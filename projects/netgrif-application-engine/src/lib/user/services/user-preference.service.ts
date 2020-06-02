8646daf26e06a46d8a50f7cac1afb39434927c0d
Jakub Kovář <kovar@netgrif.com>
2020-06-02 10:01:03 +0200

    NAE-800 - User Preference Service - changed internal storage of preferences from stream to object

--- projects/netgrif-application-engine/src/lib/user/services/user-preference.service.ts
+++ projects/netgrif-application-engine/src/lib/user/services/user-preference.service.ts
@@ -1,8 +1,6 @@
 import {Injectable} from '@angular/core';
-import {BehaviorSubject, Observable} from 'rxjs';
 import {Preferences} from '../models/preferences';
 import {UserService} from './user.service';
-import {map} from 'rxjs/operators';
 import {UserResourceService} from '../../resources/engine-endpoint/user-resource.service';
 import {LoggerService} from '../../logger/services/logger.service';
 import {SnackBarService} from '../../snack-bar/services/snack-bar.service';
@@ -12,79 +10,64 @@ import {SnackBarService} from '../../snack-bar/services/snack-bar.service';
 })
 export class UserPreferenceService {
 
-    protected _preferences$: BehaviorSubject<Preferences>;
-
-    protected _caseViewHeaders$: Observable<Preferences['caseViewHeaders']>;
-    protected _caseFilters$: Observable<Preferences['caseFilters']>;
-    protected _taskFilters$: Observable<Preferences['taskFilters']>;
+    protected _preferences: Preferences;
 
     constructor(protected _userService: UserService,
                 protected _userResourceService: UserResourceService,
                 protected _logger: LoggerService,
                 protected _snackbar: SnackBarService) {
-        this._preferences$ = new BehaviorSubject<Preferences>(this._emptyPreferences());
+        this._preferences = this._emptyPreferences();
 
         this._userService.user$.subscribe(loggedUser => {
             if (loggedUser.id !== '') {
-                this._userResourceService.getPreferences().subscribe(
-                    prefs => this._preferences$.next(prefs)
+                this._userResourceService.getPreferences().subscribe(prefs => {
+                        this._preferences = prefs;
+                    }
                 );
             } else {
-                this._preferences$.next(this._emptyPreferences());
+                this._preferences = this._emptyPreferences();
             }
         });
-
-        this._caseViewHeaders$ = this._preferences$.pipe(map(prefs => prefs.caseViewHeaders));
-        this._caseFilters$ = this._preferences$.pipe(map(prefs => prefs.caseFilters));
-        this._taskFilters$ = this._preferences$.pipe(map(prefs => prefs.taskFilters));
     }
 
     public saveTaskFilters(viewId: string, value: Array<string>): void {
-        const prefs = this._preferences$.getValue();
-        prefs.taskFilters[viewId] = value;
-        this._preferences$.next(prefs);
+        this._preferences.taskFilters[viewId] = value;
         this._savePreferences();
     }
 
-    public getTaskFilters$(viewId: string): Observable<Preferences['taskFilters']['']> {
-        return this._taskFilters$.pipe(map(taskFilterPrefs => taskFilterPrefs[viewId]));
+    public getTaskFilters(viewId: string): Array<string> | undefined {
+        return this._preferences.taskFilters[viewId];
     }
 
     public saveCaseFilters(viewId: string, value: Array<string>): void {
-        const prefs = this._preferences$.getValue();
-        prefs.caseFilters[viewId] = value;
-        this._preferences$.next(prefs);
+        this._preferences.caseFilters[viewId] = value;
         this._savePreferences();
     }
 
-    public getCaseFilters$(viewId: string): Observable<Preferences['caseFilters']['']> {
-        return this._caseFilters$.pipe(map(caseFilterPrefs => caseFilterPrefs[viewId]));
+    public getCaseFilters(viewId: string): Array<string> | undefined {
+        return this._preferences.caseFilters[viewId];
     }
 
     public saveCaseHeaders(viewId: string, value: Array<string>): void {
-        const prefs = this._preferences$.getValue();
-        prefs.caseViewHeaders[viewId] = value;
-        this._preferences$.next(prefs);
+        this._preferences.caseViewHeaders[viewId] = value;
         this._savePreferences();
     }
 
-    public getCaseHeaders$(viewId: string): Observable<Preferences['caseViewHeaders']['']> {
-        return this._caseViewHeaders$.pipe(map(caseHeaderPrefs => caseHeaderPrefs[viewId]));
+    public getCaseHeaders(viewId: string): Array<string> | undefined {
+        return this._preferences.caseViewHeaders[viewId];
     }
 
     public saveLocale(locale: string): void {
-        const prefs = this._preferences$.getValue();
-        prefs.locale = locale;
-        this._preferences$.next(prefs);
+        this._preferences.locale = locale;
         this._savePreferences();
     }
 
-    public getLocale$(): Observable<Preferences['locale']> {
-        return this._preferences$.pipe(map(prefs => prefs.locale));
+    public getLocale(): string {
+        return this._preferences.locale;
     }
 
     protected _savePreferences(): void {
-        this._userResourceService.setPreferences(this._preferences$.getValue()).subscribe(resultMessage => {
+        this._userResourceService.setPreferences(this._preferences).subscribe(resultMessage => {
             if (!!resultMessage.success) {
                 // TODO i18n
                 this._snackbar.openSuccessSnackBar('User preferences saved successfully');
