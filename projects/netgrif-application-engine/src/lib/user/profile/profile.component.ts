bae201bfca467dc7bb2db0211249f7689aad155c
Jakub Kovář <kovar@netgrif.com>
2020-06-11 10:16:32 +0200

    NAE-800 - User Preference Service - removed unnecessary code from user profile component

--- projects/netgrif-application-engine/src/lib/user/profile/profile.component.ts
+++ projects/netgrif-application-engine/src/lib/user/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import {Component, Injector, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {User} from '../models/user';
 import {UserService} from '../services/user.service';
 
@@ -10,21 +10,16 @@ import {UserService} from '../services/user.service';
 export class ProfileComponent implements OnInit {
 
     @Input() public user: User;
-    public obj;
 
-    constructor(private _injector: Injector) {
-        this.obj = Object;
+    constructor(private _userService: UserService) {
     }
 
     ngOnInit(): void {
         if (!this.user) {
-            const userService: UserService = this._injector.get(UserService);
-            if (userService) {
-                this.user = userService.user;
-                userService.user$.subscribe(user => {
-                    this.user = user;
-                });
-            }
+            this.user = this._userService.user;
+            this._userService.user$.subscribe(user => {
+                this.user = user;
+            });
         }
     }
 
