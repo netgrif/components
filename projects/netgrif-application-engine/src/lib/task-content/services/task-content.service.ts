f8e0f4387a806a44a1ee609d3deb102f9224da98
Jakub Kovář <kovar@netgrif.com>
2020-06-12 13:59:38 +0200

    NAE-876 - Tree Case View - extracted updateFromChangedFields function from task panel component

--- projects/netgrif-application-engine/src/lib/task-content/services/task-content.service.ts
+++ projects/netgrif-application-engine/src/lib/task-content/services/task-content.service.ts
@@ -5,9 +5,14 @@ import {Task} from '../../resources/interface/task';
 import {LoggerService} from '../../logger/services/logger.service';
 import {SnackBarService} from '../../snack-bar/services/snack-bar.service';
 import {TranslateService} from '@ngx-translate/core';
+import {EnumerationField, EnumerationFieldValue} from '../../data-fields/enumeration-field/models/enumeration-field';
+import {MultichoiceField} from '../../data-fields/multichoice-field/models/multichoice-field';
+import {ChangedFields} from '../../data-fields/models/changed-fields';
+import {FieldConverterService} from './field-converter.service';
 
 /**
  * Acts as a communication interface between the Component that renders Task content and it's parent Component.
+ * Also provides some general functionality that is needed when working with task content.
  *
  * Notable example of a parent Component is the {@link TaskPanelComponent}.
  *
@@ -19,7 +24,8 @@ export class TaskContentService {
     protected _task$: Subject<Task>;
     protected _task: Task;
 
-    constructor(protected _snackBarService: SnackBarService,
+    constructor(protected _fieldConverterService: FieldConverterService,
+                protected _snackBarService: SnackBarService,
                 protected _translate: TranslateService,
                 protected _logger: LoggerService) {
         this.$shouldCreate = new Subject<DataGroup[]>();
@@ -83,7 +89,7 @@ export class TaskContentService {
     }
 
     /**
-     * Clears the assignee, start and finish dates from the managed Task.
+     * Clears the assignee, start date and finish date from the managed Task.
      */
     public removeStateData(): void {
         if (this._task) {
@@ -91,5 +97,46 @@ export class TaskContentService {
             this._task.startDate = undefined;
             this._task.finishDate = undefined;
         }
+    }
+
+    /**
+     * Updates the properties of fields in the managed task based on a delta of changes from previous state.
+     * @param chFields object containing the delta of the changes from the previous state
+     */
+    public updateFromChangedFields(chFields: ChangedFields): void {
+        if (!this._task) {
+            return;
+        }
+        this._task.dataGroups.forEach(dataGroup => {
+            dataGroup.fields.forEach(field => {
+                if (chFields[field.stringId]) {
+                    const updatedField = chFields[field.stringId];
+                    Object.keys(updatedField).forEach(key => {
+                        if (key === 'value') {
+                            field.value = this._fieldConverterService.formatValue(field, updatedField[key]);
+                            field.changed = false;
+                        } else if (key === 'behavior' && updatedField.behavior[this._task.transitionId]) {
+                            field.behavior = updatedField.behavior[this._task.transitionId];
+                        } else if (key === 'choices') {
+                            const newChoices: EnumerationFieldValue[] = [];
+                            if (updatedField.choices instanceof Array) {
+                                updatedField.choices.forEach(it => {
+                                    newChoices.push({key: it, value: it} as EnumerationFieldValue);
+                                });
+                            } else {
+                                Object.keys(updatedField.choices).forEach(choice => {
+                                    newChoices.push({key: choice, value: updatedField.choices[key]} as EnumerationFieldValue);
+                                });
+                            }
+                            (field as EnumerationField | MultichoiceField).choices = newChoices;
+                        } else {
+                            field[key] = updatedField[key];
+                        }
+                        field.update();
+                    });
+                }
+            });
+        });
+        this.$shouldCreate.next(this._task.dataGroups);
     }
 }
