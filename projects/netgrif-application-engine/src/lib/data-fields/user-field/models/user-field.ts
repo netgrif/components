a27f036e5f2a70b8181a2252260d912e726b3a88
Daniel Vaník <vanik@netgrif.com>
2020-07-04 10:12:11 +0200

    NAE-878 - UserAssignComponent With Pagination - merge rc and resolve conflicts

--- projects/netgrif-application-engine/src/lib/data-fields/user-field/models/user-field.ts
+++ projects/netgrif-application-engine/src/lib/data-fields/user-field/models/user-field.ts
@@ -2,7 +2,7 @@ import {DataField} from '../../models/abstract-data-field';
 import {UserValue} from './user-value';
 import {Behavior} from '../../models/behavior';
 import {Layout} from '../../models/layout';
-import Role from '../../../user/models/role';
+import {Role} from '../../../user/models/role';
 
 export class UserField extends DataField<UserValue> {
     constructor(stringId: string, title: string, behavior: Behavior, value: UserValue, private _roles: Array<Role>,
