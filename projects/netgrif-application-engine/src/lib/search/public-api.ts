c4dfb4d59cfd219e7ad449ecb3379c3c9924a28d
Jakub Kovář <kovar@netgrif.com>
2020-07-15 15:32:32 +0200

    NAE-909 - Search mode on headers - in-progress header search input rework

--- projects/netgrif-application-engine/src/lib/search/public-api.ts
+++ projects/netgrif-application-engine/src/lib/search/public-api.ts
@@ -24,6 +24,7 @@ export * from './models/category/case/case-role';
 export * from './models/category/case/case-task';
 export * from './models/category/case/case-creation-date';
 export * from './models/category/case/case-dataset';
+export * from './models/category/case/case-simple-dataset';
 export * from './models/category/task/task-assignee';
 export * from './models/category/task/task-process';
 export * from './models/category/task/task-role';
