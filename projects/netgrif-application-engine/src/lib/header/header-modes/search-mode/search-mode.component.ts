a7b3673d79511e9da6cd9c8840ce6057979e73b8
Jakub Kovář <kovar@netgrif.com>
2020-07-17 13:03:10 +0200

    NAE-909 - Search mode on headers - replaced callbacks with formcontrols with debounce time

--- projects/netgrif-application-engine/src/lib/header/header-modes/search-mode/search-mode.component.ts
+++ projects/netgrif-application-engine/src/lib/header/header-modes/search-mode/search-mode.component.ts
@@ -1,12 +1,21 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {AbstractHeaderService} from '../../abstract-header-service';
+import {FormControl} from '@angular/forms';
+import {debounceTime} from 'rxjs/operators';
 
 @Component({
     selector: 'nae-search-mode',
     templateUrl: './search-mode.component.html',
     styleUrls: ['./search-mode.component.scss']
 })
-export class SearchModeComponent {
+export class SearchModeComponent implements OnInit {
+
+    /**
+     * The time that must elapse since last keypress in search input before a search request is sent
+     */
+    private SEARCH_DEBOUNCE_TIME = 200;
+
+    public formControls: Array<FormControl> = [];
 
     @Input()
     public headerService: AbstractHeaderService;
@@ -14,7 +23,23 @@ export class SearchModeComponent {
     constructor() {
     }
 
-    public headerSearchInputChanged(columnIndex: number, searchInput: any) {
-        this.headerService.headerSearchInputChanged(columnIndex, searchInput);
+    ngOnInit() {
+        this.headerService.headerColumnCount$.subscribe(newCount => this.updateHeaderCount(newCount));
+    }
+
+    private updateHeaderCount(newCount: number): void {
+        if (newCount < this.formControls.length) {
+            this.formControls = this.formControls.slice(0, newCount);
+            return;
+        }
+
+        while (this.formControls.length < newCount) {
+            const formControl = new FormControl();
+            const i = this.formControls.length;
+            formControl.valueChanges.pipe(debounceTime(this.SEARCH_DEBOUNCE_TIME)).subscribe(value => {
+                this.headerService.headerSearchInputChanged(i, value);
+            });
+            this.formControls.push(formControl);
+        }
     }
 }
