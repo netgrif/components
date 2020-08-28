2263132f42a85e8c6b113c350d22c136f634a75d
Jakub Kovář <kovar@netgrif.com>
2020-08-28 07:36:11 +0200

    NAE-969 - Task event notifications
    - export task event enum

--- projects/netgrif-application-engine/src/lib/task-content/public-api.ts
+++ projects/netgrif-application-engine/src/lib/task-content/public-api.ts
@@ -8,6 +8,7 @@ export * from './services/unlimited-task-content.service';
 /* MODELS */
 export * from './model/policy';
 export * from './model/task-event-notification';
+export * from './model/task-event';
 
 /* MODULES */
 export * from './task-content.module';
