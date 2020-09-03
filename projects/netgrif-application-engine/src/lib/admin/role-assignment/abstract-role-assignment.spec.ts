d723b23efe6804f86b36a91c013706fe0ab8c943
Tomáš Kováčik <kovacik@netgrif.com>
2020-09-03 07:52:12 +0200

    NAE-898 - Repair unit tests
    - write test for admin part in nae lib

---
+++ projects/netgrif-application-engine/src/lib/admin/role-assignment/abstract-role-assignment.spec.ts
@@ -0,0 +1,72 @@
+import {AbstractRoleAssignment} from './abstract-role-assignment';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {MaterialModule} from '../../material/material.module';
+import {TranslateLibModule} from '../../translate/translate-lib.module';
+import {ConfigurationService} from '../../configuration/configuration.service';
+import {TestConfigurationService} from '../../utility/tests/test-config';
+import {Component, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {BrowserDynamicTestingModule} from '@angular/platform-browser-dynamic/testing';
+import {SnackBarModule} from '../../snack-bar/snack-bar.module';
+import {ErrorSnackBarComponent} from '../../snack-bar/components/error-snack-bar/error-snack-bar.component';
+import {SuccessSnackBarComponent} from '../../snack-bar/components/success-snack-bar/success-snack-bar.component';
+import {RoleAssignmentService} from './services/role-assignment.service';
+import {UserService} from '../../user/services/user.service';
+import {AuthenticationMethodService} from '../../authentication/services/authentication-method.service';
+import {RouterTestingModule} from '@angular/router/testing';
+import {NullAuthenticationService} from '../../authentication/services/methods/null-authentication/null-authentication.service';
+
+describe('AbstractRoleAssignment', () => {
+    let component: TestRoleAssignmentComponent;
+    let fixture: ComponentFixture<TestRoleAssignmentComponent>;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                NoopAnimationsModule,
+                HttpClientTestingModule,
+                MaterialModule,
+                TranslateLibModule,
+                SnackBarModule,
+                RouterTestingModule.withRoutes([])
+            ],
+            providers: [
+                {provide: ConfigurationService, useClass: TestConfigurationService},
+                {provide: AuthenticationMethodService, useClass: NullAuthenticationService},
+                RoleAssignmentService
+            ],
+            schemas: [CUSTOM_ELEMENTS_SCHEMA],
+            declarations: [
+                TestRoleAssignmentComponent
+            ],
+        }).overrideModule(BrowserDynamicTestingModule, {
+            set: {
+                entryComponents: [
+                    ErrorSnackBarComponent,
+                    SuccessSnackBarComponent
+                ]
+            }
+        }).compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(TestRoleAssignmentComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
+
+@Component({
+    selector: 'nae-role-assignment',
+    template: '',
+})
+export class TestRoleAssignmentComponent extends AbstractRoleAssignment {
+    constructor(protected _service: RoleAssignmentService, protected _userService: UserService) {
+        super(_service, _userService);
+    }
+}
