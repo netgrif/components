ee95ee4888017bbdcc78c8c20498904f61602c67
Jakub Kovář <kovar@netgrif.com>
2020-09-04 15:36:58 +0200

    NAE-968 - Filter changes ignored
    - fixed test file name

--- projects/netgrif-application-engine/src/lib/view/abstract/page-load-request-context.spec.ts
+++ projects/netgrif-application-engine/src/lib/view/abstract/page-load-request-context.spec.ts
@@ -1,7 +1,7 @@
 import {PageLoadRequestContext} from './page-load-request-context';
 import {SimpleFilter} from '../../filter/models/simple-filter';
 
-describe('TaskPageLoadRequestContext', () => {
+describe('PageLoadRequestContext', () => {
     it('should create an instance', () => {
         expect(new PageLoadRequestContext(SimpleFilter.emptyTaskFilter(),
             {size: 1, totalElements: 1, totalPages: 1, number: 0})).toBeTruthy();
