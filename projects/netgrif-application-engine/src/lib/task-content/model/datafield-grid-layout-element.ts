90967b2567667e5b6233e631b07d50f88b3dca14
Jakub Kovář <kovar@netgrif.com>
2020-09-07 13:31:27 +0200

    NAE-965 - Grid layout container
    - fixed types of grid element classes

--- projects/netgrif-application-engine/src/lib/task-content/model/datafield-grid-layout-element.ts
+++ projects/netgrif-application-engine/src/lib/task-content/model/datafield-grid-layout-element.ts
@@ -1,7 +1,6 @@
 import {DataField} from '../../data-fields/models/abstract-data-field';
 import {GridElementWithItem} from '../../utility/grid-layout/model/grid-element-with-item';
 
-export interface DatafieldGridLayoutElement extends GridElementWithItem {
-    item: DataField<any>;
+export interface DatafieldGridLayoutElement extends GridElementWithItem<DataField<any>> {
     title?: string;
 }
