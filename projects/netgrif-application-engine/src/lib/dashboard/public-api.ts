66c7cdf9176bc611e5b7074ae353a82deec68334
Štefan Renczés <renczes@netgrif.com>
2020-09-14 15:09:11 +0200

    - commiting shelve

--- projects/netgrif-application-engine/src/lib/dashboard/public-api.ts
+++ projects/netgrif-application-engine/src/lib/dashboard/public-api.ts
@@ -8,6 +8,7 @@ export * from './cards/model/dashboard-card';
 export * from './cards/model/count-card';
 export * from './cards/model/graph-card';
 export * from './cards/model/iframe-card';
+export * from './cards/model/custom-card';
 
 /* ENUMS */
 export * from './cards/model/dashboard-card-types';
@@ -15,4 +16,5 @@ export * from './cards/model/dashboard-card-types';
 /* COMPONENT */
 export * from './cards/count-card/abstract-count-card';
 export * from './cards/iframe-card/abstract-iframe-card';
+export * from './cards/custom-card/abstract-custom-card';
 export * from './dashboard-content/abstract-dashboard-content';
