1069f3a3bc964712c4e1b13a14a7566a7b93b93d
Jakub Kovář <kovar@netgrif.com>
2020-09-16 09:42:33 +0200

    NAE-965 - Grid layout container
    - layout algorithm tests

--- projects/netgrif-application-engine/src/lib/task-content/task-content/abstract-task-content.component.spec.ts
+++ projects/netgrif-application-engine/src/lib/task-content/task-content/abstract-task-content.component.spec.ts
@@ -15,16 +15,21 @@ import {PaperViewService} from '../../navigation/quick-panel/components/paper-vi
 import {LoggerService} from '../../logger/services/logger.service';
 import {TaskEventService} from '../services/task-event.service';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
-import {DataGroupAlignment} from '../../resources/interface/data-groups';
+import {DataGroup, DataGroupAlignment} from '../../resources/interface/data-groups';
 import {DataGroupLayoutType} from '../../resources/interface/data-group-layout';
 import {IncrementingCounter} from '../../utility/incrementing-counter';
 import {BooleanField} from '../../data-fields/boolean-field/models/boolean-field';
 import {UnlimitedTaskContentService} from '../services/unlimited-task-content.service';
 import {createMockTask} from '../../utility/tests/utility/create-mock-task';
+import {DataField} from '../../data-fields/models/abstract-data-field';
+import {GridLayout} from '../../utility/grid-layout/model/grid-element';
+import {TemplateAppearance} from '../../data-fields/models/template-appearance';
+import {MaterialAppearance} from '../../data-fields/models/material-appearance';
 
 describe('AbstractTaskContentComponent', () => {
     let component: TestTaskComponent;
     let fixture: ComponentFixture<TestTaskComponent>;
+    let taskContentService: TaskContentService;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
@@ -49,7 +54,8 @@ describe('AbstractTaskContentComponent', () => {
         component = fixture.componentInstance;
         fixture.detectChanges();
 
-        TestBed.inject(TaskContentService).task = createMockTask();
+        taskContentService = TestBed.inject(TaskContentService);
+        taskContentService.task = createMockTask();
     }));
 
     it('should create', () => {
@@ -60,17 +66,9 @@ describe('AbstractTaskContentComponent', () => {
         expect(component.dataSource).toBeUndefined();
         expect(component.gridAreas).toBeUndefined();
 
-        component.computeLayoutData([{
-            fields: [createField()],
-            title: 'hello world',
-            alignment: DataGroupAlignment.START,
-            stretch: true,
-            layout: {
-                type: DataGroupLayoutType.LEGACY,
-                rows: 0,
-                cols: 4
-            }
-        }]);
+        component.computeLayoutData([
+            createDataGroup([createField()], 'hello world', DataGroupAlignment.START, DataGroupLayoutType.LEGACY, true)
+        ]);
 
         expect(component.dataSource).toBeTruthy();
         expect(Array.isArray(component.dataSource)).toBeTrue();
@@ -82,12 +80,188 @@ describe('AbstractTaskContentComponent', () => {
         expect(grid[0].length).toEqual(4);
         expect(grid[0][0]).toEqual('group0');
         expect(grid[0][3]).toEqual('group0');
-        expect(grid[1][0]).toEqual('f0');
-        expect(grid[1][3]).toEqual('f0');
+        expect(grid[1][0].startsWith('f')).toBeTrue();
+        expect(grid[1][3].startsWith('f')).toBeTrue();
     });
 
-    it('should test legacy layout algorithm start', () => {
-        component.computeLayoutData([]);
+    it('should hide data group title', () => {
+        expect(component.dataSource).toBeUndefined();
+        expect(component.gridAreas).toBeUndefined();
+
+        component.computeLayoutData([
+            createDataGroup([createField(false)], 'hello world'),
+            createDataGroup([createField()])
+        ]);
+
+        expect(component.dataSource).toBeTruthy();
+        expect(Array.isArray(component.dataSource)).toBeTrue();
+        expect(component.dataSource.length).toEqual(3);
+
+        expect(component.gridAreas).toBeTruthy();
+        const grid = transformStringToGrid(component.gridAreas);
+        expect(grid.length).toEqual(1);
+        expect(grid[0].length).toEqual(4);
+        expect(grid[0][0].startsWith('f')).toBeTrue();
+        expect(grid[0][1].startsWith('f')).toBeTrue();
+        expect(grid[0][2]).toEqual('blank0');
+        expect(grid[0][3]).toEqual('blank1');
+    });
+
+    it('should not have input side effects', () => {
+        const input = [
+            createDataGroup([createField(false)], 'hello world'),
+            createDataGroup([createField()])
+        ];
+
+        component.computeLayoutData(input);
+
+        expect(input.length).toEqual(2);
+        expect(input[0].fields.length).toEqual(1);
+        expect(input[1].fields.length).toEqual(1);
+    });
+
+    it('legacy layout should work', () => {
+        expect(component.dataSource).toBeUndefined();
+        expect(component.gridAreas).toBeUndefined();
+
+        component.computeLayoutData([
+            createDataGroup([createField(), createField(), createField(false), createField()], 'title', DataGroupAlignment.START),
+            createDataGroup([createField(), createField(), createField(false), createField()], 'title', DataGroupAlignment.CENTER),
+            createDataGroup([createField(), createField(), createField(false), createField()], 'title', DataGroupAlignment.END)
+        ]);
+
+        expect(component.dataSource).toBeTruthy();
+        expect(Array.isArray(component.dataSource)).toBeTrue();
+        expect(component.dataSource.length).toEqual(18);
+
+        expect(component.gridAreas).toBeTruthy();
+        const grid = transformStringToGrid(component.gridAreas);
+        expect(grid.length).toEqual(9);
+        expect(grid[0].length).toEqual(4);
+
+        expect(grid[0][0].startsWith('group')).toBeTrue();
+        expect(grid[1][0].startsWith('f')).toBeTrue();
+        expect(grid[1][2].startsWith('f')).toBeTrue();
+        expect(grid[1][0] !== grid[1][2]).toBeTrue();
+        expect(grid[2][0].startsWith('f')).toBeTrue();
+        expect(grid[2][2].startsWith('blank')).toBeTrue();
+
+        expect(grid[3][0].startsWith('group')).toBeTrue();
+        expect(grid[4][0].startsWith('f')).toBeTrue();
+        expect(grid[4][2].startsWith('f')).toBeTrue();
+        expect(grid[4][0] !== grid[4][2]).toBeTrue();
+        expect(grid[5][0].startsWith('blank')).toBeTrue();
+        expect(grid[5][1].startsWith('f')).toBeTrue();
+        expect(grid[5][3].startsWith('blank')).toBeTrue();
+
+        expect(grid[6][0].startsWith('group')).toBeTrue();
+        expect(grid[7][0].startsWith('f')).toBeTrue();
+        expect(grid[7][2].startsWith('f')).toBeTrue();
+        expect(grid[7][0] !== grid[7][2]).toBeTrue();
+        expect(grid[8][1].startsWith('blank')).toBeTrue();
+        expect(grid[8][2].startsWith('f')).toBeTrue();
+    });
+
+    it('flow layout should work', () => {
+        expect(component.dataSource).toBeUndefined();
+        expect(component.gridAreas).toBeUndefined();
+
+        component.computeLayoutData([
+            createDataGroup([createField(), createField(), createField(), createField(), createField(false), createField()],
+                'title', DataGroupAlignment.START, DataGroupLayoutType.FLOW),
+            createDataGroup([createField(), createField(), createField(), createField(), createField(false), createField()],
+                'title', DataGroupAlignment.CENTER, DataGroupLayoutType.FLOW),
+            createDataGroup([createField(), createField(), createField(), createField(), createField(false), createField()],
+                'title', DataGroupAlignment.END, DataGroupLayoutType.FLOW)
+        ]);
+
+        expect(component.dataSource).toBeTruthy();
+        expect(Array.isArray(component.dataSource)).toBeTrue();
+        expect(component.dataSource.length).toEqual(27);
+
+        expect(component.gridAreas).toBeTruthy();
+        const grid = transformStringToGrid(component.gridAreas);
+        expect(grid.length).toEqual(9);
+        expect(grid[0].length).toEqual(4);
+
+        expect(grid[0][0].startsWith('group')).toBeTrue();
+        expect(grid[1][0].startsWith('f')).toBeTrue();
+        expect(grid[1][1].startsWith('f')).toBeTrue();
+        expect(grid[1][2].startsWith('f')).toBeTrue();
+        expect(grid[1][3].startsWith('f')).toBeTrue();
+        expect(grid[1][0] !== grid[1][1]).toBeTrue();
+        expect(grid[1][1] !== grid[1][2]).toBeTrue();
+        expect(grid[1][2] !== grid[1][3]).toBeTrue();
+        expect(grid[2][0].startsWith('f')).toBeTrue();
+        expect(grid[2][1].startsWith('blank')).toBeTrue();
+
+        expect(grid[3][0].startsWith('group')).toBeTrue();
+        expect(grid[4][0].startsWith('f')).toBeTrue();
+        expect(grid[4][1].startsWith('f')).toBeTrue();
+        expect(grid[4][2].startsWith('f')).toBeTrue();
+        expect(grid[4][3].startsWith('f')).toBeTrue();
+        expect(grid[4][0] !== grid[4][1]).toBeTrue();
+        expect(grid[4][1] !== grid[4][2]).toBeTrue();
+        expect(grid[4][2] !== grid[4][3]).toBeTrue();
+        expect(grid[5][0].startsWith('blank')).toBeTrue();
+        expect(grid[5][1].startsWith('f')).toBeTrue();
+        expect(grid[5][2].startsWith('blank')).toBeTrue();
+
+        expect(grid[6][0].startsWith('group')).toBeTrue();
+        expect(grid[7][0].startsWith('f')).toBeTrue();
+        expect(grid[7][1].startsWith('f')).toBeTrue();
+        expect(grid[7][2].startsWith('f')).toBeTrue();
+        expect(grid[7][3].startsWith('f')).toBeTrue();
+        expect(grid[7][0] !== grid[7][1]).toBeTrue();
+        expect(grid[7][1] !== grid[7][2]).toBeTrue();
+        expect(grid[7][2] !== grid[7][3]).toBeTrue();
+        expect(grid[8][2].startsWith('blank')).toBeTrue();
+        expect(grid[8][3].startsWith('f')).toBeTrue();
+    });
+
+    it('grid layout should work', () => {
+        expect(component.dataSource).toBeUndefined();
+        expect(component.gridAreas).toBeUndefined();
+
+        component.computeLayoutData([
+            createDataGroup([
+                    createField(true, {x: 0, y: 0, rows: 1, cols: 1}),
+                    createField(false, {x: 1, y: 0, rows: 1, cols: 3}),
+                    createField(true, {x: 2, y: 1, rows: 1, cols: 2}),
+                    createField(true, {x: 1, y: 2, rows: 1, cols: 2}),
+                    createField(true, {x: 0, y: 3, rows: 2, cols: 1})],
+                '', DataGroupAlignment.START, DataGroupLayoutType.GRID)
+        ]);
+
+        expect(component.dataSource).toBeTruthy();
+        expect(Array.isArray(component.dataSource)).toBeTrue();
+        expect(component.dataSource.length).toEqual(9);
+
+        expect(component.gridAreas).toBeTruthy();
+        const grid = transformStringToGrid(component.gridAreas);
+        expect(grid.length).toEqual(3);
+        expect(grid[0].length).toEqual(4);
+
+        expect(grid[0][0].startsWith('f')).toBeTrue();
+        expect(grid[0][1].startsWith('blank')).toBeTrue();
+        expect(grid[0][2].startsWith('f')).toBeTrue();
+        expect(grid[0][3].startsWith('f')).toBeTrue();
+        expect(grid[0][2] === grid[0][3]).toBeTrue();
+
+        expect(grid[1][0].startsWith('f')).toBeTrue();
+        expect(grid[2][0].startsWith('f')).toBeTrue();
+        expect(grid[1][0] === grid[2][0]).toBeTrue();
+
+        expect(grid[1][1].startsWith('f')).toBeTrue();
+        expect(grid[1][2].startsWith('f')).toBeTrue();
+        expect(grid[1][1] === grid[1][2]).toBeTrue();
+
+        expect(grid[0][0] !== grid[0][2]).toBeTrue();
+        expect(grid[0][2] !== grid[1][2]).toBeTrue();
+        expect(grid[1][0] !== grid[1][1]).toBeTrue();
+
+        expect(grid[1][3].startsWith('blank')).toBeTrue();
+        expect(grid[2][1].startsWith('blank')).toBeTrue();
     });
 
     afterEach(() => {
@@ -95,11 +269,36 @@ describe('AbstractTaskContentComponent', () => {
     });
 });
 
+function createDataGroup(fields: Array<DataField<unknown>>,
+                         title?: string,
+                         alignment = DataGroupAlignment.START,
+                         layoutType = DataGroupLayoutType.LEGACY,
+                         stretch = false,
+                         cols = 4): DataGroup {
+    return {
+        fields,
+        title,
+        alignment,
+        stretch,
+        layout: {
+            type: layoutType,
+            rows: 0,
+            cols
+        }
+    };
+}
+
 const counter = new IncrementingCounter();
 
-function createField(visible = true): BooleanField {
+function createField(visible = true, layout: GridLayout = {x: 0, y: 0, rows: 0, cols: 0}): BooleanField {
     const b = visible ? {visible: true} : {hidden: true};
-    return new BooleanField('f' + counter.next(), 'title', false, b);
+    const l = {
+        ...layout,
+        template: TemplateAppearance.MATERIAL,
+        appearance: MaterialAppearance.OUTLINE,
+        offset: 0
+    };
+    return new BooleanField('f' + counter.next(), 'title', false, b, '', '', l);
 }
 
 function transformStringToGrid(gridString: string): Array<Array<string>> {
