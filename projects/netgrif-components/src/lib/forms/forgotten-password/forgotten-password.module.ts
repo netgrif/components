ebd706ad7f9ca4844ce986cfa9bd4b48922f2622
Jakub Kovář <kovar@netgrif.com>
2020-11-02 09:20:26 +0100

    NAE-1075 - Recover account component
    - fixed bad imports

--- projects/netgrif-components/src/lib/forms/forgotten-password/forgotten-password.module.ts
+++ projects/netgrif-components/src/lib/forms/forgotten-password/forgotten-password.module.ts
@@ -1,7 +1,7 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ForgottenPasswordComponent} from './forgotten-password.component';
-import {MaterialModule, TranslateLibModule} from 'netgrif-application-engine';
+import {MaterialModule, TranslateLibModule} from '@netgrif/application-engine';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {ReactiveFormsModule} from '@angular/forms';
 
