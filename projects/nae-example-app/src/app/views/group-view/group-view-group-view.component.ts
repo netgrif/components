6acc9488dc0a092c13a15609503681826dc0b92e
Štefan Renczés <renczes@netgrif.com>
2020-11-05 06:55:28 +0100

    NAE-1009 - Group view schematic
    - added generated view

---
+++ projects/nae-example-app/src/app/views/group-view/group-view-group-view.component.ts
@@ -0,0 +1,48 @@
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {
+    AbstractTaskView,
+    ConfigTaskViewServiceFactory, NextGroupService,
+    SearchService,
+    SimpleFilter,
+    TaskViewService
+} from '@netgrif/application-engine';
+import {
+    HeaderComponent,
+} from '@netgrif/components';
+
+const localTaskViewServiceFactory = (factory: ConfigTaskViewServiceFactory) => {
+    return factory.create('group-view');
+};
+
+const searchServiceFactory = (nextGroupService: NextGroupService) => {
+    return new SearchService(SimpleFilter.fromTaskQuery({case: {id: nextGroupService.groupOfUser.stringId}}));
+};
+
+@Component({
+    selector: 'nae-app-group-view-group-view',
+    templateUrl: './group-view-group-view.component.html',
+    styleUrls: ['./group-view-group-view.component.scss'],
+    providers: [
+        ConfigTaskViewServiceFactory,
+        {   provide: SearchService,
+            useFactory: searchServiceFactory,
+            deps: [NextGroupService]
+        },
+        {   provide: TaskViewService,
+            useFactory: localTaskViewServiceFactory,
+            deps: [ConfigTaskViewServiceFactory]},
+    ]
+})
+export class GroupViewGroupViewComponent extends AbstractTaskView implements AfterViewInit {
+
+    @ViewChild('header') public taskHeaderComponent: HeaderComponent;
+
+    constructor(taskViewService: TaskViewService) {
+        super(taskViewService);
+
+    }
+
+    ngAfterViewInit(): void {
+        this.initializeHeader(this.taskHeaderComponent);
+    }
+}
