a7047bacf704ef8a9aa5f049d2d67b1ab74c8734
Štefan Renczés <renczes@netgrif.com>
2020-11-12 09:04:58 +0100

    NAE-1019 - Currency view for number field
    - corrected findings according to comments in PR
    - logic replaced to abstract component
    - component tag is now checked, whether it is undefined or not
    - used ngSwitchDefault

--- projects/netgrif-application-engine/src/lib/data-fields/number-field/currency-number-field/abstract-currency-number-field.component.ts
+++ projects/netgrif-application-engine/src/lib/data-fields/number-field/currency-number-field/abstract-currency-number-field.component.ts
@@ -3,12 +3,54 @@ import {WrappedBoolean} from '../../data-field-template/models/wrapped-boolean';
 import {TranslateService} from '@ngx-translate/core';
 import {AbstractNumberFieldComponent} from '../abstract-number-field.component';
 import {FormControl} from '@angular/forms';
+import {CurrencyPipe, getCurrencySymbol} from '@angular/common';
+
 
 export abstract class AbstractCurrencyNumberFieldComponent extends AbstractNumberFieldComponent implements OnInit {
     @Input() showLargeLayout: WrappedBoolean;
     @Input() formControlRef: FormControl;
 
-    constructor(private _translateService: TranslateService) {
+    numberValue: number;
+    @Input() transformedValue: string;
+    fieldType: string;
+
+    constructor(protected _currencyPipe: CurrencyPipe, private _translateService: TranslateService) {
         super(_translateService);
+    }
+
+    ngOnInit() {
+        super.ngOnInit();
+        if (this.dataField.value !== undefined) {
+            this.transformedValue = this.transformCurrency(this.dataField.value.toString());
+            this.numberValue = parseFloat(this.dataField.value.toString());
+        } else {
+            this.transformedValue = '';
+            this.numberValue = 0.0;
+        }
+    }
+
+    transformToText(event: Event) {
+        const target = (event.target as HTMLInputElement);
+        this.numberValue = parseFloat(target.value);
+        this.fieldType = 'text';
+        this.transformedValue = this.transformCurrency(target.value);
+    }
+
+    transformToNumber() {
+        this.fieldType = 'number';
+        this.transformedValue = this.numberValue.toString();
+    }
+
+    getCurrencySymbol(): string {
+        return getCurrencySymbol(this.dataField._formatFilter.code, 'wide', this.dataField._formatFilter.locale);
+    }
+
+    private transformCurrency(value: string): string {
+        return this._currencyPipe.transform(
+            parseFloat(value),
+            this.dataField._formatFilter.code,
+            'symbol',
+            '1.' + this.dataField._formatFilter.fractionSize + '-' + this.dataField._formatFilter.fractionSize,
+            this.dataField._formatFilter.locale);
     }
 }
--- projects/netgrif-components/src/lib/data-fields/number-field/number-currency-field/number-currency-field.component.ts
+++ projects/netgrif-components/src/lib/data-fields/number-field/number-currency-field/number-currency-field.component.ts
@@ -1,6 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
-import {CurrencyPipe, registerLocaleData, getCurrencySymbol} from '@angular/common';
+import {CurrencyPipe, registerLocaleData} from '@angular/common';
 import en from '@angular/common/locales/en';
 import sk from '@angular/common/locales/sk';
 import de from '@angular/common/locales/de';
@@ -14,12 +14,8 @@ import {AbstractCurrencyNumberFieldComponent} from '@netgrif/application-engine'
 })
 export class NumberCurrencyFieldComponent extends AbstractCurrencyNumberFieldComponent implements OnInit {
 
-    private numberValue: number;
-    @Input() transformedValue: string;
-    fieldType: string;
-
     constructor(protected _currencyPipe: CurrencyPipe, protected _translate: TranslateService) {
-        super(_translate);
+        super(_currencyPipe, _translate);
         registerLocaleData(en);
         registerLocaleData(sk);
         registerLocaleData(de);
@@ -27,37 +23,13 @@ export class NumberCurrencyFieldComponent extends AbstractCurrencyNumberFieldCom
 
     ngOnInit() {
         super.ngOnInit();
-        if (this.dataField.value !== undefined) {
-            this.transformedValue = this.transformCurrency(this.dataField.value.toString());
-            this.numberValue = parseFloat(this.dataField.value.toString());
-        } else {
-            this.transformedValue = '';
-            this.numberValue = 0.0;
-        }
     }
 
     onFocusOut(event: Event) {
-        const target = (event.target as HTMLInputElement);
-        this.numberValue = parseFloat(target.value);
-        this.fieldType = 'text';
-        this.transformedValue = this.transformCurrency(target.value);
+        this.transformToText(event);
     }
 
     onFocusIn() {
-        this.fieldType = 'number';
-        this.transformedValue = this.numberValue.toString();
-    }
-
-    getCurrencySymbol(): string {
-        return getCurrencySymbol(this.dataField._formatFilter.code, 'wide', this.dataField._formatFilter.locale);
-    }
-
-    private transformCurrency(value: string): string {
-        return this._currencyPipe.transform(
-            parseFloat(value),
-            this.dataField._formatFilter.code,
-            'symbol',
-            '1.' + this.dataField._formatFilter.fractionSize + '-' + this.dataField._formatFilter.fractionSize,
-            this.dataField._formatFilter.locale);
+        this.transformToNumber();
     }
 }
