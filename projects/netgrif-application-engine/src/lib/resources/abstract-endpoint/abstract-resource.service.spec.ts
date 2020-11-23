a926729a8e324d3729436920e8b5561ac11ecddd
Jakub Kovář <kovar@netgrif.com>
2020-11-23 09:26:00 +0100

    NAE-1088 - Improve overridability of resource services
    - abstract resource service test

--- projects/netgrif-application-engine/src/lib/resources/abstract-endpoint/abstract-resource.service.spec.ts
+++ projects/netgrif-application-engine/src/lib/resources/abstract-endpoint/abstract-resource.service.spec.ts
@@ -1,16 +1,42 @@
-import { TestBed } from '@angular/core/testing';
-
-import { AbstractResourceService } from './abstract-resource.service';
+import {TestBed} from '@angular/core/testing';
+import {AbstractResourceService} from './abstract-resource.service';
+import {Injectable} from '@angular/core';
+import {ResourceProvider} from '../resource-provider.service';
+import {ConfigurationService} from '../../configuration/configuration.service';
+import {TestConfigurationService} from '../../utility/tests/test-config';
 
 describe('AbstractResourceService', () => {
-  let service: AbstractResourceService;
+    let service: TestAbstractResourceService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(AbstractResourceService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [
+                TestAbstractResourceService,
+                {provide: ConfigurationService, useClass: TestConfigurationService}
+            ]
+        });
+        service = TestBed.inject(TestAbstractResourceService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+
+    it('should test changeType function', () => {
+        expect(service.changeTypeTest({_embedded: []}, undefined)).toEqual([]);
+        expect(service.changeTypeTest({_embedded: {fields: []}}, 'fields')).toEqual([]);
+        expect(service.changeTypeTest({_embedded: []}, 'fields')).toEqual([]);
+        expect(service.changeTypeTest({}, undefined)).toEqual({});
+    });
 });
+
+@Injectable()
+class TestAbstractResourceService extends AbstractResourceService {
+    constructor(provider: ResourceProvider, configService: ConfigurationService) {
+        super('', provider, configService);
+    }
+
+    public changeTypeTest<T>(r: any, propertiesParams: string): T {
+        return this.changeType(r, propertiesParams);
+    }
+}
