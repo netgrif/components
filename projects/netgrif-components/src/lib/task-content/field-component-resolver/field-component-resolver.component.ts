112907321369ced8863428fc2d4ab429dfa6e59c
Jakub Kovář <kovar@netgrif.com>
2020-11-24 12:37:40 +0100

    NAE-1095 - Optimize task content rendering
    - This reverts commit af06de56

--- projects/netgrif-components/src/lib/task-content/field-component-resolver/field-component-resolver.component.ts
+++ projects/netgrif-components/src/lib/task-content/field-component-resolver/field-component-resolver.component.ts
@@ -1,9 +1,8 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {Component} from '@angular/core';
 import {AbstractFieldComponentResolverComponent, TaskContentService} from '@netgrif/application-engine';
 
 @Component({
     selector: 'nc-field-component-resolver',
-    changeDetection: ChangeDetectionStrategy.OnPush,
     templateUrl: './field-component-resolver.component.html',
     styleUrls: ['./field-component-resolver.component.scss']
 })
