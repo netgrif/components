7edc215919fe43a5179ad5db5866fccace765bd2
Jakub Kovář <kovar@netgrif.com>
2020-12-09 12:39:24 +0100

    NAE-1068 - Zlé ukladanie stĺpcov v case view
    - added view id generation into example app

--- projects/nae-example-app/src/app/doc/task-view/task-view.component.ts
+++ projects/nae-example-app/src/app/doc/task-view/task-view.component.ts
@@ -7,7 +7,7 @@ import {
     TaskEventNotification,
     TaskViewService,
     Task,
-    NAE_TASK_PANEL_DISABLE_BUTTON_FUNCTIONS
+    NAE_TASK_PANEL_DISABLE_BUTTON_FUNCTIONS, NAE_VIEW_ID_SEGMENT, ViewIdService
 } from '@netgrif/application-engine';
 import {HeaderComponent} from '@netgrif/components';
 
@@ -48,9 +48,11 @@ const disableButtonsFactory = () => {
         {   provide: TaskViewService,
             useFactory: localTaskViewServiceFactory,
             deps: [ConfigTaskViewServiceFactory]},
-        {provide: NAE_TASK_PANEL_DISABLE_BUTTON_FUNCTIONS,
+        {   provide: NAE_TASK_PANEL_DISABLE_BUTTON_FUNCTIONS,
             useFactory: disableButtonsFactory
-        }
+        },
+        {provide: NAE_VIEW_ID_SEGMENT, useValue: 'task'},
+        ViewIdService
     ]
 })
 export class TaskViewComponent extends AbstractTaskView implements AfterViewInit {
