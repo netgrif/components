7ee5d99d51c1edbc71ebded8caa3cfbb8ed7e3e4
Jakub Kovář <kovar@netgrif.com>
2020-12-09 13:19:44 +0100

    NAE-1068 - Zlé ukladanie stĺpcov v case view
    - changed view id segment separator to '-' from '.' to avoid backend bug and to mimic old frontend

--- projects/netgrif-application-engine/src/lib/user/services/view-id.service.ts
+++ projects/netgrif-application-engine/src/lib/user/services/view-id.service.ts
@@ -4,6 +4,8 @@ import {NAE_VIEW_ID_SEGMENT} from '../models/view-id-injection-tokens';
 @Injectable()
 export class ViewIdService {
 
+    public static readonly VIEW_ID_SEGMENT_SEPARATOR = '-';
+
     protected _viewId: string;
 
     constructor(@Optional() @SkipSelf() parentInjector: Injector, @Inject(NAE_VIEW_ID_SEGMENT) idSegment: string) {
@@ -11,7 +13,7 @@ export class ViewIdService {
         if (parentInjector !== null) {
             const parentIdService = parentInjector.get(ViewIdService, null);
             if (parentIdService !== null) {
-                this._viewId = parentIdService.viewId + '.';
+                this._viewId = parentIdService.viewId + ViewIdService.VIEW_ID_SEGMENT_SEPARATOR;
             }
         }
         this._viewId += idSegment;
