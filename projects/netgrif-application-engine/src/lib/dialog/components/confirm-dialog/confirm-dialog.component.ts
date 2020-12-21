2aac34305ae8fb9f6cf93d40a0c7e3d900a03a0b
Tomáš Kováčik <kovacik@netgrif.com>
2020-12-21 16:52:32 +0100

    NAE-1177 - Dialog new line dont working
    - sanitize html

--- projects/netgrif-application-engine/src/lib/dialog/components/confirm-dialog/confirm-dialog.component.ts
+++ projects/netgrif-application-engine/src/lib/dialog/components/confirm-dialog/confirm-dialog.component.ts
@@ -3,6 +3,7 @@ import {AbstractDialog} from '../../models/abstract-dialog';
 import {DialogResult} from '../../models/DialogResult';
 import {DialogData} from '../../models/DialogData';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {DomSanitizer, SafeHtml} from '@angular/platform-browser';
 
 /**
  * Question modal dialog with its own layout (which asks the user a question with two answers - yes or no)
@@ -16,18 +17,19 @@ import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 export class ConfirmDialogComponent extends AbstractDialog<ConfirmDialogComponent> {
 
     public choice: boolean;
-    public parsedContent: string;
+    public parsedContent: SafeHtml;
 
     /**
      * Only injecting.
      * @param dialogRef Reference to a dialog opened via the MatDialog service.
      * @param data Injected data that was passed in to a dialog.
+     * @param sanitizer sanitize HTML
      */
     constructor(public dialogRef: MatDialogRef<ConfirmDialogComponent, DialogResult>,
-                @Inject(MAT_DIALOG_DATA) public data: DialogData) {
+                @Inject(MAT_DIALOG_DATA) public data: DialogData, private sanitizer: DomSanitizer) {
         super(dialogRef, data);
         this.choice = false;
-        this.parsedContent = data.content.replace('\\n', '<br>');
+        this.parsedContent = sanitizer.bypassSecurityTrustHtml(data.content.replace('\\n', '<br>'));
     }
 
     public onClose(isSubmitted: boolean) {
