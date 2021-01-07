9000a14fd76c324adb79b9eb617e8763803e64d4
Jakub Kovář <kovar@netgrif.com>
2021-01-07 14:21:18 +0100

    NAE-1185 - Autoopen subtree not working correctly
    - added tests for ResultWithAfterActions class

--- projects/netgrif-application-engine/src/lib/utility/result-with-after-actions.spec.ts
+++ projects/netgrif-application-engine/src/lib/utility/result-with-after-actions.spec.ts
@@ -1,7 +1,26 @@
-import { ResultWithAfterActions } from './result-with-after-actions';
+import {ResultWithAfterActions} from './result-with-after-actions';
 
 describe('ResultWithAfterActions', () => {
-  it('should create an instance', () => {
-    expect(new ResultWithAfterActions()).toBeTruthy();
-  });
+    it('should create an instance', () => {
+        const result = new ResultWithAfterActions(true);
+        expect(result).toBeTruthy();
+        expect(result.result).toBeTrue();
+    });
+
+    it('should run no after actions', () => {
+        const result = new ResultWithAfterActions(true);
+        expect(result).toBeTruthy();
+        expect(result.executeAfterActions()).toBeUndefined();
+    });
+
+    it('should run after actions', () => {
+        let a = 0;
+        const result = new ResultWithAfterActions(true, [() => {
+            a = 1;
+        }]);
+        expect(result).toBeTruthy();
+        expect(a).toEqual(0);
+        expect(result.executeAfterActions()).toBeUndefined();
+        expect(a).toEqual(1);
+    });
 });
