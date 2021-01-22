989b830e8504f04acca061778cc619db3e8f67d5
Tomáš Kováčik <kovacik@netgrif.com>
2021-01-22 12:29:50 +0100

    NAE-1119 - constructor and destructor
    - fix pr

--- projects/netgrif-application-engine/src/lib/process/rolesAndPermissions.ts
+++ projects/netgrif-application-engine/src/lib/process/rolesAndPermissions.ts
@@ -1,6 +1,7 @@
 import NetRole from './netRole';
+import {Permissions} from './permissions';
 
 export default interface RolesAndPermissions {
     processRoles: Array<NetRole>;
-    permissions: object;
+    permissions: Permissions;
 }
