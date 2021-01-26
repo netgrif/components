4565be7f5c965774fb93b0181ccab018b9aa9614
Jakub Kovář <kovar@netgrif.com>
2021-01-26 15:44:39 +0100

    NAE-1103 - Advanced Search
    - fixed tests failing in afterAll when setTimeout is called in their constructor

--- projects/netgrif-application-engine/src/lib/search/models/category/case/case-process.spec.ts
+++ projects/netgrif-application-engine/src/lib/search/models/category/case/case-process.spec.ts
@@ -1,10 +1,11 @@
 import {CaseProcess} from './case-process';
 import {OperatorService} from '../../../operator-service/operator.service';
 import {createMockDependencies} from '../../../../utility/tests/search-category-mock-dependencies';
+import {waitForAsync} from '@angular/core/testing';
 
 describe('CaseProcess', () => {
-    it('should create an instance', () => {
+    it('should create an instance', waitForAsync(() => {
         const opService = new OperatorService();
         expect(new CaseProcess(opService, null, createMockDependencies())).toBeTruthy();
-    });
+    }));
 });
--- projects/netgrif-application-engine/src/lib/search/models/category/case/case-role.spec.ts
+++ projects/netgrif-application-engine/src/lib/search/models/category/case/case-role.spec.ts
@@ -1,10 +1,11 @@
 import {CaseRole} from './case-role';
 import {OperatorService} from '../../../operator-service/operator.service';
 import {createMockDependencies} from '../../../../utility/tests/search-category-mock-dependencies';
+import {waitForAsync} from '@angular/core/testing';
 
 describe('CaseRole', () => {
-    it('should create an instance', () => {
+    it('should create an instance', waitForAsync(() => {
         const opService = new OperatorService();
         expect(new CaseRole(opService, null, createMockDependencies())).toBeTruthy();
-    });
+    }));
 });
