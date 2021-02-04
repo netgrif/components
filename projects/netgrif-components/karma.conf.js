f17d0e2511befe2e057ef79051a74d76a2ec6052
Milan Mladoniczky <mladoniczky@netgrif.com>
2021-02-04 07:48:31 +0000

    karma.conf.js increase limit for async jasmine

--- projects/netgrif-components/karma.conf.js
+++ projects/netgrif-components/karma.conf.js
@@ -23,7 +23,7 @@ module.exports = function(config) {
         client: {
             clearContext: false, // leave Jasmine Spec Runner output visible in browser
             jasmine: {
-                timeoutInterval: 10000
+                timeoutInterval: 30000
             }
         },
 
