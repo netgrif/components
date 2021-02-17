1d99b3b0a022aadf5686d5f478fe1eb0cc30352a
Jakub Kovář <kovar@netgrif.com>
2021-02-17 17:29:54 +0100

    NAE-1211 - Search in several data fields does not work as intended
    - fixed broken tests

--- projects/netgrif-application-engine/src/lib/utility/tests/search-category-mock-dependencies.ts
+++ projects/netgrif-application-engine/src/lib/utility/tests/search-category-mock-dependencies.ts
@@ -4,16 +4,21 @@ import {CaseViewService} from '../../view/case-view/service/case-view-service';
 import {TaskViewService} from '../../view/task-view/service/task-view.service';
 import {UserResourceService} from '../../resources/engine-endpoint/user-resource.service';
 import {CategoryFactory} from '../../search/category-factory/category-factory';
+import {SearchIndexResolverService} from '../../search/search-keyword-resolver-service/search-index-resolver.service';
+import {OptionalDependencies} from '../../search/category-factory/optional-dependencies';
 
 const opService = new OperatorService();
 
-export const createMockDependencies = () => {
+export const createMockDependencies: () => OptionalDependencies = () => {
     const mockCaseView = {allowedNets$: of([])} as CaseViewService;
     const mockTaskView = {allowedNets$: of([])} as TaskViewService;
     const mockUserResourceService = {getAll: () => of({content: [], pagination: {}})} as UserResourceService;
+    const searchIndexResolver = new SearchIndexResolverService();
 
     return {
-        categoryFactory: new CategoryFactory(opService, null, mockCaseView, mockTaskView, mockUserResourceService),
+        categoryFactory: new CategoryFactory(opService, null,
+            searchIndexResolver, mockCaseView, mockTaskView, mockUserResourceService),
+        searchIndexResolver,
         userResourceService: mockUserResourceService,
         caseViewService: mockCaseView,
         taskViewService: mockTaskView,
