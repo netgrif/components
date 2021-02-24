cf981bb2f7aa5219050bbc45828e06c003d48496
Jakub Kovář <kovar@netgrif.com>
2021-02-24 09:44:26 +0100

    NAE-1249 - Async loading datagroup
    - refactored number field to inherit from correct classes

--- projects/netgrif-application-engine/src/lib/data-fields/number-field/abstract-number-errors.component.spec.ts
+++ projects/netgrif-application-engine/src/lib/data-fields/number-field/abstract-number-errors.component.spec.ts
@@ -1,26 +1,27 @@
-import {waitForAsync, ComponentFixture, TestBed} from '@angular/core/testing';
+import {Component, CUSTOM_ELEMENTS_SCHEMA, OnDestroy} from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
+import {AbstractNumberErrorsComponent} from './abstract-number-errors.component';
+import {NumberField} from './models/number-field';
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
+import {MaterialModule} from '../../material/material.module';
 import {AngularResizedEventModule} from 'angular-resize-event';
+import {TranslateLibModule} from '../../translate/translate-lib.module';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
-import {Component, CUSTOM_ELEMENTS_SCHEMA, Inject, Optional} from '@angular/core';
-import {AbstractNumberFieldComponent} from './abstract-number-field.component';
-import {TranslateService} from '@ngx-translate/core';
-import {NumberField} from './models/number-field';
-import {LanguageService} from '../../translate/language.service';
-import {MockAuthenticationMethodService} from '../../utility/tests/mocks/mock-authentication-method-service';
 import {AuthenticationMethodService} from '../../authentication/services/authentication-method.service';
-import {UserResourceService} from '../../resources/engine-endpoint/user-resource.service';
+import {MockAuthenticationMethodService} from '../../utility/tests/mocks/mock-authentication-method-service';
 import {AuthenticationService} from '../../authentication/services/authentication/authentication.service';
-import {ConfigurationService} from '../../configuration/configuration.service';
 import {MockAuthenticationService} from '../../utility/tests/mocks/mock-authentication.service';
-import {TestConfigurationService} from '../../utility/tests/test-config';
+import {UserResourceService} from '../../resources/engine-endpoint/user-resource.service';
 import {MockUserResourceService} from '../../utility/tests/mocks/mock-user-resource.service';
-import {TranslateLibModule} from '../../translate/translate-lib.module';
-import {MaterialModule} from '../../material/material.module';
-import {NAE_INFORM_ABOUT_INVALID_DATA} from '../models/invalid-data-policy-token';
+import {ConfigurationService} from '../../configuration/configuration.service';
+import {TestConfigurationService} from '../../utility/tests/test-config';
+import {LanguageService} from '../../translate/language.service';
+import {FormControl} from '@angular/forms';
 
-describe('AbstractNumberFieldComponent', () => {
-    let component: TestNumComponent;
+
+describe('AbstractNumberErrorsComponent', () => {
+    let component: TestNumErrorComponent;
     let fixture: ComponentFixture<TestWrapperComponent>;
 
     beforeEach(waitForAsync(() => {
@@ -39,7 +40,7 @@ describe('AbstractNumberFieldComponent', () => {
                 {provide: ConfigurationService, useClass: TestConfigurationService}
             ],
             declarations: [
-                TestNumComponent,
+                TestNumErrorComponent,
                 TestWrapperComponent
             ],
             schemas: [CUSTOM_ELEMENTS_SCHEMA]
@@ -55,27 +56,29 @@ describe('AbstractNumberFieldComponent', () => {
         expect(component).toBeTruthy();
     });
 
-    afterEach(() => {
-        TestBed.resetTestingModule();
+    it('should get error message', () => {
+        expect(component.getErrorMessage()).toEqual('This is custom odd message!');
+
+        component.numberField.value = 5;
+        expect(component.getErrorMessage()).toEqual('Entered number must be even');
     });
 });
 
 @Component({
-    selector: 'nae-test-num',
+    selector: 'nae-test-num-err',
     template: ''
 })
-class TestNumComponent extends AbstractNumberFieldComponent {
-    constructor(translate: TranslateService,
-                @Optional() @Inject(NAE_INFORM_ABOUT_INVALID_DATA) informAboutInvalidData: boolean | null) {
-        super(translate, informAboutInvalidData);
+class TestNumErrorComponent extends AbstractNumberErrorsComponent {
+    constructor(translate: TranslateService) {
+        super(translate);
     }
 }
 
 @Component({
     selector: 'nae-test-wrapper',
-    template: '<nae-test-num [dataField]="field"></nae-test-num>'
+    template: '<nae-test-num-err [numberField]="field" [formControlRef]="fc"></nae-test-num-err>'
 })
-class TestWrapperComponent {
+class TestWrapperComponent implements OnDestroy {
     field = new NumberField('', '', 4, {
         optional: true,
         visible: true,
@@ -91,4 +94,15 @@ class TestWrapperComponent {
         {validationRule: 'inrange 0,inf', validationMessage: 'This is custom message!'},
         {validationRule: 'inrange -5,0', validationMessage: 'This is custom message!'},
     ]);
+
+    fc = new FormControl();
+
+    constructor() {
+        this.field.registerFormControl(this.fc);
+    }
+
+    ngOnDestroy(): void {
+        this.field.disconnectFormControl();
+    }
+
 }
