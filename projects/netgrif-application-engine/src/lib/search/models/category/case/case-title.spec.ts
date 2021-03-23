238b9a0e58f7d2eb354271c0f144c45e62d1cf91
Jakub Kovář <kovar@netgrif.com>
2021-03-23 16:03:02 +0100

    NAE-1207 - Filter management
    - case title and visual id tests

--- projects/netgrif-application-engine/src/lib/search/models/category/case/case-title.spec.ts
+++ projects/netgrif-application-engine/src/lib/search/models/category/case/case-title.spec.ts
@@ -1,9 +1,53 @@
 import {CaseTitle} from './case-title';
 import {OperatorService} from '../../../operator-service/operator.service';
 import {OperatorResolverService} from '../../../operator-service/operator-resolver.service';
+import {configureCategory} from '../../../../utility/tests/utility/configure-category';
+import {Equals} from '../../operator/equals';
+import {Categories} from '../categories';
+import {Operators} from '../../operator/operators';
 
 describe('CaseTitle', () => {
+    let category: CaseTitle;
+    let operatorService: OperatorService;
+
+    beforeEach(() => {
+        operatorService = new OperatorService(new OperatorResolverService());
+        category = new CaseTitle(operatorService, null);
+    });
+
     it('should create an instance', () => {
-        expect(new CaseTitle(new OperatorService(new OperatorResolverService()), null)).toBeTruthy();
+        expect(category).toBeTruthy();
+    });
+
+    it('should not serialize incomplete instance', () => {
+        expect(category.createMetadata()).toBeUndefined();
+    });
+
+    it('should serialize complete instance', () => {
+        configureCategory(category, operatorService, Equals, ['foo']);
+
+        const metadata = category.createMetadata();
+        expect(metadata).toBeTruthy();
+        expect(metadata.values).toEqual(['foo']);
+        expect(metadata.category).toBe(Categories.CASE_TITLE);
+        expect(metadata.configuration?.operator).toBe(Operators.EQUALS);
+    });
+
+    it('should deserialize stored instance', () => {
+        configureCategory(category, operatorService, Equals, ['foo']);
+
+        const metadata = category.createMetadata();
+        const deserialized = new CaseTitle(operatorService, null);
+        deserialized.loadFromMetadata(metadata);
+        expect(deserialized.isOperatorSelected()).toBeTrue();
+        expect(deserialized.providesPredicate).toBeTrue();
+
+        expect((category as any)._operandsFormControls[0].value).toEqual((deserialized as any)._operandsFormControls[0].value);
+
+        const deserializedMetadata = deserialized.createMetadata();
+        expect(deserializedMetadata).toBeTruthy();
+        expect(deserializedMetadata.configuration).toEqual(metadata.configuration);
+        expect(deserializedMetadata.category).toEqual(metadata.category);
+        expect(deserializedMetadata.values).toEqual(metadata.values);
     });
 });
