7b3e806f7a0ab6908ed6f82db5441be8698fe094
Jakub Kovář <kovar@netgrif.com>
2021-04-06 10:13:35 +0200

    NAE-1207 - Filter management
    - filter field component basics

---
+++ projects/netgrif-components/src/lib/data-fields/filter-field/filter-field.component.ts
@@ -0,0 +1,15 @@
+import {Component, Inject, Optional} from '@angular/core';
+import {AbstractFilterFieldComponent, NAE_INFORM_ABOUT_INVALID_DATA} from '@netgrif/application-engine';
+
+@Component({
+    selector: 'nc-filter-field',
+    templateUrl: './filter-field.component.html',
+    styleUrls: ['./filter-field.component.scss']
+})
+export class FilterFieldComponent extends AbstractFilterFieldComponent {
+
+    constructor(@Optional() @Inject(NAE_INFORM_ABOUT_INVALID_DATA) informAboutInvalidData: boolean | null) {
+        super(informAboutInvalidData);
+    }
+
+}
