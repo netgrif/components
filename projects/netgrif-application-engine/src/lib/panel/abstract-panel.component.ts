f97c2f004029019255c34a05c452f86deca5891c
palajsamuel <palaj@netgrif.com>
2021-04-22 17:41:25 +0200

    NAE-1291 - Color lines
    - added line background color based on case color to case panel
    - added method to determine font color of line based on case color

--- projects/netgrif-application-engine/src/lib/panel/abstract-panel.component.ts
+++ projects/netgrif-application-engine/src/lib/panel/abstract-panel.component.ts
@@ -9,6 +9,7 @@ export abstract class AbstractPanelComponent implements AfterViewInit, OnDestroy
     @Input() panelContent: TemplateRef<object>;
     @Input() first: boolean;
     @Input() last: boolean;
+    @Input() public caseColor: string;
 
     @Output() stopLoading: EventEmitter<object> = new EventEmitter();
     @Output() getExpansionPanelRef: EventEmitter<MatExpansionPanel> = new EventEmitter();
@@ -39,5 +40,17 @@ export abstract class AbstractPanelComponent implements AfterViewInit, OnDestroy
         if (this.preventExpand) {
             this.matExpansionPanel.close();
         }
+    }
+
+    computeCaseFontColor(): string {
+        //check if hex color
+        if(!(/^#[0-9A-F]{6}$/i.exec(this.caseColor))) return 'black'
+        var m = this.caseColor.match(/^#?([\da-f]{2})([\da-f]{2})([\da-f]{2})$/i);
+        let r = parseInt(m[1], 16);
+        let g = parseInt(m[2], 16);
+        let b = parseInt(m[3], 16);
+        //možné špecifickejšie upraviť threshold hodnotu
+        return (((r * 0.299) + (g * 0.587) + (b * 0.114)) > 150) ?
+            'black' : 'white';
     }
 }
