80c6576636a4e5ad5e6c2ad297950ed38a6eb298
palajsamuel <palaj@netgrif.com>
2021-04-26 16:02:56 +0200

    NAE-1289 - Advanced search - more operators
    - fix less than equal operator for date fields

--- projects/netgrif-application-engine/src/lib/search/models/operator/less-than-equal-date.ts
+++ projects/netgrif-application-engine/src/lib/search/models/operator/less-than-equal-date.ts
@@ -1,28 +1,27 @@
 import {Operator} from './operator';
 import moment, {Moment} from 'moment';
-import {LessThanEqual} from './less-than-equal';
 import {OperatorService} from '../../operator-service/operator.service';
 import {Query} from '../query/query';
 import {Operators} from './operators';
-import {clearTimeInformation} from '../../../utility/clear-time-information';
+import {LessThanDate} from './less-than-date';
 
 /**
  * Less or equal than operator for Date fields
  */
 export class LessThanEqualDate extends Operator<Moment>{
 
-    protected lessThanEqual: LessThanEqual;
+    protected lessThan: LessThanDate;
 
     constructor(operators: OperatorService) {
         super(1);
-        this.lessThanEqual = operators.getOperator(LessThanEqual) as LessThanEqual;
+        this.lessThan = operators.getOperator(LessThanDate) as LessThanDate;
     }
 
     createQuery(elasticKeywords: Array<string>, args: Array<Moment>): Query {
         this.checkArgumentsCount(args);
         const arg = moment(args[0]);
-        clearTimeInformation(arg);
-        return this.lessThanEqual.createQuery(elasticKeywords, [arg.valueOf()]);
+        arg.date(arg.date() + 1);
+        return this.lessThan.createQuery(elasticKeywords, [arg]);
     }
 
     getOperatorNameTemplate(): Array<string> {
