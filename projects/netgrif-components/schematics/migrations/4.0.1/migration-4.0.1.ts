a5f8e5bb10a3231d5fbdfd6576aebcd2d4eea724
Jakub Kovář <kovar@netgrif.com>
2021-04-30 08:55:31 +0200

    NAE-1228 - Re-enable class name minification
    - removed schematics that disabled class name minification in delivery projects

--- projects/netgrif-components/schematics/migrations/4.0.1/migration-4.0.1.ts
+++
@@ -1,7 +0,0 @@
-import {Rule, schematic} from '@angular-devkit/schematics';
-
-export function schematicEntryPoint(): Rule {
-    return () => {
-        return schematic('update-package-json', {});
-    };
-}
