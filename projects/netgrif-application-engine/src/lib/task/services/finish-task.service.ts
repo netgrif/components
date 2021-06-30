328a898d9ef62eaa64ab4e496afa3195c9064978
palajsamuel <palaj@netgrif.com>
2021-06-30 13:30:34 +0200

    NAE-1303 - Dokoncujem ulohu [object Object] zlyhalo
    - snackbar text after usuccessful finishing of task fixed

--- projects/netgrif-application-engine/src/lib/task/services/finish-task.service.ts
+++ projects/netgrif-application-engine/src/lib/task/services/finish-task.service.ts
@@ -131,7 +131,7 @@ export class FinishTaskService extends TaskHandlingService {
             }
 
             this._snackBar.openErrorSnackBar(`${this._translate.instant('tasks.snackbar.finishTask')}
-             ${this._task} ${this._translate.instant('tasks.snackbar.failed')}`);
+             ${this._task.title} ${this._translate.instant('tasks.snackbar.failed')}`);
             this.sendNotification(false);
             afterAction.next(false);
             afterAction.complete();
