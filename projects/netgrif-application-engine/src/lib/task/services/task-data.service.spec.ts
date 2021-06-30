4c289e92aeee1bcf84d9e6793ee90c0953294f2a
Jakub Kovář <kovar@netgrif.com>
2021-06-30 12:55:11 +0200

    NAE-1386 - setData events are ignored when fired too closely together
    - added update task data field test

--- projects/netgrif-application-engine/src/lib/task/services/task-data.service.spec.ts
+++ projects/netgrif-application-engine/src/lib/task/services/task-data.service.spec.ts
@@ -20,15 +20,20 @@ import {createMockField} from '../../utility/tests/utility/create-mock-field';
 import {createMockDataGroup} from '../../utility/tests/utility/create-mock-datagroup';
 import {CallChainService} from '../../utility/call-chain/call-chain.service';
 import {DataGroup} from '../../resources/interface/data-groups';
-import {Observable, of} from 'rxjs';
-import {delay} from 'rxjs/operators';
+import {BehaviorSubject, Observable, of} from 'rxjs';
+import {delay, take} from 'rxjs/operators';
 import {TaskResourceService} from '../../resources/engine-endpoint/task-resource.service';
+import {TaskSetDataRequestBody} from '../../resources/interface/task-set-data-request-body';
+import {ChangedFieldContainer} from '../../resources/interface/changed-field-container';
+import {FormControl} from '@angular/forms';
+import {SnackBarModule} from '../../snack-bar/snack-bar.module';
 
 describe('TaskDataService', () => {
     let service: TaskDataService;
     let taskContentService: TaskContentService;
     let afterActionService: CallChainService;
     let taskResourceService: MockTaskResourceService;
+    let taskStateService: TestTaskRequestStateService;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
@@ -36,24 +41,26 @@ describe('TaskDataService', () => {
                 TranslateLibModule,
                 HttpClientTestingModule,
                 MaterialModule,
-                NoopAnimationsModule
+                NoopAnimationsModule,
+                SnackBarModule
             ],
             providers: [
                 TaskDataService,
-                TaskRequestStateService,
                 DataFocusPolicyService,
                 TaskEventService,
+                {provide: TaskRequestStateService, useClass: TestTaskRequestStateService},
                 {provide: TaskContentService, useClass: UnlimitedTaskContentService},
                 {provide: ConfigurationService, useClass: TestConfigurationService},
                 {provide: NAE_TASK_OPERATIONS, useClass: NullTaskOperations},
                 {provide: AuthenticationMethodService, useClass: MockAuthenticationMethodService},
-                {provide: TaskResourceService, useClass: MockTaskResourceService}
+                {provide: TaskResourceService, useClass: MockTaskResourceService},
             ]
         });
         service = TestBed.inject(TaskDataService);
         taskContentService = TestBed.inject(TaskContentService);
         afterActionService = TestBed.inject(CallChainService);
         taskResourceService = TestBed.inject(TaskResourceService) as unknown as MockTaskResourceService;
+        taskStateService = TestBed.inject(TaskRequestStateService) as unknown as TestTaskRequestStateService;
     });
 
     afterEach(() => {
@@ -87,11 +94,61 @@ describe('TaskDataService', () => {
             createMockField(true, {x: 0, y: 0, cols: 0, rows: 0}, 0),
             createMockField(true, {x: 1, y: 0, cols: 0, rows: 0}, 1)
         ])];
+        expect(taskContentService.task.dataGroups.length).toEqual(0);
 
         service.initializeTaskDataFields(afterActionService.create(success => {
             if (success) {
+                expect(taskContentService.task.dataGroups.length).toEqual(1);
+                expect(taskContentService.task.dataGroups[0].fields.length).toEqual(2);
+
+                const mockField = taskContentService.task.dataGroups[0].fields[0];
+                expect(((mockField as any)._value as BehaviorSubject<boolean>).observers.length).toEqual(1);
+
                 expect(service.ngOnDestroy()).toBeUndefined();
                 done();
+            }
+        }));
+    });
+
+    it('should update task data fields', (done) => {
+        expect(service).toBeTruthy();
+        expect(taskContentService).toBeTruthy();
+
+        taskContentService.task = createMockTask();
+        taskResourceService.response = [createMockDataGroup([
+            createMockField(true, {x: 0, y: 0, cols: 0, rows: 0}, 0),
+            createMockField(true, {x: 1, y: 0, cols: 0, rows: 0}, 1)
+        ])];
+
+        service.initializeTaskDataFields(afterActionService.create(success => {
+            if (success) {
+                expect(taskContentService.task.dataGroups.length).toEqual(1);
+                expect(taskContentService.task.dataGroups[0].fields.length).toEqual(2);
+
+                const mockField = taskContentService.task.dataGroups[0].fields[0];
+                expect(((mockField as any)._value as BehaviorSubject<boolean>).observers.length).toEqual(1);
+                mockField.registerFormControl(new FormControl());
+
+                let subCallCount = 0;
+                taskStateService.updating$.pipe(take(3)).subscribe(updating => {
+                    switch (subCallCount) {
+                        case 0:
+                            expect(updating).toBeFalse();
+                            break;
+                        case 1:
+                            expect(updating).toBeTrue();
+                            break;
+                        case 2:
+                            expect(updating).toBeFalse();
+                            done();
+                            break;
+                    }
+                    subCallCount++;
+                });
+
+                taskContentService.task.user = {email: '', id: '', name: '', surname: '', fullName: ''};
+
+                mockField.value = !mockField.value;
             }
         }));
     });
@@ -114,5 +171,19 @@ class MockTaskResourceService {
 
     public getData(taskId: string): Observable<Array<DataGroup>> {
         return of(this._response).pipe(delay(this._delay));
+    }
+
+    public setData(taskId: string, body: TaskSetDataRequestBody): Observable<ChangedFieldContainer> {
+        return of({changedFields: {}}).pipe(delay(this._delay));
+    }
+}
+
+class TestTaskRequestStateService extends TaskRequestStateService {
+    public get loading$(): Observable<boolean> {
+        return this._loading.asObservable();
+    }
+
+    public get updating$(): Observable<boolean> {
+        return this._updating.asObservable();
     }
 }
