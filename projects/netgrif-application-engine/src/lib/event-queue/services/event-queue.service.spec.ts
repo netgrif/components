98918f29b459d1511d05db97ebb05330d93aa4a5
Jakub Kovář <kovar@netgrif.com>
2021-07-02 14:44:51 +0200

    NAE-1386 - setData events are ignored when fired too closely together
    - fixed test

--- projects/netgrif-application-engine/src/lib/event-queue/services/event-queue.service.spec.ts
+++ projects/netgrif-application-engine/src/lib/event-queue/services/event-queue.service.spec.ts
@@ -1,11 +1,17 @@
 import {TestBed} from '@angular/core/testing';
 import {EventQueueService} from './event-queue.service';
+import {ConfigurationService} from '../../configuration/configuration.service';
+import {TestConfigurationService} from '../../utility/tests/test-config';
 
 describe('EventQueueService', () => {
     let service: EventQueueService;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        TestBed.configureTestingModule({
+            providers: [
+                {provide: ConfigurationService, useClass: TestConfigurationService}
+            ]
+        });
         service = TestBed.inject(EventQueueService);
     });
 
