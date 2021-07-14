9e98ad9980a1857c011b83ecfa2b864df94fd55f
Jakub Kovář <kovar@netgrif.com>
2021-07-14 14:52:07 +0200

    NAE-1104 - User filters
    - added doc to unrelated interface

--- projects/netgrif-application-engine/src/lib/view/task-view/models/task-view-configuration.ts
+++ projects/netgrif-application-engine/src/lib/view/task-view/models/task-view-configuration.ts
@@ -9,6 +9,12 @@ export interface TaskViewConfiguration {
      * If both this and the deprecated {@link NAE_PREFERRED_TASK_ENDPOINT} is provided, this option takes precedence.
      */
     preferredEndpoint?: TaskEndpoint;
+    /**
+     * If not present defaults to `of(true)`
+     */
     initiallyOpenOneTask?: Observable<boolean>;
+    /**
+     * If not present defaults to `of(true)`
+     */
     closeTaskTabOnNoTasks?: Observable<boolean>;
 }
