6ac630fdc9db303649a3f937098a3f163b8d72ac
Jakub Kovář <kovar@netgrif.com>
2021-07-21 12:20:02 +0200

    NAE-1104 - User filters
    - fixed two tests

--- projects/netgrif-application-engine/src/lib/navigation/group-navigation-component-resolver/abstract-group-navigation-component-resolver.component.spec.ts
+++ projects/netgrif-application-engine/src/lib/navigation/group-navigation-component-resolver/abstract-group-navigation-component-resolver.component.spec.ts
@@ -1,8 +1,8 @@
 import {AbstractGroupNavigationComponentResolverComponent} from './abstract-group-navigation-component-resolver.component';
-import {Component, Injector, Type} from '@angular/core';
+import {Component, Injector} from '@angular/core';
 import {LoggerService} from '../../logger/services/logger.service';
 import {GroupNavigationComponentResolverService} from './group-navigation-component-resolver.service';
-import {ActivatedRoute, Router} from '@angular/router';
+import {ActivatedRoute, Router, RouterModule} from '@angular/router';
 import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 import {MaterialModule} from '../../material/material.module';
 import {TranslateLibModule} from '../../translate/translate-lib.module';
@@ -10,7 +10,7 @@ import {HttpClientTestingModule} from '@angular/common/http/testing';
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 import {ConfigurationService} from '../../configuration/configuration.service';
 import {TestConfigurationService} from '../../utility/tests/test-config';
-import {TaskResourceService} from '../../resources/engine-endpoint/task-resource.service';
+import {TestGroupNavigationComponentResolverService} from './group-navigation-component-resolver.service.spec';
 
 describe('AbstractGroupNavigationComponentResolverComponent', () => {
     let component: TestAbstractGroupNavigationComponentResolverComponent;
@@ -23,6 +23,7 @@ describe('AbstractGroupNavigationComponentResolverComponent', () => {
                 TranslateLibModule,
                 HttpClientTestingModule,
                 NoopAnimationsModule,
+                RouterModule.forRoot([])
             ],
             providers: [
                 {provide: ConfigurationService, useClass: TestConfigurationService},
@@ -61,26 +62,5 @@ class TestAbstractGroupNavigationComponentResolverComponent extends AbstractGrou
                 router: Router,
                 log: LoggerService) {
         super(componentResolverService, parentInjector, activatedRoute, router, log);
-    }
-}
-
-@Component({
-    selector: 'nae-test-portal',
-    template: ''
-})
-class TestPortalComponent {
-    constructor() {
-    }
-}
-
-class TestGroupNavigationComponentResolverService extends GroupNavigationComponentResolverService {
-
-    constructor(taskResourceService: TaskResourceService,
-                log: LoggerService) {
-        super(taskResourceService, log);
-    }
-
-    protected resolveViewComponent(navigationItemTaskData: any): Type<any> {
-        return TestPortalComponent;
     }
 }
