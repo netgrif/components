9b8d17d01d303a3a0bb4f9d6a563cf24817255b6
Jakub Kovář <kovar@netgrif.com>
2021-07-22 08:44:52 +0200

    NAE-1104 - User filters
    - fix nc test

--- projects/netgrif-components/src/lib/navigation/group-navigation-component-resolver/default-components/default-tabbed-task-view/default-tabbed-task-view.component.spec.ts
+++ projects/netgrif-components/src/lib/navigation/group-navigation-component-resolver/default-components/default-tabbed-task-view/default-tabbed-task-view.component.spec.ts
@@ -1,25 +1,48 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { DefaultTabbedTaskViewComponent } from './default-tabbed-task-view.component';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {DefaultTabbedTaskViewComponent} from './default-tabbed-task-view.component';
+import {NavigationComponentModule} from '../../../navigation.module';
+import {NAE_TAB_DATA, SimpleFilter, TestMockDependenciesModule} from '@netgrif/application-engine';
+import {of} from 'rxjs';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 
 describe('DefaultTabbedTaskViewComponent', () => {
-  let component: DefaultTabbedTaskViewComponent;
-  let fixture: ComponentFixture<DefaultTabbedTaskViewComponent>;
+    let component: DefaultTabbedTaskViewComponent;
+    let fixture: ComponentFixture<DefaultTabbedTaskViewComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ DefaultTabbedTaskViewComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [
+                NavigationComponentModule,
+                TestMockDependenciesModule,
+                NoopAnimationsModule,
+            ],
+            providers: [
+                {
+                    provide: NAE_TAB_DATA,
+                    useValue: {
+                        baseFilter: SimpleFilter.emptyTaskFilter(),
+                        allowedNets: [],
+                        tabUniqueId: '1',
+                        tabSelected$: of(true),
+                        tabClosed$: of()
+                    }
+                }
+            ]
+        })
+            .compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(DefaultTabbedTaskViewComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(DefaultTabbedTaskViewComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    afterEach(() => {
+        TestBed.resetTestingModule();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
