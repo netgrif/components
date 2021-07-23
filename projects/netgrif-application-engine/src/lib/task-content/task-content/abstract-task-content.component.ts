e5232f4bd3f164cb375b910fd2bcec2fee8adc05
Stefan Renczes <renczes@netgrif.com>
2021-07-23 12:57:59 +0200

    NAE-1397 - Fix task rows
    - implemented check whether scrollbar is needed
    - implemented counting of max-height in case rows has fixed number
    - implemented scroll-bar for TaskContentComponent

--- projects/netgrif-application-engine/src/lib/task-content/task-content/abstract-task-content.component.ts
+++ projects/netgrif-application-engine/src/lib/task-content/task-content/abstract-task-content.component.ts
@@ -89,6 +89,11 @@ export abstract class AbstractTaskContentComponent implements OnDestroy {
     protected _asyncRenderingConfig: AsyncRenderingConfiguration;
     protected _asyncRenderTimeout: number;
 
+    /**
+     * Defines the row height of one row in task content
+     */
+    protected rowHeight = 105;
+
     protected constructor(protected _fieldConverter: FieldConverterService,
                           public taskContentService: TaskContentService,
                           protected _paperView: PaperViewService,
@@ -212,6 +217,27 @@ export abstract class AbstractTaskContentComponent implements OnDestroy {
 
         item.alignment = 'space-between ' + alignment;
         return item.alignment;
+    }
+
+    /**
+     * Computes component height if the rows attribute of task layout is set.
+     */
+    public resolveContentHeightCss(): string {
+        if (this.scrollingNeeded()) {
+            return this.taskContentService.task.layout.rows * this.rowHeight + 'px';
+        } else {
+            return '100%';
+        }
+    }
+
+    /**
+     * Decides whether there is a fixed number of rows and it need-s scrolling
+     */
+    public scrollingNeeded(): boolean {
+        return this.taskContentService.task
+            && !!this.taskContentService.task.layout
+            && !!this.taskContentService.task.layout.rows
+            && this.taskContentService.task.layout.rows > 0;
     }
 
     /**
