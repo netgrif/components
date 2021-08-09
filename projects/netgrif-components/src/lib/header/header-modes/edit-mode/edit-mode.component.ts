76a44caf2a76913465cf90b27ee2fc27433c7130
Samuel Spisak <spisak@netgrif.com>
2021-08-09 12:28:29 +0200

    NAE-1378 -opening panel on input focus, however panel is opened on the FIRST input header and not the CURRENT one (TODO)

--- projects/netgrif-components/src/lib/header/header-modes/edit-mode/edit-mode.component.ts
+++ projects/netgrif-components/src/lib/header/header-modes/edit-mode/edit-mode.component.ts
@@ -1,6 +1,8 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
 import {AbstractEditModeComponent, LoggerService} from '@netgrif/application-engine';
+import {MatAutocompleteTrigger} from '@angular/material/autocomplete';
+import {timeout} from 'rxjs/operators';
 
 @Component({
     selector: 'nc-edit-mode',
@@ -8,6 +10,18 @@ import {AbstractEditModeComponent, LoggerService} from '@netgrif/application-eng
     styleUrls: ['./edit-mode.component.scss']
 })
 export class EditModeComponent extends AbstractEditModeComponent {
+
+    @ViewChild(MatAutocompleteTrigger) trigger: MatAutocompleteTrigger;
+    onFocus() {
+        // this.trigger._onChange('');
+        console.log('focused');
+        console.log(this.trigger.panelOpen);
+        console.log('opening');
+        this.trigger.openPanel();
+        console.log(this.trigger.panelOpen);
+    }
+
+
     constructor(protected _translate: TranslateService,
                 protected loggerService: LoggerService) {
         super(_translate, loggerService);
