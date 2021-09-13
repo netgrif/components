43049401f333cc69d245e9ff188e623e7f63eff1
Jakub Kovář <kovar@netgrif.com>
2021-09-13 15:25:08 +0200

    NAE-1454 - Dynamic filter chaining
    - fixed tests

--- projects/netgrif-application-engine/src/lib/navigation/utility/filter-extraction.service.spec.ts
+++ projects/netgrif-application-engine/src/lib/navigation/utility/filter-extraction.service.spec.ts
@@ -1,16 +1,25 @@
-import { TestBed } from '@angular/core/testing';
-
-import { FilterExtractionService } from './filter-extraction.service';
+import {TestBed} from '@angular/core/testing';
+import {FilterExtractionService} from './filter-extraction.service';
+import {ConfigurationService} from '../../configuration/configuration.service';
+import {TestConfigurationService} from '../../utility/tests/test-config';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 
 describe('FilterExtractionService', () => {
-  let service: FilterExtractionService;
+    let service: FilterExtractionService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(FilterExtractionService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                HttpClientTestingModule
+            ],
+            providers: [
+                {provide: ConfigurationService, useClass: TestConfigurationService}
+            ]
+        });
+        service = TestBed.inject(FilterExtractionService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
