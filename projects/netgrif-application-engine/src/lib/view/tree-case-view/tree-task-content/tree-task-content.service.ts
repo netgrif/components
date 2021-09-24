1b0ac15725b3ce2764150fa483a301500ee03ff2
kozak <kozak@netgrif.com>
2021-09-24 10:08:16 +0200

    NAE-1464 Tree component refactoring
    - refactoring

--- projects/netgrif-application-engine/src/lib/view/tree-case-view/tree-task-content/tree-task-content.service.ts
+++ projects/netgrif-application-engine/src/lib/view/tree-case-view/tree-task-content/tree-task-content.service.ts
@@ -247,8 +247,8 @@ export class TreeTaskContentService implements OnDestroy {
      */
     protected resolveTaskBlockState(): void {
         const taskShouldBeBlocked = !this._taskContentService.task
-            || this._taskContentService.task.user === undefined
-            || !this._userComparator.compareUsers(this._taskContentService.task.user);
+                                    || this._taskContentService.task.user === undefined
+                                    || !this._userComparator.compareUsers(this._taskContentService.task.user);
         this._taskContentService.blockFields(taskShouldBeBlocked);
     }
 
