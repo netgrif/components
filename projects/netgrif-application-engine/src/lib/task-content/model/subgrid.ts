6acbfc07d3472f43ea9ec478b3e214e0786ef298
Jakub Kovář <kovar@netgrif.com>
2021-09-30 10:08:13 +0200

    NAE-1469 - Data group layouts refactor
    - added subgrid tests
    - added subgrid docs

--- projects/netgrif-application-engine/src/lib/task-content/model/subgrid.ts
+++ projects/netgrif-application-engine/src/lib/task-content/model/subgrid.ts
@@ -52,6 +52,9 @@ export class Subgrid {
         return this._renderedContent$.value;
     }
 
+    /**
+     * Completes the underlying stream and clears any running timeouts
+     */
     public destroy() {
         this._renderedContent$.complete();
         if (this._asyncRenderingTimeout !== undefined) {
@@ -81,28 +84,53 @@ export class Subgrid {
         return this._runningTitleCount;
     }
 
-    public addElement(element: DatafieldGridLayoutElement): void {
-        this._content.push(element);
-    }
-
+    /**
+     * Converts a {@link DataField} into a {@link DatafieldGridLayoutElement} and adds it to the content of this subgrid.
+     *
+     * Beware that this method DOES NOT add the element into the resulting grid. This must be done independently by calling the
+     * [addRow]{@link Subgrid#addRow} method.
+     * @param field the field that should be added to the subgrid
+     * @param type the type of the field
+     * @returns the created grid element
+     */
     public addField(field: DataField<unknown>, type: FieldTypeResource): DatafieldGridLayoutElement {
         const element = this.fieldElement(field, type);
         this.addElement(element);
         return element;
     }
 
+    /**
+     * Converts a {@link DataGroup} into a {@link DatafieldGridLayoutElement} and adds it to the content of this subgrid.
+     *
+     * Beware that this method DOES NOT add the element into the resulting grid. This must be done independently by calling the
+     * [addRow]{@link Subgrid#addRow} method.
+     * @param dataGroup the data group whose title should be added to the grid
+     * @returns the created grid element
+     */
     public addTitle(dataGroup: DataGroup): DatafieldGridLayoutElement {
         const element = this.groupTitleElement(dataGroup);
         this.addElement(element);
         return element;
     }
 
+    protected addElement(element: DatafieldGridLayoutElement): void {
+        this._content.push(element);
+    }
+
+    /**
+     * Adds a row of CSS `gridAreaId`s into the grid that is held in this subgrid.
+     * @param row the field Ids. The width of the row should match the number of columns of the grid. If not an error is thrown.
+     */
     public addRow(row: Array<string>): void {
+        if (row.length !== this.cols) {
+            throw new Error(`The provided grid layout row '${JSON.stringify(row)
+            }' does not match the number of columns of this subgrid (${this.cols})`);
+        }
         this._grid.push(row);
     }
 
     /**
-     * Converts the provided configuration into data that can be fed into css grid layout
+     * Converts the provided configuration into data that can be fed into CSS grid layout
      */
     public finalize() {
         this.fillEmptySpace();
@@ -182,6 +210,23 @@ export class Subgrid {
         this.gridAreas = this._grid.map(row => row.join(' ')).join(' | ');
     }
 
+    /**
+     * Marks all contained elements as kept and displays them.
+     */
+    public displayAllFields(): void {
+        this._keptElements = [...this._content];
+        this._newElements = [];
+        this._renderedContent$.next(this._keptElements);
+    }
+
+    /**
+     * Determines which elements from the provided subgrid are contained in this subgrid.
+     * Marks these elements as kept and displays them. Marks the remaining elements as new.
+     *
+     * New elements can be displayed asynchronously over time by calling the
+     * [renderContentOverTime]{@link Subgrid#renderContentOverTime} method.
+     * @param subgrid the "previous" subgrid that is used to compute the element "difference"
+     */
     public determineKeptFields(subgrid: Subgrid): void {
         this._keptElements = [];
         this._newElements = [];
@@ -197,6 +242,19 @@ export class Subgrid {
         this._renderedContent$.next(this._keptElements);
     }
 
+    /**
+     * If elements are not sorted into new and kept, marks all elements as new.
+     * The [determineKeptFields]{@link Subgrid#determineKeptFields} method can be used to mark elements as kept.
+     *
+     * Then pushes the new elements with the kept elements into the output array over time as specified by the
+     * {@link AsyncRenderingConfiguration} provided in the subgrids constructor.
+     *
+     * @param callback the function that is called once all new elements were pushed into the output array
+     * @param first whether this is the first subgrid that si rendered. If this is the first subgrid then on the initial rendering of
+     * elements a batch of new fields alongside the loader elements is displayed so that some content is immediately available.
+     * Otherwise The initial batch contains only loader elements. Being first effectively shortens the rendering by one batch, since the
+     * first loader-only batch is skipped.
+     */
     public renderContentOverTime(callback: () => void, first: boolean = false) {
         if (this._newElements === undefined) {
             this._newElements = Array.from(this._content);
