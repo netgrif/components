1056ccf741a43e80f1ecca6e5e4d8de94828dcd1
Jakub Kovář <kovar@netgrif.com>
2021-11-08 12:55:41 +0100

    NAE-1518 Export of chained filters
    - simplified filter extraction in view based on new filter process structure

--- projects/netgrif-application-engine/src/lib/navigation/utility/filter-extraction.service.ts
+++ projects/netgrif-application-engine/src/lib/navigation/utility/filter-extraction.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Filter} from '../../filter/models/filter';
 import {DataGroup} from '../../resources/interface/data-groups';
-import {extractFilterFieldFromData, extractFilterFromFilterField} from './navigation-item-task-utility-methods';
+import {extractFilterFromFilterField} from './navigation-item-task-utility-methods';
 import {getFieldFromDataGroups, getFieldIndexFromDataGroups} from '../../utility/get-field';
 import {UserFilterConstants} from '../../filter/models/user-filter-constants';
 import {FilterRepository} from '../../filter/filter.repository';
@@ -9,6 +9,8 @@ import {TextField} from '../../data-fields/text-field/models/text-field';
 import {LoggerService} from '../../logger/services/logger.service';
 import {MergeOperator} from '../../filter/models/merge-operator';
 import {TaskResourceService} from '../../resources/engine-endpoint/task-resource.service';
+import {FilterField} from '../../data-fields/filter-field/models/filter-field';
+import {DataField} from '../../data-fields/models/abstract-data-field';
 
 /**
  * This service is able to load the full saved filter including all of its ancestor filters.
@@ -26,36 +28,42 @@ export class FilterExtractionService {
                 protected _log: LoggerService) {
     }
 
-    public extractCompleteFilterFromData(dataSection: Array<DataGroup>, taskReffed = true): Filter {
-        const field = extractFilterFieldFromData(dataSection, taskReffed);
-        if (field === undefined) {
-            throw new Error('Could not extract filter field from task data');
+    public extractCompleteFilterFromData(dataSection: Array<DataGroup>, taskReffed = true): Filter | undefined {
+        const filterIndex = getFieldIndexFromDataGroups(dataSection, UserFilterConstants.FILTER_FIELD_ID, taskReffed);
+
+        if (filterIndex === undefined) {
+            return undefined;
         }
 
         let filterSegment: Filter;
         try {
-            filterSegment = extractFilterFromFilterField(field);
+            filterSegment = extractFilterFromFilterField(
+                dataSection[filterIndex.dataGroupIndex].fields[filterIndex.fieldIndex] as FilterField
+            );
         } catch (e) {
             throw new Error('Filter segment could not be extracted from filter field');
         }
 
-        const parentFilterIndex = getFieldIndexFromDataGroups(dataSection, UserFilterConstants.PARENT_FILTER_CASE_ID_FIELD_ID, taskReffed);
-        if (parentFilterIndex === undefined) {
-            const rootViewFilter = this.extractViewFilter(dataSection, true);
-            if (rootViewFilter !== undefined) {
-                filterSegment.merge(rootViewFilter, MergeOperator.AND);
-            }
+        const parentFilter = this.extractCompleteFilterFromData(dataSection.slice(filterIndex.dataGroupIndex + 1));
+
+        if (parentFilter !== undefined) {
+            return filterSegment.merge(parentFilter, MergeOperator.AND);
+        }
+
+        // Is the filter view rooted?
+        const rootViewIdField = getFieldFromDataGroups(dataSection, UserFilterConstants.ORIGIN_VIEW_ID_FIELD_ID, taskReffed);
+        if (rootViewIdField === undefined) {
             return filterSegment;
         }
 
-        return filterSegment.merge(
-            this.extractCompleteFilterFromData(dataSection.slice(parentFilterIndex.dataGroupIndex + 1)),
-            MergeOperator.AND
-        );
+        const rootViewFilter = this.extractViewFilter(rootViewIdField);
+        if (rootViewFilter !== undefined) {
+            return filterSegment.merge(rootViewFilter, MergeOperator.AND);
+        }
+        return filterSegment;
     }
 
-    protected extractViewFilter(dataSection: Array<DataGroup>, taskReffed = true): Filter {
-        const originViewIdField = getFieldFromDataGroups(dataSection, UserFilterConstants.ORIGIN_VIEW_ID_FIELD_ID, taskReffed);
+    protected extractViewFilter(originViewIdField: DataField<any>): Filter {
         if (originViewIdField === undefined || !(originViewIdField instanceof TextField)) {
             throw new Error('Could not extract origin view id field from task data');
         }
