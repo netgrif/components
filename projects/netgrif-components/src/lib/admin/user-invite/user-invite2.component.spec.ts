c814a28ad8fa3a05524f668283d64b0a3b8d27ef
Machac <machac@netgrif.com>
2022-03-30 00:42:50 +0200

    [NAE-1594] SonarCloud analysis is not configured correctly for @netgrif/components  - update sonar properties  - sonar Test

---
+++ projects/netgrif-components/src/lib/admin/user-invite/user-invite2.component.spec.ts
@@ -0,0 +1,45 @@
+import {waitForAsync, ComponentFixture, TestBed} from '@angular/core/testing';
+
+import {UserInviteComponent} from './user-invite.component';
+import {
+    AuthenticationMethodService,
+    AuthenticationService,
+    ConfigurationService,
+    MaterialModule,
+    MockAuthenticationMethodService,
+    MockAuthenticationService,
+    SignUpService,
+    TestConfigurationService,
+    TranslateLibModule
+} from '@netgrif/components-core';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+
+describe('UserInviteComponent2', () => {
+    let component: UserInviteComponent;
+    let fixture: ComponentFixture<UserInviteComponent>;
+
+    beforeEach(waitForAsync(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule, MaterialModule, TranslateLibModule, NoopAnimationsModule],
+            providers: [
+                {provide: AuthenticationMethodService, useClass: MockAuthenticationMethodService},
+                {provide: AuthenticationService, useClass: MockAuthenticationService},
+                {provide: ConfigurationService, useClass: TestConfigurationService},
+                SignUpService
+            ],
+            declarations: [UserInviteComponent]
+        })
+            .compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(UserInviteComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
