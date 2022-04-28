dad5a3b30ecc32198939ba4ceb7e65b000fc9b24
Stefan Renczes <renczes.stefan@gmail.com>
2022-04-28 09:32:20 +0200

    [NAE-1363] Refactor panel featured values
    - refactored index attribute on abstract-panel-item.component.ts to use boolean value instead to determine if case icon is enabled

--- projects/netgrif-components-core/src/lib/panel/panel-item/abstract-panel-item.component.ts
+++ projects/netgrif-components-core/src/lib/panel/panel-item/abstract-panel-item.component.ts
@@ -4,9 +4,9 @@ import { FeaturedValue } from '../abstract/featured-value';
 export abstract class AbstractPanelItemComponent {
 
     @Input() leadingIcon: string;
+    @Input() leadingIconEnabled: boolean;
     @Input() featuredValue: FeaturedValue;
     @Input() textEllipsis = false;
-    @Input() index: number;
 
     constructor() {
 
