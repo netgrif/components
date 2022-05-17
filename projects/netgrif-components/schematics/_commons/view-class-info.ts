27253d25d733f75300f2fb19f684726240ec2021
palajsamuel <palaj@netgrif.com>
2022-05-17 14:18:06 +0200

    [NAE-1437] Missing view components schematics
    - added schematics for generation of Workflow and Role Assignment views

--- projects/netgrif-components/schematics/_commons/view-class-info.ts
+++ projects/netgrif-components/schematics/_commons/view-class-info.ts
@@ -67,6 +67,10 @@ export class ViewClassInfo extends ImportToAdd {
                 return 'Dashboard';
             case 'treeCaseView':
                 return 'TreeCaseView';
+            case 'workflowView':
+                return 'WorkflowView';
+            case 'roleAssignmentView':
+                return 'RoleAssignmentView';
             default:
                 throw new SchematicsException(`Unknown view type '${view}'`);
         }
