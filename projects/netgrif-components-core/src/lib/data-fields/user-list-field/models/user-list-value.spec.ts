b6ece05a4ce4910262c6efb5f2e196454dd052f8
Stefan Renczes <renczes.stefan@gmail.com>
2022-08-11 21:57:16 +0200

    [NAE-1679] User list component
    - commiting tests

--- projects/netgrif-components-core/src/lib/data-fields/user-list-field/models/user-list-value.spec.ts
+++ projects/netgrif-components-core/src/lib/data-fields/user-list-field/models/user-list-value.spec.ts
@@ -7,11 +7,11 @@ describe('UserListValue', () => {
     it('should create an instance', () => {
         let user: UserListValue;
         user = new UserListValue([new UserValue('0', 'name', 'surname', 'mail')]);
-        expect(user[0].id).toEqual('0');
-        expect(user[0].name).toEqual('name');
-        expect(user[0].surname).toEqual('surname');
-        expect(user[0].fullName).toEqual('name surname');
-        expect(user[0].email).toEqual('mail');
+        expect(user.userValues[0].id).toEqual('0');
+        expect(user.userValues[0].name).toEqual('name');
+        expect(user.userValues[0].surname).toEqual('surname');
+        expect(user.userValues[0].fullName).toEqual('name surname');
+        expect(user.userValues[0].email).toEqual('mail');
     });
 
     afterEach(() => {
