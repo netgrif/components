eceb3234da664c5e5a81fc87a30bf06d35732170
Tomáš Kováčik <kovacik@netgrif.com>
2022-08-18 13:02:54 +0200

    [NAE-1662] Navigation breadcrumbs
    -fix test

--- projects/netgrif-components/src/lib/navigation/breadcrumbs/breadcrumbs.component.spec.ts
+++ projects/netgrif-components/src/lib/navigation/breadcrumbs/breadcrumbs.component.spec.ts
@@ -1,25 +1,51 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { BreadcrumbsComponent } from './breadcrumbs.component';
+import {BreadcrumbsComponent} from './breadcrumbs.component';
+import {CommonModule} from '@angular/common';
+import {RouterTestingModule} from '@angular/router/testing';
+import {
+    AuthenticationMethodService,
+    ConfigurationService,
+    MaterialModule,
+    MockAuthenticationMethodService,
+    TestConfigurationService, TranslateLibModule,
+} from 'netgrif-components-core';
+import {FlexLayoutModule, FlexModule} from '@angular/flex-layout';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 
 describe('BreadcrumbsComponent', () => {
-  let component: BreadcrumbsComponent;
-  let fixture: ComponentFixture<BreadcrumbsComponent>;
+    let component: BreadcrumbsComponent;
+    let fixture: ComponentFixture<BreadcrumbsComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ BreadcrumbsComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [BreadcrumbsComponent],
+            imports: [
+                CommonModule,
+                RouterTestingModule.withRoutes([]),
+                MaterialModule,
+                FlexModule,
+                FlexLayoutModule,
+                HttpClientTestingModule,
+                TranslateLibModule
+            ],
+            providers: [
+                {provide: AuthenticationMethodService, useClass: MockAuthenticationMethodService},
+                {provide: ConfigurationService, useClass: TestConfigurationService}
+            ],
+            schemas: [CUSTOM_ELEMENTS_SCHEMA]
+        })
+            .compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(BreadcrumbsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(BreadcrumbsComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
