955fe6916b503fba1f71fe89f1380857a5039275
Stefan Renczes <renczes.stefan@gmail.com>
2022-08-22 15:20:37 +0200

    [NAE-1626] Trigger of set event for fileList value deletion is not implemented
    - corrected tests

--- projects/netgrif-components-core/src/lib/data-fields/user-list-field/abstract-user-list-field.component.spec.ts
+++ projects/netgrif-components-core/src/lib/data-fields/user-list-field/abstract-user-list-field.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { AbstractUserListFieldComponent } from './abstract-user-list-field.component';
 import { Component, Inject, Optional } from '@angular/core';
@@ -7,52 +7,117 @@ import { SnackBarService } from '../../snack-bar/services/snack-bar.service';
 import { TranslateService } from '@ngx-translate/core';
 import { NAE_INFORM_ABOUT_INVALID_DATA } from '../models/invalid-data-policy-token';
 import { UserListField } from './models/user-list-field';
+import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../../material/material.module';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { TranslateLibModule } from '../../translate/translate-lib.module';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import {
+    AbstractSideMenuContainerComponent
+} from '../../side-menu/side-menu-container/abstract-side-menu-container.component';
+import { NAE_SIDE_MENU_CONTROL } from '../../side-menu/side-menu-injection-token';
+import { SideMenuControl } from '../../side-menu/models/side-menu-control';
+import {
+    AbstractUserAssignComponent
+} from '../../side-menu/content-components/user-assign/abstract-user-assign.component';
+import { expect } from '@angular/flex-layout/_private-utils/testing';
+import { UserValue } from '../user-field/models/user-value';
 
-describe('AbstractUserlListFieldComponent', () => {
-  let component: TestUserListComponent;
-  let fixture: ComponentFixture<TestWrapperComponent>;
+describe('AbstractUserListFieldComponent', () => {
+    let component: TestUserListFieldComponent;
+    let fixture: ComponentFixture<TestUserListFieldComponent>;
+    let sideMenuComponent: TestSideMenuComponent;
+    let sideMenuFixture: ComponentFixture<TestSideMenuComponent>;
+    let service: SideMenuService;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ AbstractUserListFieldComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(waitForAsync(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                CommonModule,
+                MaterialModule,
+                NoopAnimationsModule,
+                TranslateLibModule,
+                HttpClientTestingModule
+            ],
+            declarations: [TestUserListFieldComponent, TestSideMenuComponent],
+            providers: [
+                TranslateService
+            ]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TestWrapperComponent);
-    component = fixture.debugElement.children[0].componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(TestUserListFieldComponent);
+        sideMenuFixture = TestBed.createComponent(TestSideMenuComponent);
+        service = TestBed.inject(SideMenuService);
+        component = fixture.componentInstance;
+        sideMenuComponent = sideMenuFixture.componentInstance;
+        fixture.detectChanges();
+        sideMenuFixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should open and close', (done) => {
+        component.selectAbstractUser(TestAssignUserComponent);
+        service.close({opened: true, message: 'Test',
+            data: new UserValue('test', 'name', 'surname', 'test@email.com')});
+        expect(component.dataField.value.userValues[0].id === 'test').toBeTruthy();
+        done();
+    });
+
+    afterEach(() => {
+        TestBed.resetTestingModule();
+    });
 });
 
 @Component({
-    selector: 'ncc-test-user-list',
+    selector: 'ncc-test-userlistfield',
     template: ''
 })
-class TestUserListComponent extends AbstractUserListFieldComponent {
+class TestUserListFieldComponent extends AbstractUserListFieldComponent {
     constructor(sideMenuService: SideMenuService,
                 snackbar: SnackBarService,
                 translate: TranslateService,
                 @Optional() @Inject(NAE_INFORM_ABOUT_INVALID_DATA) informAboutInvalidData: boolean | null) {
         super(sideMenuService, snackbar, translate, informAboutInvalidData);
+        this.dataField = new UserListField('', '', {
+            required: true,
+            optional: true,
+            visible: true,
+            editable: true,
+            hidden: true
+        }, undefined);
     }
 }
 
 @Component({
-    selector: 'ncc-test-wrapper',
-    template: '<ncc-test-user-list [dataField]="field"> </ncc-test-user-list>'
+    selector: 'ncc-test-import',
+    template: '<input type="file" id="sidemenu-fileUpload" name="fileUpload" multiple="multiple" accept="text/xml"/>'
 })
-class TestWrapperComponent {
-    field = new UserListField('', '', {
-        required: true,
-        optional: true,
-        visible: true,
-        editable: true,
-        hidden: true
-    }, undefined);
+class TestAssignUserComponent extends AbstractUserAssignComponent {
+    constructor(@Inject(NAE_SIDE_MENU_CONTROL) protected _sideMenuControl: SideMenuControl) {
+        super(_sideMenuControl);
+    }
+}
+
+@Component({
+    selector: 'ncc-test-sidemenu',
+    template: '<mat-sidenav-container class="side-menu-container">\n' +
+        '    <mat-sidenav #rightSideMenu mode="over" position="end" class="side-menu" ngClass.lt-sm="side-menu-size-mobile"' +
+        ' [ngClass.gt-xs]="portalWrapper.size">\n' +
+        '        <ng-template [cdkPortalOutlet]="portalWrapper.portal"></ng-template>\n' +
+        '    </mat-sidenav>\n' +
+        '    <mat-sidenav-content>\n' +
+        '        <ng-content></ng-content>\n' +
+        '    </mat-sidenav-content>\n' +
+        '</mat-sidenav-container>'
+})
+class TestSideMenuComponent extends AbstractSideMenuContainerComponent {
+    constructor(protected _sideMenuService: SideMenuService) {
+        super(_sideMenuService);
+    }
 }
