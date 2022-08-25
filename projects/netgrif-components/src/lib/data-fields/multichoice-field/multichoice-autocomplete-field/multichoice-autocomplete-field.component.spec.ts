367ad36cbec48dc41cf838d9da53c49b97710e36
Machac <machac@netgrif.com>
2022-08-25 15:47:33 +0200

    [NAE-1650] Multichoice autocomplete field component missing
    - test

--- projects/netgrif-components/src/lib/data-fields/multichoice-field/multichoice-autocomplete-field/multichoice-autocomplete-field.component.spec.ts
+++ projects/netgrif-components/src/lib/data-fields/multichoice-field/multichoice-autocomplete-field/multichoice-autocomplete-field.component.spec.ts
@@ -1,25 +1,59 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 
-import { MultichoiceAutocompleteFieldComponent } from './multichoice-autocomplete-field.component';
+import {MultichoiceAutocompleteFieldComponent} from './multichoice-autocomplete-field.component';
+import {EnumerationField, MaterialModule, TranslateLibModule, WrappedBoolean} from '@netgrif/components-core';
+import {AngularResizeEventModule} from 'angular-resize-event';
+import {BrowserAnimationsModule, NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {Component, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {FormControl} from '@angular/forms';
 
 describe('MultichoiceAutocompleteFieldComponent', () => {
-  let component: MultichoiceAutocompleteFieldComponent;
-  let fixture: ComponentFixture<MultichoiceAutocompleteFieldComponent>;
+    let component: MultichoiceAutocompleteFieldComponent;
+    let fixture: ComponentFixture<TestWrapperComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ MultichoiceAutocompleteFieldComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(waitForAsync(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                MaterialModule,
+                AngularResizeEventModule,
+                BrowserAnimationsModule,
+                TranslateLibModule,
+                HttpClientTestingModule,
+                NoopAnimationsModule
+            ],
+            declarations: [MultichoiceAutocompleteFieldComponent, TestWrapperComponent],
+            schemas: [CUSTOM_ELEMENTS_SCHEMA]
+        })
+            .compileComponents();
+        fixture = TestBed.createComponent(TestWrapperComponent);
+        component = fixture.debugElement.children[0].componentInstance;
+        fixture.detectChanges();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MultichoiceAutocompleteFieldComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    afterEach(() => {
+        TestBed.resetTestingModule();
+    });
+
 });
+
+@Component({
+    selector: 'nc-test-wrapper',
+    template: '<nc-enumeration-autocomplete-select-field [showLargeLayout]="label" [enumerationField]="field" [formControlRef]="form">' +
+        '</nc-enumeration-autocomplete-select-field>'
+})
+class TestWrapperComponent {
+    label = new WrappedBoolean();
+    field = new EnumerationField('', '', 'test', [{key: 'test', value: 'test'}], {
+        required: true,
+        optional: true,
+        visible: true,
+        editable: true,
+        hidden: true
+    });
+    form = new FormControl();
+}
