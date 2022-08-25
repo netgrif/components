0aea40ed714c07cd4b0af1e9bf1f1673646f5545
Machac <machac@netgrif.com>
2022-08-25 15:29:35 +0200

    [NAE-1650] Multichoice autocomplete field component missing
    - test

--- projects/netgrif-components-core/src/lib/data-fields/multichoice-field/multichoice-autocomplete-field/abstract-multichoice-autocomplete-field-component.component.spec.ts
+++ projects/netgrif-components-core/src/lib/data-fields/multichoice-field/multichoice-autocomplete-field/abstract-multichoice-autocomplete-field-component.component.spec.ts
@@ -1,25 +1,71 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 
-import { AbstractMultichoiceAutocompleteFieldComponentComponent } from './abstract-multichoice-autocomplete-field-component.component';
+import {
+    AbstractMultichoiceAutocompleteFieldComponentComponent
+} from './abstract-multichoice-autocomplete-field-component.component';
+import {MaterialModule} from '../../../material/material.module';
+import {AngularResizeEventModule} from 'angular-resize-event';
+import {BrowserAnimationsModule, NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {TranslateLibModule} from '../../../translate/translate-lib.module';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {Component, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {WrappedBoolean} from '../../data-field-template/models/wrapped-boolean';
+import {DynamicEnumerationField} from '../../enumeration-field/models/dynamic-enumeration-field';
+import {FormControl} from '@angular/forms';
 
 describe('AbstractMultichoiceAutocompleteFieldComponentComponent', () => {
-  let component: AbstractMultichoiceAutocompleteFieldComponentComponent;
-  let fixture: ComponentFixture<AbstractMultichoiceAutocompleteFieldComponentComponent>;
+    let component: TestEnumAutoComponent;
+    let fixture: ComponentFixture<TestWrapperComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ AbstractMultichoiceAutocompleteFieldComponentComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(waitForAsync(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                MaterialModule,
+                AngularResizeEventModule,
+                BrowserAnimationsModule,
+                TranslateLibModule,
+                HttpClientTestingModule,
+                NoopAnimationsModule
+            ],
+            declarations: [TestEnumAutoComponent, TestWrapperComponent],
+            schemas: [CUSTOM_ELEMENTS_SCHEMA]
+        }).compileComponents();
+        fixture = TestBed.createComponent(TestWrapperComponent);
+        component = fixture.debugElement.children[0].componentInstance;
+        fixture.detectChanges();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AbstractMultichoiceAutocompleteFieldComponentComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    afterEach(() => {
+        TestBed.resetTestingModule();
+    });
 });
+
+
+@Component({
+    selector: 'ncc-test-enum-auto',
+    template: ''
+})
+class TestEnumAutoComponent extends AbstractMultichoiceAutocompleteFieldComponentComponent {
+
+}
+
+@Component({
+    selector: 'ncc-test-wrapper',
+    template: '<ncc-test-enum-auto [showLargeLayout]="label" [enumerationField]="field" [formControlRef]="form">' +
+        '</ncc-test-enum-auto>'
+})
+class TestWrapperComponent {
+    label = new WrappedBoolean();
+    field = new DynamicEnumerationField('', '', 'test', [{key: 'test', value: 'test'}], {
+        required: true,
+        optional: true,
+        visible: true,
+        editable: true,
+        hidden: true
+    }, undefined, undefined, undefined, undefined, undefined, undefined, {name: 'autocomplete_dynamic'});
+    form = new FormControl();
+}
