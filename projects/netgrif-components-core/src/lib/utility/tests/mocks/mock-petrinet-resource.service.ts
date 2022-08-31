77fdc52b4184cdaa928204eb16f86e52c608b12f
Stefan Renczes <renczes.stefan@gmail.com>
2022-08-31 12:07:40 +0200

    [NAE-1631] Process download in workflow view
    - corrected test

--- projects/netgrif-components-core/src/lib/utility/tests/mocks/mock-petrinet-resource.service.ts
+++ projects/netgrif-components-core/src/lib/utility/tests/mocks/mock-petrinet-resource.service.ts
@@ -2,24 +2,13 @@ import { Injectable } from '@angular/core';
 import { ProviderProgress, ResourceProvider } from '../../../resources/resource-provider.service';
 import { Observable, of } from 'rxjs';
 import { filter, map } from 'rxjs/operators';
-import { HttpEventType } from '@angular/common/http';
+import { HttpEvent, HttpEventType, HttpResponse } from '@angular/common/http';
 
 @Injectable()
 export class MockPetrinetResourceService {
 
     public getNetFile(netId: string): Observable<ProviderProgress | Blob> {
-        return of({} as Blob).pipe(
-            map(event => {
-                switch (event.type) {
-                    case HttpEventType.DownloadProgress:
-                        return ResourceProvider.getProgress(event);
-                    case HttpEventType.Response:
-                        return event.body;
-                    default:
-                        return undefined;
-                }
-            }),
-            filter(value => !!value)
-        );
+        return of(new HttpResponse<Blob>()).pipe(
+            map(event => { return event.body; }));
     }
 }
