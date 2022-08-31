20577f91b6bbccae7faa104fe5dcb2311dfa3516
Tomáš Kováčik <kovacik@netgrif.com>
2022-08-31 14:31:04 +0200

    [NAE-1457] Schematic support for public view generation
    -fixes according to PR

--- projects/netgrif-components/schematics/view/create-view-prompt/views/public-resolver-view/create-public-resolver-view.ts
+++ projects/netgrif-components/schematics/view/create-view-prompt/views/public-resolver-view/create-public-resolver-view.ts
@@ -20,12 +20,8 @@ export function createPublicResolverView(tree: Tree, args: CreateTaskViewArgumen
     const templateParams = {
         prefix: projectInfo.projectPrefixDasherized,
         className: view.nameWithoutComponent,
-        viewPath: args.path,
         dasherize: strings.dasherize,
-        classify: strings.classify,
-        configName: projectInfo.projectNameClassified,
-        configImportPath: createRelativePath(view.fileImportPath, `./${projectInfo.projectNameDasherized}-configuration.service`),
-        isDefaultTabbedTaskView: !!args.isDefaultTabbedTaskView
+        classify: strings.classify
     };
 
     const commonPathPrefix = './views/public-resolver-view/files/';
