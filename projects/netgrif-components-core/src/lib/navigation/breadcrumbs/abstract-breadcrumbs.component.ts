22b137e3e41a1d643faabd3aeaaf03738bac620b
Tomáš Kováčik <kovacik@netgrif.com>
2022-08-31 15:01:01 +0200

    [NAE-1662] Navigation breadcrumbs
    -fix according to PR

--- projects/netgrif-components-core/src/lib/navigation/breadcrumbs/abstract-breadcrumbs.component.ts
+++ projects/netgrif-components-core/src/lib/navigation/breadcrumbs/abstract-breadcrumbs.component.ts
@@ -7,17 +7,18 @@ import {UriService} from '../service/uri.service';
 })
 export abstract class AbstractBreadcrumbsComponent {
 
+    private static ROOT: string = 'root';
     private _showPaths: boolean;
 
     constructor(protected _uriService: UriService) {
     }
 
     public getPath(): string[] {
-        const tmp = this._uriService.activeNode?.uriPath.split('/').filter(s => s !== 'root');
+        const tmp = this._uriService.activeNode?.uriPath.split('/').filter(s => s !== AbstractBreadcrumbsComponent.ROOT);
         if (tmp?.length > 3 && !this._showPaths) {
             return [tmp[0], '...' , tmp[tmp.length - 2], tmp[tmp.length - 1]]
         }
-        return tmp;
+        return tmp === undefined ? [] : tmp;
     }
 
     public reset(): void {
@@ -30,7 +31,7 @@ export abstract class AbstractBreadcrumbsComponent {
             return;
         }
         let fullPath: string = '';
-        const tmp = this._uriService.activeNode?.uriPath.split('/').filter(s => s !== 'root');
+        const tmp = this._uriService.activeNode?.uriPath.split('/').filter(s => s !== AbstractBreadcrumbsComponent.ROOT);
         if (tmp === undefined) return;
         const control = this._showPaths ? count : this.resultCounter(count, tmp);
         for (let i = 0; i <= control; i++) {
@@ -42,7 +43,7 @@ export abstract class AbstractBreadcrumbsComponent {
         })
     }
 
-    private resultCounter(count: number, tmp: string[]) {
+    private resultCounter(count: number, tmp: string[]): number {
         if (count === 2) return tmp.length - 2;
         if (count === 3) return tmp.length - 1;
         return count;
