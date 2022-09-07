10f6b1e914272604e08458e2539ecaa553145869
Jakub Kovář <6195502+minop@users.noreply.github.com>
2022-09-07 16:47:56 +0200

    [NAE-1704] Dashboard data field component - component resolver task ref field

--- projects/netgrif-components/src/lib/data-fields/task-ref-field/task-ref-field.component.ts
+++ projects/netgrif-components/src/lib/data-fields/task-ref-field/task-ref-field.component.ts
@@ -1,13 +1,15 @@
-import {Component} from '@angular/core';
+import {Component, Inject, Optional} from '@angular/core';
+import {AbstractTaskRefFieldComponent, NAE_INFORM_ABOUT_INVALID_DATA} from '@netgrif/components-core';
 
 @Component({
     selector: 'nc-task-ref-field',
     templateUrl: './task-ref-field.component.html',
     styleUrls: ['./task-ref-field.component.scss']
 })
-export class TaskRefFieldComponent {
+export class TaskRefFieldComponent extends AbstractTaskRefFieldComponent {
 
-    constructor() {
+    constructor(@Optional() @Inject(NAE_INFORM_ABOUT_INVALID_DATA) informAboutInvalidData: boolean | null) {
+        super(informAboutInvalidData);
     }
 
 }
