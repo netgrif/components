3523f0c7d6f59bcd67303c84cfd8731b097ab0bf
Stefan Renczes <renczes.stefan@gmail.com>
2022-12-16 13:16:52 +0100

    [NAE-1803] UserListComponent validations bug
    - added check for empty array

--- projects/netgrif-components-core/src/lib/data-fields/user-list-field/models/user-list-field.ts
+++ projects/netgrif-components-core/src/lib/data-fields/user-list-field/models/user-list-field.ts
@@ -4,6 +4,12 @@ import { Layout } from '../../models/layout';
 import { Validation } from '../../models/validation';
 import { Component } from '../../models/component';
 import { UserListValue } from './user-list-value';
+import {AbstractControl, FormControl, ValidationErrors, ValidatorFn, Validators} from "@angular/forms";
+
+export enum UserListFieldValidation {
+    REQUIRED = 'required'
+}
+
 
 export class UserListField extends DataField<UserListValue> {
 
@@ -17,4 +23,46 @@ export class UserListField extends DataField<UserListValue> {
         return (!a && !b) ||
             (!!a && !!b && a.userValues.length === b.userValues.length);
     }
+
+    protected calculateValidity(forValidRequired: boolean, formControl: FormControl): boolean {
+        const isDisabled = formControl.disabled;
+        if (forValidRequired) {
+            formControl.enable();
+        }
+        formControl.clearValidators();
+        if (forValidRequired) {
+            formControl.setValidators(this.behavior.required ? [this.requiredTrue] : []);
+        } else {
+            formControl.setValidators(this.resolveFormControlValidators());
+        }
+        formControl.updateValueAndValidity();
+        const validity = this._determineFormControlValidity(formControl);
+        isDisabled ? formControl.disable() : formControl.enable();
+        return validity;
+    }
+
+
+    protected resolveFormControlValidators(): Array<ValidatorFn> {
+        const result = [];
+
+        if (this.behavior.required) {
+            result.push(this.requiredTrue);
+        }
+
+        if (this.validations) {
+            if (this._validators) {
+                result.push(...this._validators);
+            } else {
+                this._validators = this.resolveValidations();
+                result.push(...this._validators);
+            }
+        }
+
+        return result;
+    }
+
+    private requiredTrue(control: AbstractControl): { [k: string]: boolean } {
+        return !!control.value && !!control.value._userValues && control.value._userValues.length > 0 ? null : {requiredUserList: true};
+    }
+
 }
