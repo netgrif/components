6646735654b8e220d1243730ffea4b0ea5842eb9
Stefan Renczes <renczes.stefan@gmail.com>
2023-01-11 13:24:42 +0100

    [NAE-1809] UserList field show selected users
    - corrected according to tests

---
+++ projects/netgrif-components-core/src/lib/side-menu/content-components/user-assign/base-user-assign-list/abstract-base-user-assign-list.component.spec.ts
@@ -0,0 +1,83 @@
+import {waitForAsync, ComponentFixture, TestBed} from '@angular/core/testing';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {Component, NO_ERRORS_SCHEMA} from '@angular/core';
+import {BrowserDynamicTestingModule} from '@angular/platform-browser-dynamic/testing';
+import {FormControl} from '@angular/forms';
+import {UserListService} from '../../../../user/services/user-list.service';
+import {ErrorSnackBarComponent} from '../../../../snack-bar/components/error-snack-bar/error-snack-bar.component';
+import {UserValue} from '../../../../data-fields/user-field/models/user-value';
+import {UserListInjectedData} from '../model/user-list-injected-data';
+import {ConfigurationService} from '../../../../configuration/configuration.service';
+import {TestConfigurationService} from '../../../../utility/tests/test-config';
+import {TranslateLibModule} from '../../../../translate/translate-lib.module';
+import {MaterialModule} from '../../../../material/material.module';
+import {SnackBarModule} from '../../../../snack-bar/snack-bar.module';
+import {AbstractBaseUserAssignListComponent} from "./abstract-base-user-assign-list.component";
+
+describe('AbstractBaseUserAssignListComponent', () => {
+    let component: TestUserComponent;
+    let fixture: ComponentFixture<TestWrapperComponent>;
+
+    beforeEach(waitForAsync(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                MaterialModule,
+                NoopAnimationsModule,
+                HttpClientTestingModule,
+                TranslateLibModule,
+                SnackBarModule
+            ],
+            providers: [
+                UserListService,
+                {provide: ConfigurationService, useClass: TestConfigurationService}
+            ],
+            declarations: [
+                TestUserComponent,
+                TestWrapperComponent
+            ],
+            schemas: [NO_ERRORS_SCHEMA]
+        }).overrideModule(BrowserDynamicTestingModule, {
+            set: {
+                entryComponents: [
+                    ErrorSnackBarComponent
+                ]
+            }
+        }).compileComponents();
+
+        fixture = TestBed.createComponent(TestWrapperComponent);
+        component = fixture.debugElement.children[0].componentInstance;
+        fixture.detectChanges();
+    }));
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should get loading', () => {
+        expect(component.loading).toBeFalse();
+    });
+
+    afterEach(() => {
+        TestBed.resetTestingModule();
+    });
+});
+
+@Component({
+    selector: 'ncc-test-user',
+    template: ''
+})
+class TestUserComponent extends AbstractBaseUserAssignListComponent {
+    constructor(protected _userListService: UserListService) {
+        super(_userListService);
+    }
+}
+
+@Component({
+    selector: 'ncc-test-wrapper',
+    template: '<ncc-test-user [searchUserControl]="formControl"></ncc-test-user>'
+})
+class TestWrapperComponent {
+    injectedData = {roles: [], value: new UserValue('5', 'admin', 'netgrif', 'super@netgrif.com')} as UserListInjectedData;
+    formControl = new FormControl();
+}
--- projects/netgrif-components-core/src/lib/side-menu/content-components/user-assign/multi-user-assign-list/abstract-multi-user-assign-list.component.ts
+++ projects/netgrif-components-core/src/lib/side-menu/content-components/user-assign/multi-user-assign-list/abstract-multi-user-assign-list.component.ts
@@ -1,10 +1,8 @@
-import {Component, EventEmitter, Input, OnDestroy, OnInit, Output, ViewChild} from '@angular/core';
+import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
 import {Observable, ReplaySubject} from 'rxjs';
-import {FormControl} from '@angular/forms';
-import {CdkVirtualScrollViewport} from '@angular/cdk/scrolling';
-import {debounceTime} from 'rxjs/operators';
-import {RolesObject, UserListItem, UserListService} from '../../../../user/services/user-list.service';
+import {UserListItem, UserListService} from '../../../../user/services/user-list.service';
 import {UserValue} from '../../../../data-fields/user-field/models/user-value';
+import {AbstractBaseUserAssignListComponent} from "../base-user-assign-list/abstract-base-user-assign-list.component";
 
 /**
  * Is responsible for displaying, filtering, loading and selecting users.
@@ -13,23 +11,14 @@ import {UserValue} from '../../../../data-fields/user-field/models/user-value';
     selector: 'ncc-abstract-multi-user-assign-list',
     template: ''
 })
-export abstract class AbstractMultiUserAssignListComponent implements OnInit, OnDestroy {
-
-    protected SEARCH_DEBOUNCE_TIME = 600;
+export abstract class AbstractMultiUserAssignListComponent extends AbstractBaseUserAssignListComponent implements OnInit, OnDestroy {
 
     @Input() initiallySelectedUsers: Array<UserValue> | undefined;
-
-    @Input() searchUserControl: FormControl;
-
-    @Input() roles: RolesObject | Array<string>;
-
-    @Input() negativeRoles: RolesObject | Array<string>;
 
     @Output() userSelected: EventEmitter<UserValue>;
 
     @Output() userUnselected: EventEmitter<UserValue>;
 
-    @ViewChild(CdkVirtualScrollViewport) public viewport: CdkVirtualScrollViewport;
 
     public users$: Observable<Array<UserListItem>>;
 
@@ -37,7 +26,8 @@ export abstract class AbstractMultiUserAssignListComponent implements OnInit, On
 
     protected _currentlySelectedUsers: Array<string>;
 
-    constructor(protected _userListService: UserListService) {
+    constructor(_userListService: UserListService) {
+        super(_userListService);
         this.users$ = this._userListService.users$;
         this.userSelected = new EventEmitter<UserValue>();
         this.userUnselected = new EventEmitter<UserValue>();
@@ -46,22 +36,10 @@ export abstract class AbstractMultiUserAssignListComponent implements OnInit, On
     }
 
     ngOnInit() {
+        super.ngOnInit();
         if (this.initiallySelectedUsers) {
             this._selectedUsers$.next(this.initiallySelectedUsers.map(v => v.id));
             this._currentlySelectedUsers.push(...this.initiallySelectedUsers.map(v => v.id));
-        }
-        this.searchUserControl.valueChanges.pipe(debounceTime(this.SEARCH_DEBOUNCE_TIME)).subscribe(searchText => {
-            this._userListService.reload(searchText);
-        });
-        if (this.roles instanceof Array) {
-            this._userListService.rolesQuery = this.roles;
-        } else if (this.roles !== undefined && this.roles !== null) {
-            this._userListService.rolesQuery = Object.keys(this.roles);
-        }
-        if (this.negativeRoles instanceof Array) {
-            this._userListService.negativeRolesQuery = this.negativeRoles;
-        } else if (this.negativeRoles !== undefined && this.negativeRoles !== null) {
-            this._userListService.negativeRolesQuery = Object.keys(this.negativeRoles);
         }
     }
 
@@ -77,14 +55,6 @@ export abstract class AbstractMultiUserAssignListComponent implements OnInit, On
         return this._currentlySelectedUsers;
     }
 
-    public trackBy(index: number, item: UserValue): any {
-        return item.id;
-    }
-
-    public get loading(): boolean {
-        return this._userListService.loading;
-    }
-
     public select(selectedUser: UserListItem): void {
         const index = this._currentlySelectedUsers.indexOf(selectedUser.id, 0);
         if (index > -1) {
@@ -95,12 +65,5 @@ export abstract class AbstractMultiUserAssignListComponent implements OnInit, On
             this.userSelected.emit(new UserValue(selectedUser.id, selectedUser.name, selectedUser.surname, selectedUser.email));
         }
         this._selectedUsers$.next([...this._currentlySelectedUsers]);
-    }
-
-    public loadNextPage(): void {
-        if (!this.viewport) {
-            return;
-        }
-        this._userListService.nextPage(this.viewport.getRenderedRange().end, this.viewport.getDataLength());
     }
 }
