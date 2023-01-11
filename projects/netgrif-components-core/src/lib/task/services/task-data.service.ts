14e2faea4d2f3dd1846992212ea430402b0ac967
Stefan Renczes <renczes.stefan@gmail.com>
2023-01-11 09:20:28 +0100

    [NAE-1792] The value of dynamic autocomplete enumeration field disappears after update
    - blocking field while it returns value from backend

--- projects/netgrif-components-core/src/lib/task/services/task-data.service.ts
+++ projects/netgrif-components-core/src/lib/task/services/task-data.service.ts
@@ -204,8 +204,10 @@ export class TaskDataService extends TaskHandlingService implements OnDestroy {
                         if (this.wasFieldUpdated(field)) {
                             if (field instanceof DynamicEnumerationField) {
                                 field.loading = true;
+                                field.block = true;
                                 this.updateTaskDataFields(this._afterActionFactory.create(bool => {
                                     field.loading = false;
+                                    field.block = false;
                                 }));
                             } else {
                                 this.updateTaskDataFields();
