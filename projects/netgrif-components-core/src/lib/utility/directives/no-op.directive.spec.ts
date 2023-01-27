4c55d9b8acc7935159f8c4a074eab7b216318e9f
Stefan Renczes <renczes.stefan@gmail.com>
2023-01-27 12:50:19 +0100

    [NAE-1822] Task list broken scrolling
    - corrected according to consulting and assisted testing.

--- projects/netgrif-components-core/src/lib/utility/directives/no-op.directive.spec.ts
+++ projects/netgrif-components-core/src/lib/utility/directives/no-op.directive.spec.ts
@@ -1,8 +1,33 @@
-import { LetDirective } from './let.directive';
+import {LetDirective} from './let.directive';
+import {Component} from "@angular/core";
+import {ComponentFixture, TestBed} from "@angular/core/testing";
 
 describe('NoOpDirective', () => {
-  it('should create an instance', () => {
-    const directive = new LetDirective();
-    expect(directive).toBeTruthy();
-  });
+    let component: TestComponent;
+    let fixture: ComponentFixture<TestComponent>;
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(TestComponent);
+        component = fixture.componentInstance;
+    })
+
+    it('should create an instance', () => {
+        const directive = new LetDirective();
+        expect(directive).toBeTruthy();
+    });
+
+    it('should set', () => {
+        expect(component).toBeTruthy();
+    });
 });
+
+@Component({
+    selector: 'ncc-test-component',
+    template: '<div *nccLet="this.nmr"></div>'
+})
+class TestComponent {
+    nmr: number;
+    constructor() {
+        this.nmr = 0;
+    }
+}
