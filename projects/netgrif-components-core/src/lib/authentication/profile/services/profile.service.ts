12e57da845648ce6db34f736e5d83e2ae4d237b0
Machac <machac@netgrif.com>
2023-03-02 10:49:16 +0100

    [NAE-1835] Change password component
    - test

--- projects/netgrif-components-core/src/lib/authentication/profile/services/profile.service.ts
+++ projects/netgrif-components-core/src/lib/authentication/profile/services/profile.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {ConfigurationService} from "../../../configuration/configuration.service";
 import {HttpClient} from "@angular/common/http";
 import {LoggerService} from "../../../logger/services/logger.service";
@@ -9,19 +9,14 @@ import {processMessageResponse} from "../../../utility/process-message-response"
 import {UserChangePasswordRequest} from "../models/user-change-password-request";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ProfileService {
 
     protected readonly _changePassword: string;
 
-
     constructor(protected _config: ConfigurationService, protected _http: HttpClient, protected _log: LoggerService) {
-        const authAddress = _config.get().providers.auth.address;
-        if (!authAddress) {
-            throw new Error('Authentication provider address is not set!');
-        }
-        this._changePassword = this.resolveEndpoint('changePassword', authAddress);
+        this._changePassword = _config.resolveProvidersEndpoint('changePassword');
     }
 
     public changePassword(changePasswordRequest: UserChangePasswordRequest): Observable<MessageResource> {
@@ -35,20 +30,5 @@ export class ProfileService {
 
 //TODO: Change Profile photo
 
-    /**
-     * @param endpointKey the attribute name of the endpoint address in `nae.json`
-     * @param baseUrl the base of the endpoint URL. Content resolved by key from `nae.json` is appended to the URL provided by this argument
-     * @returns the endpoint address or `undefined` if such endpoint is not defined in `nae.json`
-     */
-    protected resolveEndpoint(endpointKey: string, baseUrl: string): string | undefined {
-        const config = this._config.get();
-        if (!config
-            || !config.providers
-            || !config.providers.auth
-            || !config.providers.auth.endpoints
-            || !config.providers.auth.endpoints[endpointKey]) {
-            return undefined;
-        }
-        return baseUrl + config.providers.auth.endpoints[endpointKey];
-    }
+
 }
