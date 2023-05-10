bff424a0801006a9416dd390e3f235ea3cea27bc
chvostek <chvostek@netgrif.com>
2023-05-10 08:55:44 +0200

    [NAE-1882] Filter folder process - remove deprecated code

--- projects/netgrif-components-core/src/lib/navigation/model/group-navigation-constants.ts
+++ projects/netgrif-components-core/src/lib/navigation/model/group-navigation-constants.ts
@@ -8,28 +8,10 @@ export enum GroupNavigationConstants {
      */
     GROUP_NAVIGATION_ROUTER_PARAM = 'filterCaseId',
     /**
-     * Id of the transition that stores the group navigation menu data
-     */
-    NAVIGATION_CONFIG_TRANSITION_ID = 'navigationMenuConfig',
-    /**
-     * Id of the task ref field that holds the ids of the individual navigation item tasks
-     */
-    NAVIGATION_ENTRIES_TASK_REF_FIELD_ID = 'filter_tasks',
-    /**
      * Suffix of the field id that contains the name of the navigation entry.
      * Since the field itself is inserted via task ref the actual id is prefixed by the task id.
      */
     NAVIGATION_ENTRY_TITLE_FIELD_ID_SUFFIX = 'entry_name',
-    /**
-     * Suffix of the field id that contains the filter case id.
-     * Since the field itself is inserted via task ref the actual id is prefixed by the task id.
-     */
-    NAVIGATION_FILTER_CASE_ID_FIELD_ID_SUFFIX = 'filter_case_id',
-    /**
-     * Suffix of the field id that is the navigation entry marker.
-     * Since the field itself is inserted via task ref the actual id is prefixed by the task id.
-     */
-    NAVIGATION_ENTRY_MARKER_FIELD_ID_SUFFIX = 'entry_marker',
     /**
      * Suffix of the boolean field id that determines if the navigation entry defines an icon.
      * Since the field itself is inserted via task ref the actual id is prefixed by the task id.
@@ -39,19 +21,5 @@ export enum GroupNavigationConstants {
      * Suffix of the text field id that contains the id of the used material icon.
      * Since the field itself is inserted via task ref the actual id is prefixed by the task id.
      */
-    NAVIGATION_ENTRY_ICON_FIELD_ID_SUFFIX = 'icon_name',
-    /**
-     * Suffix of the text field id that contains the IDs of roles that are able to view the navigation entry.
-     * Since the field itself is inserted via task ref the actual id is prefixed by the task id.
-     */
-    NAVIGATION_ENTRY_ALLOWED_ROLES_FIELD_ID_SUFFIX = 'allowed_roles',
-    /**
-     * Suffix of the text field id that contains the IDs of roles that are banned from viewing the navigation entry.
-     * Since the field itself is inserted via task ref the actual id is prefixed by the task id.
-     */
-    NAVIGATION_ENTRY_BANNED_ROLES_FIELD_ID_SUFFIX = 'banned_roles',
-    /**
-     * The number of datagroups in the navigation configuration task that correspond to a single navigation entry.
-     */
-    DATAGROUPS_PER_NAVIGATION_ENTRY = 2,
+    NAVIGATION_ENTRY_ICON_FIELD_ID_SUFFIX = 'icon',
 }
