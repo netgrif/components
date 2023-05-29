ddc71acfbcb34632092c735be8f6a13b44c3a1e5
chvostek <chvostek@netgrif.com>
2023-05-29 11:57:13 +0200

    [NAE-1882] Filter folder process - implement application of custom view in double drawer

--- projects/netgrif-components/src/lib/navigation/group-navigation-component-resolver/default-group-navigation-component-resolver.service.ts
+++ projects/netgrif-components/src/lib/navigation/group-navigation-component-resolver/default-group-navigation-component-resolver.service.ts
@@ -1,22 +1,65 @@
 import {Injectable, Type} from '@angular/core';
 import {
-    DataGroup, extractFilterFromData,
+    DataGroup,
+    extractFilterFromData,
     FilterType,
-    GroupNavigationComponentResolverService, LoggerService, TaskResourceService,
+    GroupNavigationComponentResolverService,
+    LoggerService,
+    TaskResourceService,
+    ConfigurationService,
+    View,
+    ViewService,
+    extractFieldValueFromData, RoutingBuilderService
 } from '@netgrif/components-core';
 import {DefaultTabViewComponent} from './default-components/default-tab-view/default-tab-view.component';
-import {DefaultSimpleTaskViewComponent} from './default-components/default-simple-task-view/default-simple-task-view.component';
 
 @Injectable()
 export class DefaultGroupNavigationComponentResolverService extends GroupNavigationComponentResolverService {
 
-    constructor(taskResourceService: TaskResourceService, log: LoggerService) {
+    constructor(taskResourceService: TaskResourceService, log: LoggerService, private _configService: ConfigurationService, private _viewService: ViewService,) {
         super(taskResourceService, log);
     }
 
-    public resolveViewComponent(navigationItemTaskData: Array<DataGroup>): Type<any> {
-        const filter = extractFilterFromData(navigationItemTaskData);
+    public resolveViewComponent(navItemData: Array<DataGroup>): Type<any> {
+        const useCustomRouting = extractFieldValueFromData<boolean>(navItemData, 'use_custom_view');
+        if (useCustomRouting) {
+            return this.resolveCustomComponent(navItemData);
+        } else {
+            return this.resolveDefaultComponent(navItemData);
+        }
+    }
 
+    private resolveCustomComponent(navItemData: Array<DataGroup>): Type<any> {
+        const customSelector = extractFieldValueFromData<string>(navItemData, 'custom_view_selector');
+        for (const [pathSegment, view] of Object.entries(this._configService.get().views)) {
+            if (pathSegment === customSelector) {
+                return this.resolveComponentClass(view, pathSegment);
+            }
+        }
+    }
+
+    private resolveComponentClass(view: View, configPath: string): Type<any> | undefined {
+        let result;
+        if (!!view.component) {
+            result = this._viewService.resolveNameToClass(view.component.class);
+        } else if (!!view.layout) {
+            result = this.resolveComponentClassFromLayout(view, configPath);
+        } else {
+            return undefined;
+        }
+        if (result === undefined) {
+            return undefined;
+        }
+        return result;
+    }
+
+    private resolveComponentClassFromLayout(view: View, configPath: string): Type<any> | undefined {
+        const className = RoutingBuilderService.parseClassNameFromView(view, configPath);
+        return this._viewService.resolveNameToClass(className);
+    }
+
+    private resolveDefaultComponent(navItemData: Array<DataGroup>): Type<any> {
+        const filter = extractFilterFromData(navItemData);
         if (filter === undefined) {
             throw new Error('Provided navigation item task data does not contain a filter field');
         }
@@ -26,7 +69,7 @@ export class DefaultGroupNavigationComponentResolverService extends GroupNavigat
             case FilterType.TASK:
                 return DefaultTabViewComponent;
             default:
-                throw new Error(`Cannot resolve group navigation component from '${filter.type}' filter type`);
+                throw new Error(`Cannot resolve navigation component from '${filter.type}' filter type`);
         }
     }
 }
