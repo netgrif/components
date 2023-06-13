fe09d23ba876fd8bc6a3f86983439b1564747e63
Stefan Renczes <renczes.stefan@gmail.com>
2023-06-13 15:37:00 +0200

    [NAE-1901] Taskref rendering update
    - fixing tests.

--- projects/netgrif-components/src/lib/data-fields/task-ref-field/task-ref-list-field/task-ref-list-field.component.spec.ts
+++ projects/netgrif-components/src/lib/data-fields/task-ref-field/task-ref-list-field/task-ref-list-field.component.spec.ts
@@ -1,25 +1,42 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { TaskRefListFieldComponent } from './task-ref-list-field.component';
+import {TaskRefListFieldComponent} from './task-ref-list-field.component';
+import {Component} from "@angular/core";
+import {TaskRefField} from "netgrif-components-core";
 
 describe('TaskRefListFieldComponent', () => {
-  let component: TaskRefListFieldComponent;
-  let fixture: ComponentFixture<TaskRefListFieldComponent>;
+    let component: TaskRefListFieldComponent;
+    let fixture: ComponentFixture<TestWrapperComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ TaskRefListFieldComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [TaskRefListFieldComponent]
+        })
+            .compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TaskRefListFieldComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(TestWrapperComponent);
+        component = fixture.debugElement.children[0].componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
+
+
+@Component({
+    selector: 'nc-test-wrapper',
+    template: '<nc-task-ref-list-field [dataField]="field"></nc-task-ref-list-field>'
+})
+class TestWrapperComponent {
+    field = new TaskRefField('', '', ['633c6187bb12a90925e0a17e'], {
+        required: true,
+        optional: true,
+        visible: true,
+        editable: true,
+        hidden: true
+    }, undefined, undefined, undefined, []);
+}
