0ec1a25597e0c59a1a785405b5650420efe531ad
Stefan Renczes <renczes.stefan@gmail.com>
2023-07-13 14:40:15 +0200

    [NAE-1497] Frontend Actions
    - corrected accroding to PR

--- projects/netgrif-components-core/src/lib/public/factories/public-search-factory.ts
+++ projects/netgrif-components-core/src/lib/public/factories/public-search-factory.ts
@@ -8,18 +8,22 @@ import {FilterType} from '../../filter/models/filter-type';
 import {TranslateService} from '@ngx-translate/core';
 import {PublicTaskLoadingService} from '../../view/task-view/service/public-task-loading.service';
 
+export const CASE_ID = "caseId";
+export const PETRI_NET_ID = "petriNetId"
+export const TRANSITION_ID = "transitionId"
+
 export const publicBaseFilterFactory = (router: Router, route: ActivatedRoute, process: ProcessService,
                                         caseResourceService: CaseResourceService, snackBarService: SnackBarService,
                                         translate: TranslateService, publicTaskLoadingService: PublicTaskLoadingService) => {
-    if (route.snapshot.paramMap.get('caseId') === null && route.snapshot.paramMap.get('petriNetId') !== null) {
+    if (route.snapshot.paramMap.get(CASE_ID) === null && route.snapshot.paramMap.get(PETRI_NET_ID) !== null) {
         getNetAndCreateCase(router, route, process, caseResourceService, snackBarService, translate, publicTaskLoadingService);
-    } else if (route.snapshot.paramMap.get('caseId') !== null && route.snapshot.paramMap.get('transitionId') !== null) {
+    } else if (route.snapshot.paramMap.get(CASE_ID) !== null && route.snapshot.paramMap.get(TRANSITION_ID) !== null) {
         return {
-            filter: new SimpleFilter('', FilterType.TASK, {case: {id: route.snapshot.paramMap.get('caseId')}, transitionId: route.snapshot.paramMap.get('transitionId')})
+            filter: new SimpleFilter('', FilterType.TASK, {case: {id: route.snapshot.paramMap.get(CASE_ID)}, transitionId: route.snapshot.paramMap.get(TRANSITION_ID)})
         };
-    } else if (route.snapshot.paramMap.get('caseId') !== null) {
+    } else if (route.snapshot.paramMap.get(CASE_ID) !== null) {
         return {
-            filter: new SimpleFilter('', FilterType.TASK, {case: {id: route.snapshot.paramMap.get('caseId')}})
+            filter: new SimpleFilter('', FilterType.TASK, {case: {id: route.snapshot.paramMap.get(CASE_ID)}})
         };
     }
     return {
