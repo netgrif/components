8d4118f5daf316eb2b8bb153d81e66dae0e5e3b8
Stefan Renczes <renczes.stefan@gmail.com>
2024-01-08 14:13:32 +0100

    [NAE-1935] Improved breadcrumbs from menu items
    - made changes according to PR

--- projects/netgrif-components-core/src/lib/navigation/model/uri-resource.ts
+++ projects/netgrif-components-core/src/lib/navigation/model/uri-resource.ts
@@ -3,7 +3,6 @@ export interface UriNodeResource {
     id: string;
     uriPath: string;
     name: string;
-    displayName: string;
     parentId: string;
     parent: UriNodeResource;
     childrenId: Set<string>;
