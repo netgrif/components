ffd83b3d2db8e77bae3349bb146b17eb0a5e0933
Tomáš Kováčik <kovacik@netgrif.com>
2024-03-20 15:53:29 +0100

    [NAE-1957] Allow filter to caseRef field and variants  - changes according to PR

--- projects/netgrif-components-core/src/lib/data-fields/multichoice-field/multichoice-caseref-field/abstract-multichoice-caseref-field.component.ts
+++ projects/netgrif-components-core/src/lib/data-fields/multichoice-field/multichoice-caseref-field/abstract-multichoice-caseref-field.component.ts
@@ -1,69 +1,28 @@
 import {AfterViewInit, Component, Inject, Injector, Optional, Type} from "@angular/core";
-import {NAE_BASE_FILTER} from "../../../search/models/base-filter-injection-token";
-import {SimpleFilter} from "../../../filter/models/simple-filter";
-import {BaseFilter} from "../../../search/models/base-filter";
-import {NAE_VIEW_ID_SEGMENT} from "../../../user/models/view-id-injection-tokens";
-import {ViewIdService} from "../../../user/services/view-id.service";
-import {AbstractBaseDataFieldComponent} from "../../base-component/abstract-base-data-field.component";
 import {DATA_FIELD_PORTAL_DATA, DataFieldPortalData} from "../../models/data-field-portal-data-injection-token";
 import {ComponentPortal} from "@angular/cdk/portal";
-import {NAE_DEFAULT_HEADERS} from '../../../header/models/default-headers-token';
 import {MultichoiceField} from '../models/multichoice-field';
-import {NAE_CASE_REF_CREATE_CASE, NAE_CASE_REF_SEARCH} from '../../case-ref-field/model/case-ref-injection-tokens';
-import {CaseSearchRequestBody} from '../../../filter/models/case-search-request-body';
+import {AbstractCaseRefBaseFieldComponent} from '../../case-ref-field/model/abstract-case-ref-base-field-component';
 
 @Component({
     selector: 'ncc-abstract-case-ref-default',
     template: ''
 })
-export abstract class AbstractMultichoiceCaseRefComponent extends AbstractBaseDataFieldComponent<MultichoiceField> implements AfterViewInit {
+export abstract class AbstractMultichoiceCaseRefComponent extends AbstractCaseRefBaseFieldComponent<MultichoiceField> implements AfterViewInit {
 
     public componentPortal: ComponentPortal<any>;
 
     protected constructor(protected injector: Injector,
                           protected caseViewType: Type<any>,
                           @Optional() @Inject(DATA_FIELD_PORTAL_DATA) dataFieldPortalData: DataFieldPortalData<MultichoiceField>) {
-        super(dataFieldPortalData);
+        super(injector, caseViewType, dataFieldPortalData);
     }
 
     ngAfterViewInit(): void {
-        this.createFilter();
+        this.createFilter(this.dataField.choices.length > 0 ? this.dataField.choices.map(value => value.key) : '');
         this.dataField.updatedChoices.subscribe(() => {
-            this.createFilter();
+            this.createFilter(this.dataField.choices.length > 0 ? this.dataField.choices.map(value => value.key) : '');
         });
-    }
-
-    createFilter() {
-        let portalInjector;
-        const filterValue : string | string[] = this.dataField.choices.length > 0 ? this.dataField.choices.map(value => value.key) : '';
-        const filterProperty: boolean = this.dataField?.component?.properties?.filter === 'true';
-        let query: CaseSearchRequestBody;
-        if (filterProperty) {
-            query = JSON.parse(this.dataField?.component?.properties?.filterQuery) as CaseSearchRequestBody;
-        }
-        portalInjector = Injector.create({
-            providers: [
-                {
-                    provide: NAE_DEFAULT_HEADERS, useValue: this.dataField.component?.properties?.headers.split(',')
-                },
-                {
-                    provide: NAE_CASE_REF_CREATE_CASE, useValue: this.dataField.component?.properties?.createCase === 'true'
-                },
-                {
-                    provide: NAE_CASE_REF_SEARCH, useValue: this.dataField.component?.properties?.search === 'true'
-                },
-                {
-                    provide: NAE_BASE_FILTER,
-                    useValue: { filter: SimpleFilter.fromCaseQuery((filterProperty && query ? query : {stringId: filterValue})) } as BaseFilter
-                },
-                {
-                    provide: NAE_VIEW_ID_SEGMENT,
-                    useValue: this.dataField.parentCaseId + '_' + this.dataField.parentTaskId + '_' + this.dataField.stringId
-                },
-                { provide: ViewIdService, useClass: ViewIdService }],
-            parent: this.injector
-        });
-        this.componentPortal = new ComponentPortal(this.caseViewType, null, portalInjector);
     }
 
 }
