269e2a1b44a0b82fc1b687a42b5450931e80cc58
Jozef Daxner <daxner.dodo@gmail.com>
2025-06-13 13:36:58 +0200

    [NAE-2116] Frontend remote configuration
    - production env fix

--- projects/nae-example-app/src/environments/environment.prod.ts
+++ projects/nae-example-app/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+    production: true,
+    resolve_configuration: window['env']['resolve_configuration'] || false,
+    gateway_url: window['env']['gateway_url'] || 'http://localhost:8800/api',
+    application_identifier: window['env']['application_identifier'] || 'nae',
+    type_identifier: window['env']['type_identifier'] || 'default'
 };
