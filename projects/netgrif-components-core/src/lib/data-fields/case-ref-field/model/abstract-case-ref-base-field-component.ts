8f7fbd31b240a8270f043a997b9dd8b03362d013
Tomáš Kováčik <kovacik@netgrif.com>
2025-09-25 14:01:25 +0200

    [NAE-2205] DefaultCaseRefListViewComponent - headers not displayed when processes exceed single view
    - another fix

--- projects/netgrif-components-core/src/lib/data-fields/case-ref-field/model/abstract-case-ref-base-field-component.ts
+++ projects/netgrif-components-core/src/lib/data-fields/case-ref-field/model/abstract-case-ref-base-field-component.ts
@@ -17,7 +17,6 @@ import {BaseFilter} from '../../../search/models/base-filter';
 import {NAE_VIEW_ID_SEGMENT} from '../../../user/models/view-id-injection-tokens';
 import {ViewIdService} from '../../../user/services/view-id.service';
 import {DataField} from '../../models/abstract-data-field';
-import {TaskSearchRequestBody} from "../../../filter/models/task-search-request-body";
 
 export abstract class AbstractCaseRefBaseFieldComponent<T extends DataField<unknown>> extends AbstractBaseDataFieldComponent<T> {
 
@@ -35,7 +34,7 @@ export abstract class AbstractCaseRefBaseFieldComponent<T extends DataField<unkn
         let query: CaseSearchRequestBody;
         if (filterProperty) {
             try {
-                query = JSON.parse(this.dataField?.component?.properties?.filterQuery) as TaskSearchRequestBody;
+                query = JSON.parse(this.dataField?.component?.properties?.filterQuery) as CaseSearchRequestBody;
             } catch {
                 console.warn('Invalid filterQuery JSON on CaseRef field or component ', this.dataField?.stringId);
                 query = undefined;
