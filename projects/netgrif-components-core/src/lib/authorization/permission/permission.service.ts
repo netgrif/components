8b4ac4966feb731696d6bd6f86d7575336e31ab0
chvostek <chvostek@netgrif.com>
2026-02-06 18:06:06 +0100

    [NAE-2285] Group as a value of userlists - make rabbit happy

--- projects/netgrif-components-core/src/lib/authorization/permission/permission.service.ts
+++ projects/netgrif-components-core/src/lib/authorization/permission/permission.service.ts
@@ -110,17 +110,17 @@ export class PermissionService {
         let userPermValue: boolean;
         if (!!users) {
             const loggedUser: User = this._userService.user.getSelfOrImpersonated();
-            const processedActorIds: Array<string> = []
+            const processedActorIds: Array<string> = [];
             Object.keys(users).forEach(actorId => {
                 if (userPermValue === false || processedActorIds.includes(actorId)
                     || actorId !== loggedUser.id && !loggedUser.nextGroups?.includes(actorId)) {
                     return;
                 }
-                let currentUserPermission: boolean = this.getPermissionByUserOrGroup(users, permission, loggedUser)
+                let currentUserPermission: boolean = this.getPermissionByUserOrGroup(users, permission, loggedUser);
                 if (currentUserPermission !== undefined) {
                     userPermValue = userPermValue === undefined ? currentUserPermission : userPermValue && currentUserPermission;
                 }
-                loggedUser.nextGroups !== undefined ? processedActorIds.push(actorId, ...loggedUser.nextGroups) : processedActorIds.push(actorId)
+                loggedUser.nextGroups !== undefined ? processedActorIds.push(actorId, ...loggedUser.nextGroups) : processedActorIds.push(actorId);
             });
         }
         return userPermValue;
