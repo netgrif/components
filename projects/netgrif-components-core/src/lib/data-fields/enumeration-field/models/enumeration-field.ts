969a6cda5add3caa755a119c7cf788c17deb2be2
renczesstefan <renczes.stefan@gmail.com>
2026-05-11 16:46:20 +0200

    Refactor validation logic in enumeration field.
    Replaced `find` with `some` to improve code clarity and intent. The updated method now directly checks for the existence of a matching key, simplifying the logic while maintaining functionality.

--- projects/netgrif-components-core/src/lib/data-fields/enumeration-field/models/enumeration-field.ts
+++ projects/netgrif-components-core/src/lib/data-fields/enumeration-field/models/enumeration-field.ts
@@ -86,6 +86,6 @@ export class EnumerationField extends DataField<string> {
         if (this._choices === undefined || this._choices.length === 0) {
             return {wrongValue: true};
         }
-        return this._choices.find(choice => choice.key === control.value) ? null : {wrongValue: true};
+        return this._choices.some(choice => choice.key === control.value) ? null : {wrongValue: true};
     }
 }
