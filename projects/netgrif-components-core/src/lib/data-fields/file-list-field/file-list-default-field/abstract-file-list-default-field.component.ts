f40cc9fe89b2e1a70fe6a20ac952824aaa9f70f8
renczesstefan <renczes.stefan@gmail.com>
2026-05-18 08:24:46 +0200

    Fix redundant error check during file upload failure
    Simplifies the error handling logic by removing an unnecessary null check for `response.error`. This ensures the snackbar displays error messages consistently without redundant conditionals.

--- projects/netgrif-components-core/src/lib/data-fields/file-list-field/file-list-default-field/abstract-file-list-default-field.component.ts
+++ projects/netgrif-components-core/src/lib/data-fields/file-list-field/file-list-default-field/abstract-file-list-default-field.component.ts
@@ -183,9 +183,7 @@ export abstract class AbstractFileListDefaultFieldComponent extends AbstractFile
                             this._log.error(
                                 `File [${this.dataField.stringId}] ${this.fileUploadEl.nativeElement.files.item(0)?.name} uploading has failed!`, response.error
                             );
-                            if (response.error) {
-                                this._snackbar.openErrorSnackBar(this._translate.instant(response.error));
-                            }
+                            this._snackbar.openErrorSnackBar(this._translate.instant(response.error));
                         } else {
                             this._log.debug(
                                 `Files [${this.dataField.stringId}] were successfully uploaded`
