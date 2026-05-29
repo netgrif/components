6debca3a6794f2bff17c3f3f15c6a4eaa5daa495
chvostek <chvostek@netgrif.com>
2026-05-29 15:09:22 +0200

    [NAE-2439] Implement new filter data types - fix test

--- projects/netgrif-components/src/lib/data-fields/filter-field/filter-string-query-field/filter-string-query-field.component.spec.ts
+++ projects/netgrif-components/src/lib/data-fields/filter-field/filter-string-query-field/filter-string-query-field.component.spec.ts
@@ -1,25 +1,57 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { FilterStringQueryFieldComponent } from './filter-string-query-field.component';
+import {FilterStringQueryFieldComponent} from './filter-string-query-field.component';
+import {
+    MaterialModule,
+    TranslateLibModule,
+    FilterField,
+    DataFieldPortalData,
+    DATA_FIELD_PORTAL_DATA,
+    FilterType,
+    ConfigurationService,
+    TestConfigurationService,
+    WrappedBoolean
+} from '@netgrif/components-core';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {FormControl, ReactiveFormsModule} from '@angular/forms';
 
 describe('FilterStringQueryFieldComponent', () => {
-  let component: FilterStringQueryFieldComponent;
-  let fixture: ComponentFixture<FilterStringQueryFieldComponent>;
+    let component: FilterStringQueryFieldComponent;
+    let fixture: ComponentFixture<FilterStringQueryFieldComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ FilterStringQueryFieldComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [FilterStringQueryFieldComponent],
+            imports: [
+                TranslateLibModule,
+                HttpClientTestingModule,
+                MaterialModule,
+                NoopAnimationsModule,
+                ReactiveFormsModule
+            ],
+            providers: [
+                {provide: ConfigurationService, useClass: TestConfigurationService},
+                {provide: DATA_FIELD_PORTAL_DATA, useValue: {
+                        dataField: new FilterField('', '', '', {
+                            filterType: FilterType.CASE, predicateMetadata: [], searchCategories: []
+                        }, [], {}, '', ''),
+                        formControlRef: new FormControl(),
+                        showLargeLayout: new WrappedBoolean()
+                    } as DataFieldPortalData<FilterField>
+                }
+            ]
+        })
+            .compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(FilterStringQueryFieldComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(FilterStringQueryFieldComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
