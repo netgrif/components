d8b5418cf695987669766acc4daebba093f3afc0
chvostek <chvostek@netgrif.com>
2026-06-01 16:09:32 +0200

    [NAE-2439] Implement new filter data types - update default component exports

--- projects/netgrif-components/src/lib/navigation/public-api.ts
+++ projects/netgrif-components/src/lib/navigation/public-api.ts
@@ -10,13 +10,14 @@ export * from './navigation-drawer/navigation-drawer.component';
 export * from './navigation-double-drawer/navigation-double-drawer.component';
 export * from './navigation-rail/navigation-rail.component';
 export * from './group-navigation-component-resolver/group-navigation-component-resolver.component';
+export * from './group-navigation-component-resolver/default-components/default-no-filter-provided/default-no-filter-provided.component';
 export * from './group-navigation-component-resolver/default-components/simple-views/default-simple-task-view/default-simple-task-view.component';
 export * from './group-navigation-component-resolver/default-components/simple-views/default-simple-case-view/default-simple-case-view.component';
+export * from './group-navigation-component-resolver/default-components/simple-views/default-single-task-view/default-single-task-view.component';
 export * from './group-navigation-component-resolver/default-components/tabbed/default-tab-view/default-tab-view.component';
 export * from './group-navigation-component-resolver/default-components/tabbed/default-tabbed-case-view/default-tabbed-case-view.component';
 export * from './group-navigation-component-resolver/default-components/tabbed/default-tabbed-task-view/default-tabbed-task-view.component';
 export * from './group-navigation-component-resolver/default-components/tabbed/default-tabbed-single-task-view/default-tabbed-single-task-view.component';
-export * from './group-navigation-component-resolver/default-components/simple-views/default-single-task-view/default-single-task-view.component';
 export * from './group-navigation-component-resolver/default-components/tabbed/default-ticket-view/default-ticket-view.component';
 export * from './breadcrumbs/breadcrumbs.component';
 export * from './group-navigation-component-resolver/default-components/refs/default-task-view/default-task-view.component';
@@ -24,8 +25,12 @@ export * from './group-navigation-component-resolver/default-components/public/d
 export * from './group-navigation-component-resolver/default-components/public/default-public-task-view/default-public-task-view.component';
 export * from './group-navigation-component-resolver/default-components/public/default-public-workflow-view/default-public-workflow-view.component';
 export * from './group-navigation-component-resolver/default-components/public/default-public-single-task-view/default-public-single-task-view.component';
+export * from './group-navigation-component-resolver/default-components/refs/default-case-ref-list-view/default-case-ref-list-view.component';
+export * from './group-navigation-component-resolver/default-components/refs/default-task-view/default-task-view.component';
 export * from './dashboard/dashboard.component';
 
 /* FUNCTIONS */
 export * from './group-navigation-component-resolver/default-components/model/factory-methods';
 export * from './group-navigation-component-resolver/default-components/model/injected-tabbed-case-view-data-with-navigation-item-task-data';
+export * from './group-navigation-component-resolver/default-components/model/injected-tabbed-task-view-data-with-navigation-item-task-data';
+export * from './group-navigation-component-resolver/default-components/model/injected-tabbed-ticket-view-data-with-navigation-item-task-data';
