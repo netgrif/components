301d29c9b4bf4c2cb75db8d2812bd690d5adf1b4
Tomáš Kováčik <kovacik@netgrif.com>
2026-06-05 09:40:33 +0200

    [NAE-2444] Upgrade Angular to 17
    - commit some fixes

--- projects/netgrif-components-core/src/lib/data-fields/file-field/abstract-file-field.component.ts
+++ projects/netgrif-components-core/src/lib/data-fields/file-field/abstract-file-field.component.ts
@@ -5,6 +5,7 @@ import {
 } from '@angular/core';
 import {AbstractDataFieldComponent} from '../models/abstract-data-field-component';
 import {NAE_INFORM_ABOUT_INVALID_DATA} from '../models/invalid-data-policy-token';
+import {FileField} from "./models/file-field";
 
 /**
  * @deprecated
