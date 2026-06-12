dd06dcd3ed98da5b47d7f61477b667b4b2468716
Tomáš Kováčik <kovacik@netgrif.com>
2026-06-12 11:10:26 +0200

    [NAE-2412] EnumerationMap, MultichoiceMap with 'caseref' component are editable on unassigned task
    - fix merge

--- projects/netgrif-components-core/src/lib/panel/task-panel-list/task-panel-list-pagination/abstract-task-list-pagination.component.ts
+++ projects/netgrif-components-core/src/lib/panel/task-panel-list/task-panel-list-pagination/abstract-task-list-pagination.component.ts
@@ -20,7 +20,6 @@ export abstract class AbstractTaskListPaginationComponent extends AbstractDefaul
     public pageIndex = 0;
     public pageSizeOptions: Array<number> = [10, 20, 50, 100];
 
-    @Input() public disabled: boolean;
     @Input()
     set tasks$(tasks: Observable<Array<TaskPanelData>>) {
         this._tasks$ = tasks.pipe((tap(() => {
