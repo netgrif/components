8c64819e8e545c5d60e481dad6cafc4ca6a8563b
renczesstefan <renczes.stefan@gmail.com>
2026-08-31 15:32:46 +0200

    Add OnDestroy lifecycle and mediaObserver subscription handling
    Refactored the EditModeComponent to implement OnDestroy, managing `mediaObserver` subscriptions properly to prevent memory leaks. Ensured `removeHiddenSorts` is called during initialization, subscription updates, and component destruction for consistent behavior.

--- projects/netgrif-components/src/lib/header/header-modes/edit-mode/edit-mode.component.ts
+++ projects/netgrif-components/src/lib/header/header-modes/edit-mode/edit-mode.component.ts
@@ -1,14 +1,18 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
 import {AbstractEditModeComponent, LoggerService} from '@netgrif/components-core';
 import {MediaObserver} from '@ngbracket/ngx-layout';
+import {Subscription} from "rxjs";
 
 @Component({
     selector: 'nc-edit-mode',
     templateUrl: './edit-mode.component.html',
     styleUrls: ['./edit-mode.component.scss']
 })
-export class EditModeComponent extends AbstractEditModeComponent implements OnInit {
+export class EditModeComponent extends AbstractEditModeComponent implements OnInit, OnDestroy {
+
+    private mediaSubscription!: Subscription;
+
     constructor(protected _translate: TranslateService,
                 protected loggerService: LoggerService,
                 protected mediaObserver: MediaObserver) {
@@ -17,9 +21,18 @@ export class EditModeComponent extends AbstractEditModeComponent implements OnIn
 
     override ngOnInit(): void {
         super.ngOnInit();
-        if (this.headerService.responsiveHeaders && !this.headerService.overflowMode) {
+        this.removeHiddenSorts(this.visibleHeaderCount());
+        this.mediaSubscription = this.mediaObserver.asObservable().subscribe(() => {
             this.removeHiddenSorts(this.visibleHeaderCount());
-        }
+        });
+    }
+
+    ngOnDestroy() {
+        super.ngOnDestroy();
+        this.removeHiddenSorts(this.visibleHeaderCount());
+        this.mediaSubscription = this.mediaObserver.asObservable().subscribe(() => {
+            this.removeHiddenSorts(this.visibleHeaderCount());
+        });
     }
 
     public setValue() {
